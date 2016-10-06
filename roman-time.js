d9fd3b6de9f44c607cb4d4ba61ef2f1de1a8d540
Gallyam Biktahsev <gallyamb@gmail.com>
2016-10-06 21:22:54 +0500

    Fix test errors

--- roman-time.js
+++ roman-time.js
@@ -1,25 +1,31 @@
-'use strict';
+"use strict";
 
-function isInRange(value, start, end) {
+function isInRange (value, start, end) {
     return value >= start && value < end;
 }
 
 function toRoman(value, dict) {
-    var result =  "";
+    var result = "";
     while (value >= 0) {
         var keys = Object.keys(dict);
-        for (var i = keys.length - 1; i >= 0; i--) {
-            if (keys[i] <= value) {
-                value -= keys[i];
-                result += dict[keys[i]];
-                break;
-            }
-        }
+        var i;
+        for (i = keys.length - 1; i >= 0 && keys[i] > value; i--) {}
+        value -= keys[i];
+        result += dict[keys[i]];
         if (value === 0) {
             break;
         }
     }
+    
     return result;
+}
+
+function isCorrectTime(parts) {
+    return parts.length !== 2 ||
+        parts[0].length !== 2 ||
+        parts[1].length !== 2 ||
+        !isInRange(parts[0], 0, 24) ||
+        !isInRange(parts[1], 0, 60)
 }
 
 /**
@@ -28,15 +34,12 @@ function toRoman(value, dict) {
  */
 function romanTime(time) {
     if (time === undefined || time === null) {
-        throw TypeError();
+        throw new TypeError();
     }
     var parts = time.split(":");
-    if (parts.length !== 2 || 
-        parts[0].length !== 2 || 
-        parts[1].length !== 2 ||
-        !isInRange(parts[0], 0, 24) || 
-        !isInRange(parts[1], 0, 60))
-        throw TypeError();
+    if (isCorrectTime(parts)) {
+        throw new TypeError();
+    }
     var dict = {
         "0": "N",
         "1": "I",
@@ -46,14 +49,15 @@ function romanTime(time) {
         "10": "X",
         "40": "XL",
         "50": "L"
-    }
+    };
     var result = "";
     for (var i = 0; i < parts.length; i++) {
         if (result !== "") {
-            result += ":"
+            result += ":";
         }
         result += toRoman(parseInt(parts[i]), dict);
     }
+
     return result;
 }
 
