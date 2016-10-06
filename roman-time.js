6e3198b7712114f35f61e041e765e9310d8b775f
Sergey Ubogov <sergeiubogov@mail.ru>
2016-10-06 23:29:50 +0500

    Update roman-time.js
    privet

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,34 @@
 'use strict';
 
-/**
- * @param {String} time – время в формате HH:MM (например, 09:05)
- * @returns {String} – время римскими цифрами (IX:V)
- */
+function arabToRoman(number) {
+    var arab = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+    var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+    if (!number) {
+        return 'N';
+    }
+    var ret = '';
+    var i = arab.length - 1;
+    while (number > 0) {
+        if (number >= arab[i]) {
+            ret += roman[i];
+            number -= arab[i];
+        } else {
+            i--;
+        }
+    }
+
+    return ret;
+}
+
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    var hours = Number(time.split(':')[0]);
+    var minutes = Number(time.split(':')[1]);
+    if (typeof time === 'undefined' || isNaN(hours) ||
+        isNaN(minutes) || hours > 23 || minutes > 59) {
+        throw new TypeError();
+    }
+
+    return arabToRoman(Number(hours)) + ':' + arabToRoman(Number(minutes));
 }
 
 module.exports = romanTime;
