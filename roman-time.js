592506941f349eabaab820cae153e245f0b578af
AtNes Ness <atnesness@gmail.com>
2016-10-06 20:06:04 +0500

    Linting fix

--- roman-time.js
+++ roman-time.js
@@ -4,16 +4,17 @@
  * @param {String} num– любое положительное число
  * @returns {String} – число num в римском формате
  */
-function romanize (num) {
-    if (+num  == 0)
+function romanize(num) {
+    if (+num == 0)
         return "N";
     if (!+num)
         return false;
 
     var digits = String(+num).split(""),
-        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
-               "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
-               "","I","II","III","IV","V","VI","VII","VIII","IX"],
+        key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
+            "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
+            "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"
+        ],
         roman = "",
         i = 3;
     while (i--)
@@ -28,22 +29,20 @@ function romanize (num) {
 function romanTime(time) {
     var timeError = new TypeError('Неверное время');
     var splitedTime = time.split(":");
-    if (splitedTime.length != 2 || 
+    if (splitedTime.length != 2 ||
         +splitedTime[0] > 23 ||
-         +splitedTime[0] < 0 ||
-         +splitedTime[1] > 59 ||
-         +splitedTime[1] < 0 )
+        +splitedTime[0] < 0 ||
+        +splitedTime[1] > 59 ||
+        +splitedTime[1] < 0)
         throw timeError;
     var romanAray = []
-    for (var i=0; i<2; i++)
-    {
+    for (var i = 0; i < 2; i++) {
         var romanized = romanize(splitedTime[i]);
-        if (!romanized)
-        {
+        if (!romanized) {
             throw timeError;
         }
         romanAray.push(romanized);
-    }    
+    }
     time = romanAray.join(":")
     return time;
 }
