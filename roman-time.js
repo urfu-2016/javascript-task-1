e84d3b62dff162ad4f6229362431dd79a648f5b6
fng3r <dinvk@mail.ru>
2016-10-06 22:42:04 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -9,16 +9,13 @@ function romanTime(time) {
     var hours = parseInt(timeArgs[0], 10);
     var mins = parseInt(timeArgs[1], 10);
 
-    if (isNaN(hours) || isNaN(mins) ||
-        hours < 0 || hours > 23 ||
-        mins < 0 || mins > 59) {
-        throw TypeError();
-    }
+    if (!isTime(hours, mins))
+        throw new TypeError();
 
     timeArgs[0] = hours;
     timeArgs[1] = mins;
 
-    var dict = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L"};
+    var dict = { 1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L" };
     var result = [];
 
     for (var i = 0; i < 2; i++) {
@@ -29,24 +26,30 @@ function romanTime(time) {
 }
 
 function convertToRoman(number, romanNumbers) {
-    if (number === 0)
+    if (number === 0) {
         return "N";
+    }
 
-    var numbers = Object.keys(romanNumbers);
+    var arabicNumbers = Object.keys(romanNumbers);
     var result = "";
+    var index = arabicNumbers.length - 1;
 
     while (number > 0) {
-        for (var i = numbers.length - 1; i >= 0; i--) {
-            var arabic = numbers[i];
-            if (number >= arabic) {
-                number -= arabic;
-                result += romanNumbers[arabic];
-                break;
-            }
+        while (arabicNumbers[index] > number) {
+            index--;
         }
+        var arabic = arabicNumbers[index];
+        number -= arabic;
+        result += romanNumbers[arabic];
     }
 
     return result;
+}
+
+function isTime(hours, mins) {
+    return !(isNaN(hours) || isNaN(mins) ||
+        hours < 0 || hours > 23 ||
+        mins < 0 || mins > 59);
 }
 
 module.exports = romanTime;
