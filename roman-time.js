a04378a9e0784804b54fb61dbe8e95103d83a5e4
Koshcheev Nikita <nikitakosheleo@live.ru>
2016-10-08 04:12:12 +0500

    refactoring

--- roman-time.js
+++ roman-time.js
@@ -4,46 +4,37 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
+var arabianNumbers = [50, 40, 10, 9, 5, 4, 1];
+var romanNumbers = ["L", "XL", "X", "IX", "V", "IV", "I"];
+
 function romanTime(time) {
-    var arabianNumbers = [50,40,10,9,5,4,1];
-    var romanNumbers = ["L", "XL", "X", "IX", "V", "IV", "I"];
-
-    function toRoman(number) {
-        if(number == 0)
-            return "N";
-
-        var result = '';
-
-        while (number > 0)
-        {
-            for(var i = 0; i < arabianNumbers.length ; i++)
-            {
-                if (number >= arabianNumbers[i])
-                {
-                    number -= arabianNumbers[i];
-                    result += romanNumbers[i];
-                    break;
-                }
-            }
-        }
-        return result;
-    }
-
     var arr = time.split(':');
-    if (arr.length != 2 || arr[0].length != 2 || arr[1].length != 2)
-        throw new TypeError('Неверное время');
-
     var hours = parseInt(arr[0], 10);
-    if(hours > 23 || hours < 0)
-        throw new TypeError('Неверное время');
-
     var minutes = parseInt(arr[1], 10);
-    if(minutes > 59 || minutes < 0)
+
+    if (hours > 23 || hours < 0 || minutes > 59 || minutes < 0) {
         throw new TypeError('Неверное время');
+    }
 
     return toRoman(hours) + ':' + toRoman(minutes);
 }
 
+function toRoman(number) {
+    if (number === 0) {
+        return "N";
+    }
 
+    var result = '';
+
+    for (var i = 0; i < arabianNumbers.length; i++) {
+        if (number >= arabianNumbers[i]) {
+            number -= arabianNumbers[i];
+            result += romanNumbers[i];
+            i = 0;
+        }
+    }
+
+    return result;
+}
 
 module.exports = romanTime;
