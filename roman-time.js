3c65f714b405bd0ccb928414c7f928cfdef4f53e
FoKycHuK <fokychuk47@ya.ru>
2016-10-07 12:51:19 +0500

    Возможно нужно еще и на два символа ограничить?

--- roman-time.js
+++ roman-time.js
@@ -21,6 +21,9 @@ var ROMAN_NUMBERS = {
 var ARABIAN_NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 function isCorrectNumber(number) {
+    if (number.length !== 2) {
+        return false;
+    }
     for (var i in number) {
         if (ARABIAN_NUMBERS.indexOf(i) === -1) {
             return false;
