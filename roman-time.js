3b26b9299d18fa6ce95d8212a572e1947ace0ade
Трефилов Андрей <trefilovandrew@yandex.ru>
2016-10-07 13:16:21 +0500

    "Римские часики"

--- roman-time.js
+++ roman-time.js
@@ -5,8 +5,63 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    try
+    {
+        time = time.split(':');
+        if (Number(time[0]) > -1 && Number(time[0]) < 24 
+            && Number(time [1]) > -1 && Number(time [1]) < 60)
+        {
+            if (time[0] == 0) time[0] = 'N';
+            else time[0] = getRomeHours(time[0]);
+            if (time[1] == 0) time[1] = 'N';
+            else time[1] = getRomeMinutes(time[1]);
+            time = time[0] + ':' + time[1];
+            return time;
+        }
+        else {
+            throw new TypeError('Неверное время');
+        }
+    }catch(e){
+        return e.name + ': ' + e.message;
+    }
+}
+
+function getRomeHours(hours){
+    var romeHours = '';
+    for (var i = 0; i < Math.floor( hours / 10 ); i++){
+        romeHours += 'X';
+    }
+    return romeHours += getUnits(hours);
+}
+
+function getRomeMinutes(minutes){
+    var romeMinutes = '';
+    if (minutes < 40)
+        for (var i = 0; i < Math.floor( minutes / 10 ); i++){
+            romeMinutes += 'X';
+    } else if (Math.floor( minutes / 10 ) === 4) romeMinutes += 'XL';
+    else romeMinutes += 'L';
+    return romeMinutes += getUnits(minutes);
+}
+
+function getUnits(time){
+    var unit = '';
+    if (time % 10 < 4){
+        for (var i = 0; i < time % 10 ; i++){
+            unit += 'I';
+        }
+        return unit;
+    } else if(time % 10 === 4){
+        return unit += 'IV';
+    } else if(time % 10 < 9){
+        unit += 'V';
+        for (var i = 0; i < (time % 10 - 5) ; i++){
+            unit += 'I';
+        }
+        return unit;
+    } else {
+        return unit += 'IX';
+    }
 }
 
 module.exports = romanTime;
