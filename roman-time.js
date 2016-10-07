12c53a401a84120b893c8b76774a559de9a67b39
KatyrevaElizaveta <alchimia@ro.ru>
2016-10-08 03:29:34 +0600

    Bug with zeroes fixed - 2

--- roman-time.js
+++ roman-time.js
@@ -6,9 +6,9 @@
  */
 function romanTime(time) {
 
-    var romanOnes = ["N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    var romanOnes = ["L", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 
-    var romanTens = ["N", "X", "XX", "XXX", "XL", "L"];
+    var romanTens = ["L", "X", "XX", "XXX", "XL", "L"];
 
     var arrayOfDateDatas = time.split(":");
     var hours = Number(arrayOfDateDatas[0]);
@@ -18,16 +18,14 @@ function romanTime(time) {
 
     if (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59 &&
             hours % 1 === 0 && minutes % 1 === 0) {
+
         var hoursOnes = hours % 10;
-        var hoursTens = (hours - hoursOnes) / 10;
-
         var minutesOnes = minutes % 10;
-        var minutesTens = (minutes - minutesOnes) / 10;
 
-        romanTimeString = romanTens[hoursTens] + romanOnes[hoursOnes] +
-                ":" + romanTens[minutesTens] + romanOnes[minutesOnes];
-        romanTimeString = romanTimeString.replace(/N:/g, ":");
-        romanTimeString = romanTimeString.replace(/N$/g, "");
+        romanTimeString = romanTens[(hours - hoursOnes) / 10] + romanOnes[hoursOnes] +
+                ":" + romanTens[(minutes - minutesOnes) / 10] + romanOnes[minutesOnes];
+        romanTimeString = romanTimeString.replace(/LL/g, "N");
+        romanTimeString = romanTimeString.replace(/L/g, "");
 
     } else {
         throw new TypeError();
