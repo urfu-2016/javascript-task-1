fd01b53aba5e81a134fbd9b0b814dc0560412cd2
xTave <MishaLevshin@list.ru>
2016-10-07 17:47:46 +0500

    написал функцию romanTime

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,47 @@
 'use strict';
 
+function fromArabianToRoman(arabianNumber) {
+    var romanNumber = "";
+
+    function tryToAddLettersToRomanNumber(letters, arabianMinValue, arabianMaxValue) {
+        if (arabianNumber >= arabianMinValue && arabianNumber <= arabianMaxValue) {
+            arabianNumber -= arabianMinValue;
+            romanNumber += letters;
+        }
+    }
+
+    if (arabianNumber === 0) {
+
+        return 'N';
+    }
+    while (arabianNumber > 0) {
+        tryToAddLettersToRomanNumber('L', 50, 60);
+        tryToAddLettersToRomanNumber('XL', 40, 49);
+        tryToAddLettersToRomanNumber('X', 10, 39);
+        tryToAddLettersToRomanNumber('IX', 9, 9);
+        tryToAddLettersToRomanNumber('V', 5, 8);
+        tryToAddLettersToRomanNumber('IV', 4, 4);
+        tryToAddLettersToRomanNumber('I', 1, 3);
+    }
+
+    return romanNumber;
+}
+
+
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    return time;
+    var splittedTime = time.split(':');
+    var hours = parseInt(splittedTime[0]);
+    var minutes = parseInt(splittedTime[1]);
+    if (isNaN(hours) || isNaN(minutes) || hours > 23 || minutes > 59) {
+        throw TypeError;
+    }
+
+    return fromArabianToRoman(hours) + ':' + fromArabianToRoman(minutes);
 }
 
 module.exports = romanTime;
