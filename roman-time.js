8c53daf4a30e0b7d17cd10a57bf596c33e8045bd
Pavel <pp-1995@mail.ru>
2016-10-08 01:21:56 +0500

    Добавил проверку на неверный формат

--- roman-time.js
+++ roman-time.js
@@ -8,11 +8,17 @@ var ROMAN_NUMBER = ['L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 var ARAB_NUMBER = [50, 40, 10, 9, 5, 4, 1];
 
 function isTime(str) {
-    var hours = str.split(':')[0];
-    var minutes = str.split(':')[1];
-    hours = parseInt(hours);
-    minutes = parseInt(minutes);
-    if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
+    var hours;
+    var minutes;
+    try {
+        hours = str.split(':')[0];
+        minutes = str.split(':')[1];
+        hours = parseInt(hours);
+        minutes = parseInt(minutes);
+        if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
+            throw new TypeError('Неверное время');
+        }
+    } catch (e) {
         return false;
     }
 
@@ -39,7 +45,7 @@ function getRomanNumber(number) {
 
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    if (!isTime(time)) {
+    if (!isTime(time) || time.length !== 5) {
         throw new TypeError('Неверное время');
     }
 
