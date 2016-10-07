6eb9259e60453fbf68ef9a8260a31bf8f4013121
FoKycHuK <fokychuk47@ya.ru>
2016-10-07 13:00:41 +0500

    возможно проверка неправильная?

--- roman-time.js
+++ roman-time.js
@@ -66,7 +66,7 @@ function numberToRoman(number) {
 
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    if (typeof(time) !== 'string' || time.indexOf(' ') !== -1) {
+    if (time === undefined || time === null) {
         throw new TypeError();
     }
     var splitedTime = time.split(':');
