51c4ed1a381b1d2908ec422a8d0bf392ac1487ca
KatyrevaElizaveta <alchimia@ro.ru>
2016-10-08 02:20:30 +0600

    Roman-time function added

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,31 @@
-'use strict';
+﻿'use strict';
 
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+
+    var romanOnes = ["N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+
+    var romanTens = ["", "X", "XX", "XXX", "XL", "L"];
+
+    var arrayOfDateDatas = time.split(":");
+    var hours = Number(arrayOfDateDatas[0]);
+    var minutes = Number(arrayOfDateDatas[1]);
+
+    if (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59 && hours % 1 === 0 && minutes % 1 === 0) {
+        var hoursOnes = hours % 10;
+        var hoursTens = (hours - hoursOnes) / 10;
+
+        var minutesOnes = minutes % 10;
+        var minutesTens = (minutes - minutesOnes) / 10;
+
+        var romanTimeString = romanTens[hoursTens] + romanOnes[hoursOnes] + ":" + romanTens[minutesTens] + romanOnes[minutesOnes];
+        return romanTimeString;
+    } else {
+        throw new TypeError();
+    }
 }
 
 module.exports = romanTime;
