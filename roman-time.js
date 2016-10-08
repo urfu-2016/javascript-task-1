c08a3d17c1ec468c4eddf90d425a7e852aa22122
Ludmilishee <luda_babich@mail.ru>
2016-10-09 03:43:18 +0500

    Надеюсь, последние правки

--- roman-time.js
+++ roman-time.js
@@ -5,16 +5,33 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    var result = [];
-    var dozens = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
-    var units = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
-    var i;
-
     try {
         validateData(time);
     } catch (e) {
         return e.name + ": " + e.message;
     }
+
+    return conversionTime(time);
+}
+
+function validateData(time) {
+    var timeWithoutSeparator = time.split(':');
+    var hours = parseInt(timeWithoutSeparator[0]);
+    var minutes = parseInt(timeWithoutSeparator[1]);
+
+    if (time.length !== 5 || isNaN(hours) || isNaN(minutes)) {
+        throw new TypeError('Неверный формат входных данных');
+    }
+    if (hours > 23 || minutes > 59) {
+        throw new TypeError('Неверный формат времени');
+    }
+}
+
+function conversionTime(time) {
+    var result = [];
+    var dozens = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+    var units = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    var i;
 
     for (i = 0; i < dozens.length; i++) {
         if (parseInt(time[0]) === i) {
@@ -24,7 +41,6 @@ function romanTime(time) {
             result[3] = dozens[i].toString();
         }
     }
-
     for (i = 0; i < units.length; i++) {
         if (parseInt(time[1]) === i) {
             result[1] = units[i].toString();
@@ -42,19 +58,6 @@ function romanTime(time) {
     result[2] = ':';
 
     return result.join("");
-}
-
-function validateData(time) {
-    var timeWithoutSeparator = time.split(':');
-    var hours = parseInt(timeWithoutSeparator[0]);
-    var minutes = parseInt(timeWithoutSeparator[1]);
-
-    if (time.length !== 5 || isNaN(hours) || isNaN(minutes)) {
-        throw new TypeError('Неверный формат входных данных');
-    }
-    if (hours > 23 || minutes > 59) {
-        throw new TypeError('Неверный формат времени');
-    }
 }
 
 module.exports = romanTime;
