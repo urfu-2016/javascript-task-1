74fd87854089aacdd0d09644cd9995cd4cb5e416
AlexeyCherepanov <alex.nvs.96@yandex.ru>
2016-10-08 23:30:16 +0500

    Добавлен второй вариант задания

--- roman-time.js
+++ roman-time.js
@@ -4,9 +4,44 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
+
+var unitsRoman = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VII", "IX"];
+var tensRoman = ["", "X", "XX", "XXX", "XL", "L"];
+
+function convertUnitsRoman(units) {
+		
+		return unitsRoman[units];
+};
+
+function convertTensRoman(tens) {
+    
+	return tensRoman[tens];
+};
+
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    if (!/^([0-2][0-3]|[0-1]\d):[0-5]\d$/.test(time)) {
+        throw new TypeError("Неверное время");
+    }
+	if (time == "00:00") {
+		time = "N:N";
+	}
+    if (/^([0-2][0-3]|[0-1]\d):[0-5]\d$/.test(time)) {
+        time = time.replace(/:/, "");
+        var splitedTime = time.split("");
+		var hourTen = convertTensRoman(parseInt(splitedTime[0]));
+		var hourUnits = convertUnitsRoman(parseInt(splitedTime[1]));
+        var minuteTen = convertTensRoman(parseInt(splitedTime[2]));
+        var minuteUnits = convertUnitsRoman(parseInt(splitedTime[3]));
+        if (hourTen + hourUnits === "") {
+            hourUnits = "N";
+        }
+        if (minuteTen + minuteUnits === "") {
+            minuteUnits = "N";
+        }
+        time = hourTen + hourUnits + ":" + minuteTen + minuteUnits;
+    }
+    
+	return time;
 }
 
 module.exports = romanTime;
