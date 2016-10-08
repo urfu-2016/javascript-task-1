c95dda0cdc2ffa5a821da46737fc8b45ff0ccfcc
axel86rus <lopuchaleksey@mail.ru>
2016-10-08 18:24:06 +0500

    Добавлен первый вариант решения

--- roman-time.js
+++ roman-time.js
@@ -1,11 +1,36 @@
 'use strict';
 
-/**
- * @param {String} time – время в формате HH:MM (например, 09:05)
- * @returns {String} – время римскими цифрами (IX:V)
- */
+function isValiable(hour, minute) {
+    if (isNaN(minute)|| isNaN(hour) || hour > 23 || hour < 0 || minute > 59 || minute < 0) {
+        throw new TypeError("Неверное время")
+    }
+}
+
+function roman(units, dozens) {
+    var res;
+    var romanUnits = ["","I","II","III","IV","V","VI","VII","VIII","IX"];
+    var romanDozens = ["","X","XX","XXX","XL","L"];
+    if (units == 0){
+        res = "N";
+    }else{
+        res = romanDozens[(units - units % 10)/10] + romanUnits[units % 10]
+    }
+    res += ':'
+    if (dozens == 0){
+        res += 'N'
+    }else{
+        res += romanDozens[(dozens - dozens % 10)/10] + romanUnits[dozens % 10]
+    }
+    return res
+}
+
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
+    time = time.split(':')
+    var hour = Number(time[0])
+    var minute = Number(time[1])
+
+    isValiable(hour, minute)
+    time  =  roman(hour, minute)
     return time;
 }
 
