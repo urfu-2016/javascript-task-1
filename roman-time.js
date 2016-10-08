32de146dc06b513dcc2f7bf9f47b8c166e844715
Дмитрий Козачук < >
2016-10-09 00:13:40 +0300

    second attempt

--- roman-time.js
+++ roman-time.js
@@ -1,32 +1,39 @@
 'use strict';
 
 /**
- * @param {String} time – время в формате HH:MM (например, 09:05)
- * @returns {String} – время римскими цифрами (IX:V)
+ * @param {String} arabicTime – арабское число, меньшее 60
+ * @returns {String} – римский аналог
  */
- 
 function transform(arabicTime) {
     var i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
     var x = ["", "X", "XX", "XXX", "XL", "L"];
-    var romanTime = '';
+    var romTime = '';
     var n = Number(arabicTime);
-    if ((arabicTime == '0') || (arabicTime == '00')) {
+    if ((arabicTime === '0') || (arabicTime === '00')) {
         return 'N';
     }
-    romanTime += x[Math.floor(n / 10)];
+    romTime += x[Math.floor(n / 10)];
     n %= 10;
-    romanTime += i[n];
-    return romanTime;
-    
+    romTime += i[n];
+
+    return romTime;
+
 }
+
+/**
+ * @param {String} time – время в формате HH:MM (например, 09:05)
+ * @returns {String} – время римскими цифрами (IX:V)
+ */
 function romanTime(time) {
     var timeList = time.split(':');
-    var isHours = (timeList[0].length == 2) & (timeList[0] <= 24) & (timeList[0] >= 0);
-    var isMinutes = (timeList[1].length == 2) & (timeList[1] <= 60) & (timeList[1] >= 0);
-    if  (isHours & isMinutes & isNaN(timeList[2])) {
-        return transform(timeList[0]) + ':' + transform(timeList[1])
+    var isHours = (timeList[0].length === 2) && (timeList[0] <= 23) && (timeList[0] >= 0);
+    var isMinutes = (timeList[1].length === 2) && (timeList[1] <= 59) && (timeList[1] >= 0);
+    if (isHours && isMinutes && timeList.length === 2) {
+        return transform(timeList[0]) + ':' + transform(timeList[1]);
     }
-    return new TypeError();
+
+    throw new TypeError('TypeError');
+
 }
 module.exports = romanTime;
 
