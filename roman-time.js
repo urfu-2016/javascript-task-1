34275c81997bcb016b1067d29232d3dc0d2b6a2b
Сергей Кобяков <sergeykob96@gmail.com>
2016-10-08 22:11:41 +0500

    Style fix

--- roman-time.js
+++ roman-time.js
@@ -5,11 +5,15 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    if(!isCorrectTimeFormat(time))
+    if (!isCorrectTimeFormat(time))
+    {
       throw new TypeError('Invalid time');
+    }
     var parsedTime = parseTime(time);
-    if(!isCorrectHoursRange(parsedTime))
+    if (!isCorrectHoursRange(parsedTime))
+    {
       throw new TypeError('Invalid time');
+    }
     var hours = convertRomanToArabic(parsedTime[0]);
     var minutes = convertRomanToArabic(parsedTime[1]);
     return hours + ':' + minutes;
@@ -17,7 +21,9 @@ function romanTime(time) {
 
 function parseTime(time) {
   var parsedTime = time.split(':');
-  return parsedTime.map(function(e) { return Number(e); });
+  return parsedTime.map(function(e) {
+    return Number(e);
+  });
 }
 
 function isCorrectHoursRange(time) {
@@ -29,8 +35,10 @@ function isCorrectTimeFormat(time) {
 }
 
 function convertRomanToArabic(value) {
-  if(value === 0)
+  if (value === 0)
+  {
     return 'N';
+  }
   var arabic = [50, 40, 10, 9, 5, 4, 1];
   var roman = ['L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
   var result = "";
