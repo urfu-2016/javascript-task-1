3a539782cf1b0f424ce94937b5033d186c9c6809
Ludmilishee <luda_babich@mail.ru>
2016-10-09 03:04:28 +0500

    'Правки'

--- roman-time.js
+++ roman-time.js
@@ -12,6 +12,7 @@ function romanTime(time) {
         var minutes = parseInt(timeWithoutSeparator[1]);
         var dozens = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
         var units = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+        var i;
 
         if (time.length !== 5 || isNaN(hours) || isNaN(minutes)) {
             throw new TypeError('Неверный формат входных данных');
@@ -19,34 +20,35 @@ function romanTime(time) {
         if (hours > 23 || minutes > 59) {
             throw new TypeError('Неверный формат времени');
         }
-
-        for (var i = 0; i < dozens.length; i++) {
-            if (parseInt(time[0]) === i) {
-                result[0] = dozens[i].toString();
-            }
-            if (parseInt(time[3]) === i) {
-                result[3] = dozens[i].toString();
-            }
-        }
-
-        for (var i = 0; i < units.length; i++) {
-            if (parseInt(time[1]) === i) {
-                result[1] = units[i].toString();
-            }
-            if (parseInt(time[4]) === i) {
-                result[4] = units[i].toString();
-            }
-        }
-        if (result[0]  === '' && result[1] === '') {
-            result[0] = 'N';
-        }
-        if (result[3]  === '' && result[4] === '') {
-            result[3] = 'N';
-        }
-        result[2] = ':'
     } catch (e) {
         console.log(e.name + ": " + e.message);
     }
+
+    for (i = 0; i < dozens.length; i++) {
+        if (parseInt(time[0]) === i) {
+            result[0] = dozens[i].toString();
+        }
+        if (parseInt(time[3]) === i) {
+            result[3] = dozens[i].toString();
+        }
+    }
+
+    for (i = 0; i < units.length; i++) {
+        if (parseInt(time[1]) === i) {
+            result[1] = units[i].toString();
+        }
+        if (parseInt(time[4]) === i) {
+            result[4] = units[i].toString();
+        }
+    }
+    if (result[0]  === '' && result[1] === '') {
+        result[0] = 'N';
+    }
+    if (result[3]  === '' && result[4] === '') {
+        result[3] = 'N';
+    }
+    result[2] = ':';
+
     return result.join("");
 }
 
