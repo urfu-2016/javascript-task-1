f71a3a706599a88b9fef811b43dc2a0a949fc7d1
Aleksandr Bukin <gebetix@gmail.com>
2016-10-08 19:50:48 +0600

    Добавлен третий вариант решения

--- roman-time.js
+++ roman-time.js
@@ -18,12 +18,7 @@ function isTime(time) {
     return /^([0-1][0-9]|2[0-3]):[0-5][0-9]$/.test(time);
 }
 
-/**
- * @param {String} time – время в формате HH:MM (например, 09:05)
- * @returns {String} – время римскими цифрами (IX:V)
- */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
     if (!isTime(time)) {
         throw new TypeError('Input time has wrong format!');
     }
