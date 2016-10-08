a1186db948ca48679db58747125c2835e40f9023
Maxim <endurancemayer@yandex.ru>
2016-10-08 19:53:18 +0500

    Roman Time converter Solution

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,84 @@
 'use strict';
 
+function romanObject(starterString){
+	try{
+		var value = starterString.split(":");
+		var probableHours = parseInt(value[0]);
+		var probableMinutes = parseInt(value[1]);
+	}
+	catch (e) {
+		throw new TypeError("Incorrect time");
+	}
+	
+
+	this.hours = checkTime(probableHours, "hours");
+	this.minutes = checkTime(probableMinutes, "minutes");
+	this.romanAnalogues = {1: "I", 4: "IV",5:"V", 9: "IX", 10:"X", 40: "XL", 50:"L" };
+	this.getMaximumInsideDict = getMaximumInsideDict;
+	this.convertArabicToRoman = convertArabicToRoman;
+	this.returnRomanResult = returnRomanResult;
+	
+
+}
+function checkTime(time, tag) {
+	var result = parseInt(time, 10);
+	
+	if (tag === "hours")
+		if (!((result <= 23) && (result >= 0)) || (isNaN(result)))
+			throw new TypeError("Incorrect time");
+	else if (tag === "minutes")
+		if (!((result <= 59) && (result >= 0)) || (isNaN(result)))
+			throw new TypeError("Incorrect time");
+	return result;
+}
+ function getMaximumInsideDict(elem){
+ 	var difference = Number.MAX_SAFE_INTEGER;
+ 	var searchFor;
+ 	var keys = Object.keys(this.romanAnalogues);
+ 	
+ 	for (var i =0; i < keys.length; i++){
+ 		var curDiff = elem - keys[i];
+ 		if (curDiff < difference && curDiff >= 0)
+ 		{
+ 			difference = curDiff;
+ 			searchFor = keys[i];
+
+ 		}
+	}
+	return searchFor;
+ }
+ function convertArabicToRoman(measure){
+ 	if (measure == 0)
+ 		return "N";
+ 	var number = this.getMaximumInsideDict(measure);
+ 	if (number == measure)
+ 		return this.romanAnalogues[number];
+ 	return this.romanAnalogues[number] + this.convertArabicToRoman(measure - number); 
+}
+
+function returnRomanResult() {
+	return this.convertArabicToRoman(this.hours) + ":" + this.convertArabicToRoman(this.minutes);
+}
+
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    return time;
+    var convertedTime = new romanObject(time);
+	return convertedTime.returnRomanResult();
 }
 
 module.exports = romanTime;
+
+console.info(romanTime('09:10'));
+
+// Выведет 'N:N'
+console.info(romanTime('00:00'));
+
+// Выведет 'XXIII:LIX'
+console.info(romanTime('23:59'));
+
+// Выбросится ошибка [TypeError: Неверное время]
+console.info(romanTime('24:00'));
