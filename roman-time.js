c6a836b5b2f9b3e305ce21a813578eef1256395a
Maxim <endurancemayer@yandex.ru>
2016-10-08 19:56:18 +0500

    deleted comments

--- roman-time.js
+++ roman-time.js
@@ -72,13 +72,3 @@ function romanTime(time) {
 
 module.exports = romanTime;
 
-console.info(romanTime('09:10'));
-
-// Выведет 'N:N'
-console.info(romanTime('00:00'));
-
-// Выведет 'XXIII:LIX'
-console.info(romanTime('23:59'));
-
-// Выбросится ошибка [TypeError: Неверное время]
-console.info(romanTime('24:00'));
