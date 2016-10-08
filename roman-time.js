40fa92e456982728178070cfe97aed8510343f33
Сокерин Тимофей <sokerin-timofey@yandex.ru>
2016-10-08 22:50:02 +0500

    Roman-time

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,33 @@
 'use strict';
 
-/**
- * @param {String} time – время в формате HH:MM (например, 09:05)
- * @returns {String} – время римскими цифрами (IX:V)
- */
-function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+function isTime( time ) {
+    var regexn = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+    if ( regexn.test( time ) ) return true;
+    else return false;
+}
+
+function translationFromArabicToRoman( number ) {
+    if ( !number ) return 'N';
+    var alphabet = { M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1 };
+    var i, rom = '';
+    for ( i in alphabet ) {
+        while ( number >= alphabet[i] ) {
+        rom = rom + i;
+        number = number - alphabet[i];
+    }
+  }
+  return rom;
+}
+
+function romanTime( time ) {
+    try {
+        if ( !isTime( time ) ) throw new TypeError( 'Неверное время!' );
+		time = time.split( ':' );
+        var hour = parseInt( time[0] ), minute = parseInt( time[1] );
+        return translationFromArabicToRoman( hour ) + ':' + translationFromArabicToRoman( minute );
+    } catch(e) {
+		return e;
+    }
 }
 
 module.exports = romanTime;
