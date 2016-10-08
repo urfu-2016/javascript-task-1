89a901759cd819814e616b80068f304db30d0c3c
KatyrevaElizaveta <alchimia@ro.ru>
2016-10-08 16:51:42 +0600

    Undefined and null handling

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,4 @@
-﻿'use strict';
+﻿"use strict";
 
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
@@ -6,26 +6,40 @@
  */
 function romanTime(time) {
 
-    var romanOnes = ["o", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    function deleteZeroesFromRomanTime(romanTimeString) {
+        romanTimeString = romanTimeString.replace(/oo/g, "N");
+        romanTimeString = romanTimeString.replace(/o/g, "");
+        return romanTimeString;
+    }
 
-    var romanTens = ["o", "X", "XX", "XXX", "XL", "L"];
+    function createRomanTimeString(hours, minutes) {
+        var romanOnes = ["o", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+        var romanTens = ["o", "X", "XX", "XXX", "XL", "L"];
+        var hoursOnes = hours % 10;
+        var minutesOnes = minutes % 10;
+        var romanTimeString = romanTens[(hours - hoursOnes) / 10] + romanOnes[hoursOnes] +
+                ":" + romanTens[(minutes - minutesOnes) / 10] + romanOnes[minutesOnes];
+        return romanTimeString;
+    }
 
-    var arrayOfDateDatas = time.split(":");
-    var hours = Number(arrayOfDateDatas[0]);
-    var minutes = Number(arrayOfDateDatas[1]);
+    var hours;
+    var minutes;
+    
+    try {
+        var arrayOfDateDatas = time.split(":");
+        hours = Number(arrayOfDateDatas[0]);
+        minutes = Number(arrayOfDateDatas[1]);
+    } catch (e) {
+        throw new TypeError();
+    }
 
     var romanTimeString;
 
     if (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59 &&
             hours % 1 === 0 && minutes % 1 === 0) {
 
-        var hoursOnes = hours % 10;
-        var minutesOnes = minutes % 10;
-
-        romanTimeString = romanTens[(hours - hoursOnes) / 10] + romanOnes[hoursOnes] +
-                ":" + romanTens[(minutes - minutesOnes) / 10] + romanOnes[minutesOnes];
-        romanTimeString = romanTimeString.replace(/oo/g, "N");
-        romanTimeString = romanTimeString.replace(/o/g, "");
+        romanTimeString = createRomanTimeString(hours, minutes);
+        romanTimeString = deleteZeroesFromRomanTime(romanTimeString);
 
     } else {
         throw new TypeError();
