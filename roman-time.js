57dfabd5cd988a8c45bd0e46aac386b5e0d17f02
Nikolai Bravilov <bravilov@mail.ru>
2016-10-08 20:26:59 +0500

    Каркас программы

--- roman-time.js
+++ roman-time.js
@@ -5,8 +5,21 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    var parsedInput = tryParseInput(time);
+    var hours = parsedInput[0];
+    var minutes = parsedInput[1];
+    return intToRomanNumber(hours) + " " + intToRomanNumber(minutes);
+}
+
+function tryParseInput(time) {
+    var hours = 0;
+    var minutes = 0;
+    return [hours, minutes];
+}
+
+function intToRomanNumber(number) {
+    var result = "";
+    return result;
 }
 
 module.exports = romanTime;
