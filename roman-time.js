589f8d2f15041579f6e06301e42e6320c8ee85c7
Maxim <endurancemayer@yandex.ru>
2016-10-08 21:00:14 +0500

    Simplifed OP, because of style critical error, why??

--- roman-time.js
+++ roman-time.js
@@ -1,49 +1,6 @@
 'use strict';
 
-function romanObject(starterString){
-	try{
-		var value = starterString.split(":");
-		var probableHours = parseInt(value[0]);
-		var probableMinutes = parseInt(value[1]);
-	}
-	catch (e) {
-		throw new TypeError("Incorrect time");
-	}
-	
-	this.hours = checkTime(probableHours, "hours");
-	this.minutes = checkTime(probableMinutes, "minutes");
-	this.romanAnalogues = {1: "I", 4: "IV",5:"V", 9: "IX", 10:"X", 40: "XL", 50:"L" };
-	
-	this.getMaximumInsideDict = function getMaximumInsideDict(elem){
- 	var difference = Number.MAX_SAFE_INTEGER;
- 	var searchFor;
- 	var keys = Object.keys(this.romanAnalogues);
- 	
- 	for (var i =0; i < keys.length; i++){
- 		var curDiff = elem - keys[i];
- 		if (curDiff < difference && curDiff >= 0)
- 		{
- 			difference = curDiff;
- 			searchFor = keys[i];
-
- 		}
-	}
-	return searchFor;
- 	}
-
-	this.convertArabicToRoman = function convertArabicToRoman(measure){
- 	if (measure == 0)
- 		return "N";
- 	var number = this.getMaximumInsideDict(measure);
- 	if (number == measure)
- 		return this.romanAnalogues[number];
- 	return this.romanAnalogues[number] + this.convertArabicToRoman(measure - number); 
-	}
-
-	this.returnRomanResult = function returnRomanResult() {
-	return this.convertArabicToRoman(this.hours) + ":" + this.convertArabicToRoman(this.minutes);
-	}
-}
+var romanAnalogues = {1: "I", 4: "IV",5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L" };
 
 function checkTime(time, tag) {
 	var result = parseInt(time, 10);
@@ -57,15 +14,54 @@ function checkTime(time, tag) {
 	return result;
 }
  
- 
+function getMaximumInsideDict(elem) {
+ 	var difference = Number.MAX_SAFE_INTEGER;
+ 	var searchFor;
+ 	var keys = Object.keys(romanAnalogues);
+ 	
+ 	for (var i =0; i < keys.length; i++) {
+ 		var curDiff = elem - keys[i];
+ 		if (curDiff < difference && curDiff >= 0)
+ 		{
+ 			difference = curDiff;
+ 			searchFor = keys[i];
+
+ 		}
+	}
+	return searchFor;
+}
+
+function convertArabicToRoman(measure) {
+	if (measure == 0)
+ 		return "N";
+ 	var number = getMaximumInsideDict(measure);
+ 	if (number == measure)
+ 		return romanAnalogues[number];
+
+ 	return romanAnalogues[number] + convertArabicToRoman(measure - number); 
+	}
+
+function returnRomanResult(hours, minutes) {
+	return convertArabicToRoman(hours) + ":" + convertArabicToRoman(minutes);
+}
+
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    var convertedTime = new romanObject(time);
-	return convertedTime.returnRomanResult();
+    try {
+		var value = time.split(":");
+		var hours = checkTime(parseInt(value[0]), "hours");
+		var minutes = checkTime(parseInt(value[1]), "minutes");
+		
+		return returnRomanResult(hours, minutes);
+	}
+
+	catch (e) {
+		throw new TypeError("Incorrect time");
+	}
+
 }
 
 module.exports = romanTime;
