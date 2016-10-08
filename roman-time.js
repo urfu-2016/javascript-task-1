1d6869dcd8b688dd0f50d5f970efddeeb5c11c6d
KrazyD <Svalovdmitrii@mail.ru>
2016-10-09 03:43:36 +0500

    Правки

--- roman-time.js
+++ roman-time.js
@@ -9,36 +9,28 @@ function romanTime(time) {
     var splitTime = time.split(':', 2);
     var hours = parseInt(splitTime[0]);
     var minutes = parseInt(splitTime[1]);
-    if (!isNaN(hours) && !isNaN(minutes)) {
-        try {
-            if (hours > 23 || minutes > 59 || hours < 0 || minutes < 0) {
-                throw new TypeError('Неверный формат времени (HH:MM)', 'roman-time.js');
-            }
-        } catch (e) {
-            console.log(e.toString());
-            return '0';
+    try {
+        if (!isNaN(hours) && !isNaN(minutes) && (hours > 23 || minutes > 59 || hours < 0 || minutes < 0)) {
+            throw new TypeError('Неверный формат времени (HH:MM)', 'roman-time.js');
         }
-        var splitHours = hours.toString().split('');
-        var splitMinutes = minutes.toString().split('');
-        var decimalPart = ['N', 'X', 'XX', 'XXX', 'XL', 'L'];
-        var numberPart = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
-        if (hours < 10) {
-            if (minutes <= 10) {
-                time = numberPart[splitHours[0]] + ':' + numberPart[splitMinutes[0]];
-            }
-            else if (minutes > 10) {
-                time = numberPart[splitHours[0]] + ':' + decimalPart[splitMinutes[0]] + numberPart[splitMinutes[1]];
-            }
-        } else if (hours >= 10) {
-            if (minutes <= 10) {
-                time = decimalPart[splitHours[0]] + numberPart[splitHours[1]] + ':' + numberPart[splitMinutes[0]];
-            }
-            else if (minutes > 10) {
-                time = decimalPart[splitHours[0]] + numberPart[splitHours[1]] + ':' + decimalPart[splitMinutes[0]] + numberPart[splitMinutes[1]];
-            }
-        }
+    } catch (e) {
+
+        return '0';
     }
-    
+    var splitH = hours.toString().split('');
+    var splitM = minutes.toString().split('');
+    var decPart = ['N', 'X', 'XX', 'XXX', 'XL', 'L'];
+    var numPart = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    if (hours < 10 && minutes <= 10) {
+        time = numPart[splitH[0]]+':'+numPart[splitM[0]];
+    } else if (hours < 10 && minutes > 10) {
+        time = numPart[splitH[0]]+':'+decPart[splitM[0]]+numPart[splitM[1]];
+    } else if (hours >= 10 && minutes <= 10) {
+        time = decPart[splitH[0]]+numPart[splitH[1]]+':'+numPart[splitM[0]];
+    } else if (hours >= 10 && minutes > 10) {
+        time = decPart[splitH[0]]+numPart[splitH[1]]+':'+decPart[splitM[0]]+numPart[splitM[1]];
+    }
+
     return time;
 }
 
