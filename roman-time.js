573d644e58870777619abdeeca8c2292b4aba4b3
KrazyD <Svalovdmitrii@mail.ru>
2016-10-09 04:18:27 +0500

    Ещё правки

--- roman-time.js
+++ roman-time.js
@@ -8,27 +8,34 @@ function romanTime(time) {
     // Немного авторского кода и замечательной магии
     var splitTime = time.split(':', 2);
     var hours = parseInt(splitTime[0]);
-    var minutes = parseInt(splitTime[1]);
+    var mins = parseInt(splitTime[1]);
     try {
-        if (!isNaN(hours) && !isNaN(minutes) && (hours > 23 || minutes > 59 || hours < 0 || minutes < 0)) {
+        if (!isNaN(hours) && !isNaN(mins) && (hours > 23 || mins > 59 || hours < 0 || mins < 0)) {
             throw new TypeError('Неверный формат времени (HH:MM)', 'roman-time.js');
         }
     } catch (e) {
 
         return '0';
     }
+    time = conversionTime(hours, mins);
+
+    return time;
+}
+
+function conversionTime(hours, mins) {
+    var time = '';
     var splitH = hours.toString().split('');
-    var splitM = minutes.toString().split('');
-    var decPart = ['N', 'X', 'XX', 'XXX', 'XL', 'L'];
-    var numPart = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
-    if (hours < 10 && minutes <= 10) {
-        time = numPart[splitH[0]]+':'+numPart[splitM[0]];
-    } else if (hours < 10 && minutes > 10) {
-        time = numPart[splitH[0]]+':'+decPart[splitM[0]]+numPart[splitM[1]];
-    } else if (hours >= 10 && minutes <= 10) {
-        time = decPart[splitH[0]]+numPart[splitH[1]]+':'+numPart[splitM[0]];
-    } else if (hours >= 10 && minutes > 10) {
-        time = decPart[splitH[0]]+numPart[splitH[1]]+':'+decPart[splitM[0]]+numPart[splitM[1]];
+    var splitM = mins.toString().split('');
+    var decs = ['N', 'X', 'XX', 'XXX', 'XL', 'L'];
+    var nums = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    if (hours < 10 && mins <= 10) {
+        time = nums[splitH[0]] + ':' + nums[splitM[0]];
+    } else if (hours < 10 && mins > 10) {
+        time = nums[splitH[0]] + ':' + decs[splitM[0]] + nums[splitM[1]];
+    } else if (hours >= 10 && mins <= 10) {
+        time = decs[splitH[0]] + nums[splitH[1]] + ':' + nums[splitM[0]];
+    } else if (hours >= 10 && mins > 10) {
+        time = decs[splitH[0]] + nums[splitH[1]] + ':' + decs[splitM[0]] + nums[splitM[1]];
     }
 
     return time;
