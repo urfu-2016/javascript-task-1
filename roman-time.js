be142bdb45d7a645c87d18d6204502d9aa08415e
Дмитрий Козачук < >
2016-10-08 23:20:34 +0300

    first attempt

--- roman-time.js
+++ roman-time.js
@@ -4,9 +4,34 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+ 
+function transform(arabicTime) {
+    var i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    var x = ["", "X", "XX", "XXX", "XL", "L"];
+    var romanTime = '';
+    var n = Number(arabicTime);
+    if ((arabicTime == '0') || (arabicTime == '00')) {
+        return 'N';
+    }
+    else {
+        romanTime += x[Math.floor(n / 10)];
+        n %= 10;
+        romanTime += i[n];
+        return romanTime;
+    }
+    
 }
-
+function romanTime(time) {
+    var timeList = time.split(':');
+    //лучше проверять, что содержат ровно два символа, , а потом что находятся в диапазоне - чтобы избежать корректности '12:000005';
+    var isHours = (timeList[0].length == 2) & (timeList[0] <= 24) & (timeList[0] >= 0);
+    var isMinutes = (timeList[1].length == 2) & (timeList[1] <= 60) & (timeList[1] >= 0);
+    if  (isHours & isMinutes & isNaN(timeList[2])) {
+        return transform(timeList[0]) + ':' + transform(timeList[1])
+    }
+    else {
+        return new TypeError();
+    }
+}
 module.exports = romanTime;
+
