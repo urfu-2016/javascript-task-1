b8b455b7a00487ffcb124ced45976aa72b29b545
uliana1997 <blabla@bla.bla>
2016-10-10 01:52:28 +0600

    lint

--- roman-time.js
+++ roman-time.js
@@ -1,38 +1,34 @@
 'use strict';
 
-/**
- * @param {String} time – время в формате HH:MM (например, 09:05)
- * @returns {String} – время римскими цифрами (IX:V)
- */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
     var t = time.split(':');
 
-    if (isNaN(parseInt(t[0])) || isNaN(parseInt(t[1])) || t[0]>23 || t[1]>59 || t[0]<0 || t[1]<0){
-    	throw new TypeError('Неверный ввод!');
+    if (isNaN(Number(t[0])) || isNaN(Number(t[1])) || t[0] > 23 || t[1] > 59 || t[0] < 0 ||
+     t[1] < 0) {
+        throw new TypeError('Неверный ввод!');
     }
-    else{
-    	var hh = parseInt(t[0]);
-    	var mm = parseInt(t[1]);
-    	var hours = convert(hh);
-    	var min = convert(mm);
-    	if(isNaN(hours) ==  false){hours = "N"};
-    	if(isNaN(min) == false){min = "N"};
-        time = hours + ':' + min;
-    
+    else {
+        var hh = parseInt(t[0]);
+        var mm = parseInt(t[1]);
+        var hours = convert(hh);
+        var min = convert(mm);
+        if (isNaN(hours) === false) { hours = "N"; }
+        if (isNaN(min) === false) { min = "N"; }
     }
- return time;
+
+    return time;
 }
 
 function convert(num) {
-    var lookup = {L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}, roman = '', i;
-    for ( i in lookup ) {
-        while ( num >= lookup[i] ) {
+    var lookup = { L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+    var roman = '';
+    for (var i in lookup) {
+        while (num >= lookup[i]) {
             roman += i;
             num -= lookup[i];
         }
     }
-    
+
     return roman;
 }
 
