a1bd559afb24d44631c6794be2b7a9a8c42954cf
Миннибаева Генриетта <henryminnib@gmail.com>
2016-10-10 00:03:22 +0500

    пробелы и пр 3

--- roman-time.js
+++ roman-time.js
@@ -1,16 +1,16 @@
 'use strict';
 function convertToRomanNum(num) {
-    var rN = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']; 
+    var rN = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
     var rNty = ['XX', 'XXX', 'XL', 'L'];
     if (num < 11) {
-    num = rN[num];
+        num = rN[num];
 }
     else {
-    var tempNum = rNty[(num - num % 10) / 10 - 2];
-    if (num % 10 != 0) {
-    num = tempNum + rN[num % 10];
-}
-}
+        var tempNum = rNty[(num - num % 10) / 10 - 2];
+        if (num % 10 !== 0) {
+        num = tempNum + rN[num % 10];
+    }
+    }
 
     return num;
 }
@@ -23,13 +23,13 @@ function romanTime(time) {
     var arrHoursMin = time.split(':');
     var hours = parseInt(arrHoursMin[0], 10);
     var min = parseInt(arrHoursMin[1], 10);
-	if (hours < 24 && min < 60) {
+    if (hours < 24 && min < 60) {
     hours = convertToRomanNum(hours);
     min = convertToRomanNum(min);
     time = hours + ':' + min;
 	
     return time;
-	}
+    }
     throw new TypeError('Неверное время');
 }
 
