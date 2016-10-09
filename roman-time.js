a3de01bde521a38e270ac15d0ab97da429656bd1
Chernykh Artyom <chernykhand@mail.ru>
2016-10-10 02:01:11 +0500

    fix sintax

--- roman-time.js
+++ roman-time.js
@@ -1,22 +1,23 @@
 'use strict';
-var firstDigit = new Array("","X", "XX", "XXX", "XL", "L");
-var secondDigit = new Array("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX");
+var firstDigit = ["", "X", "XX", "XXX", "XL", "L"];
+var secondDigit = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
- 
 
-function arabToRoman(number){
-    if (number === 0){
+
+function arabToRoman(number) {
+    if (number === 0) {
         return "N";
     }
-    var fDigit = parseInt(number/10);
-    var sDigit = parseInt(number%10);
+    var fDigit = parseInt(number / 10);
+    var sDigit = parseInt(number % 10);
+    
     return firstDigit[fDigit] + secondDigit[sDigit];
 } 
- 
+
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
     var splitTime = time.split(":");
