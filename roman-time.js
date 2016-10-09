4b89a05f6ab2007cccbb2fd5d7c910e406ddb8b5
Миннибаева Генриетта <henryminnib@gmail.com>
2016-10-09 22:27:55 +0500

    пробелы и пр

--- roman-time.js
+++ roman-time.js
@@ -1,13 +1,15 @@
 'use strict';
 function ConvertToRomanNum(num){
-	var romanNum = ['N','I','II','III','IV','V','VI','VII','VIII','IX', 'X', 'XX','XXX','XL','L'];
-	if(num < 11){
-	num  = romanNum[num];
+	var romanNum = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XX', 'XXX', 'XL', 'L'];
+	if (num < 11) {
+		num  = romanNum[num];
 	}
-	else if(num % 10 == 0){
-			num = romanNum[((num-num%10)/10+9)]; 
-		 }
-		else { num = romanNum[((num-num%10)/10+9)]+ romanNum[num%10];}
+	else if (num % 10 === 0) {
+			num = romanNum[((num - num % 10) / 10 + 9)]; 
+		}
+		else { 
+			num = romanNum[((num - num % 10) / 10 + 9)] + romanNum[num % 10];
+			}
 	return num;
 }
 /**
@@ -18,11 +20,11 @@ function romanTime(time) {
     var arrHoursMin= time.split(':');
 	var hours = parseInt(arrHoursMin[0], 10);
 	var min = parseInt(arrHoursMin[1], 10);
-	if(hours >= 0 && hours < 24 && min >= 0 && min < 60){
-	hours = ConvertToRomanNum(hours);
-	min = ConvertToRomanNum(min);
-	time = hours +':'+ min;
-    return time;
+	if(hours < 24 && min < 60) {
+		hours = ConvertToRomanNum(hours);
+		min = ConvertToRomanNum(min);
+		time = hours + ':' + min;
+		return time;
 	}
 	throw new TypeError('Неверное время');
 }
