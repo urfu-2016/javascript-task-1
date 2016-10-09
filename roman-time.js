16aa7e5127ca2d1bb229f547e4cefa1b16faa885
Chernykh Artyom <chernykhand@mail.ru>
2016-10-10 02:19:34 +0500

    again complexity

--- roman-time.js
+++ roman-time.js
@@ -22,7 +22,7 @@ function romanTime(time) {
     // Немного авторского кода и замечательной магии
     var splitTime = time.split(":");
     var hourMinute = [Number(splitTime[0]), Number(splitTime[1])];
-    if(hourMinute[0] < 0 || hourMinute[1] < 0 || hourMinute[0] > 23 || hourMinute[1] > 59){
+    if(hourMinute[0] > 23 || hourMinute[1] > 59){
         throw new TypeError("Bad format");
     } 
     if (isNaN(hourMinute[0]) || isNaN(hourMinute[1])){
