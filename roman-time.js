9ee264af47290a8360b11c8fff2e41f9ce3f38d8
Миннибаева Генриетта <henryminnib@gmail.com>
2016-10-10 01:21:40 +0500

    проверка на длину подстрок

--- roman-time.js
+++ roman-time.js
@@ -1,11 +1,11 @@
 'use strict';
 function convertToRomanNum(num) {
     var rN = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
-    var rNty = ['XX', 'XXX', 'XL', 'L'];
+    var rNty = ['X', 'XX', 'XXX', 'XL', 'L'];
     if (num < 11) {
         num = rN[num];
     } else {
-        var tempNum = rNty[(num - num % 10) / 10 - 2];
+        var tempNum = rNty[(num - num % 10) / 10 - 1];
         if (num % 10 !== 0) {
             num = tempNum + rN[num % 10];
         }
@@ -22,7 +22,7 @@ function romanTime(time) {
     var arrHoursMin = time.split(':');
     var hours = parseInt(arrHoursMin[0], 10);
     var min = parseInt(arrHoursMin[1], 10);
-    if (hours < 24 && min < 60) {
+    if (hours < 24 && min < 60 && arrHoursMin[0].length === 2 && arrHoursMin[1].length === 2) {
         hours = convertToRomanNum(hours);
         min = convertToRomanNum(min);
         time = hours + ':' + min;
