fde243cad82131481c51f9b809053a3026e1ad2e
Matvey <xaxalox2@gmail.com>
2016-10-09 22:30:53 +0500

    Fix errors 2

--- roman-time.js
+++ roman-time.js
@@ -5,42 +5,38 @@
 
 'use strict';
 
+function convertToRoman(num1, num2, time) {
+    if (time === 0) {
+        return 'N';
+    } else {
+        var counter = 0;
+        while (time > 10) {
+            time -= 10;
+            counter++;
+        }
+        if (counter === 0) {
+            return time = num1[time];
+        } else {
+            return time = num2[counter] + num1[time];
+        }
+    }
+}
 
 function romanTime(time) {
     var tempTime = time.split(':');
-    if (!(tempTime[0] < 0 || tempTime[0] > 23 || tempTime[1] < 0 || tempTime[1] > 59)) {
-        var firstRomanNumbers = { 0: 'N', 1: 'I', 2: 'II', 3: 'III', 4: 'IV',
-                                 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX', 10: 'X' };
-        if (tempTime[0] <= 10) {
-            tempTime[0] = firstRomanNumbers[tempTime[0]];
-        }
-        if (tempTime[0] <= 20) {
-            temptime[0] = 'X' + firstRomanNumbers[tempTime[0] - 10];
-        } else {
-            tempTime[0] = 'XX' + firstRomanNumbers[tempTime[0] - 20];
-        }
-        if (tempTime[1] <= 10) {
-            tempTime[1] = firstRomanNumbers[tempTime[0]];
-        }
-        if (tempTime[1] <= 20) {
-            tempTime[1] = 'X' + firstRomanNumbers[tempTime[0] - 10];
-        }
-        if (tempTime[1] <= 30) {
-            tempTime[1] = 'XX' + firstRomanNumbers[tempTime[0] - 20];
-        }
-        if (tempTime[1] <= 40) {
-            tempTime[1] = 'XXX' + firstRomanNumbers[tempTime[0] - 30];
-        }
-        if (tempTime[1] <= 50) {
-            tempTime[1] = 'XL' + firstRomanNumbers[tempTime[0] - 40];
-        } else {
-            tempTime[1] = 'L' + firstRomanNumbers[tempTime[0] - 50];
-        }
-    }
-    else {
+    tempTime[0] = parseInt(tempTime[0], 10);
+    tempTime[1] = parseInt(tempTime[1], 10);
+    var firstRomanNumbers = { 0: 'N', 1: 'I', 2: 'II', 3: 'III', 4: 'IV',
+        5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX' };
+    var secondRomanNumbers = { 1: 'X', 2: 'XX', 3: 'XXX', 4: 'XL', 5: 'L' };
+    if (tempTime[0] > 0 && tempTime[0] < 23 && tempTime[1] > 0 && tempTime[1] < 59) {
+        tempTime[0] = convertToRoman(firstRomanNumbersm secondRomanNumbers, tempTime[0]);
+        tempTime[1] = convertToRoman(firstRomanNumbersm secondRomanNumbers, tempTime[1]);
+    } else {
         throw new TypeError('Неверный формат времени');
     }
     time = tempTime.join(':');
+
     return time;
 }
 
