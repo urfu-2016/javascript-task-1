6aae89f5f812d4690b47de4a605e985850df6709
imatasov <ivan.matasov@yandex.ru>
2016-10-10 00:48:37 +0500

    Убраны некоторые недочеты

--- roman-time.js
+++ roman-time.js
@@ -1,23 +1,24 @@
 'use strict';
 
-var romanNumber = {
-        0: 'N',
-        1: 'I',
-        2: 'II',
-        3: 'III',
-        4: 'IV',
-        5: 'V',
-        6: 'VI',
-        7: 'VII',
-        8: 'IIX',
-        9: 'IX',
-        10: 'X',
-        20: 'XX',
-        30: 'XXX',
-        40: 'XL',
-        50: 'L'
+var romanNumer = {
+    0: 'N',
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'IIX',
+    9: 'IX',
+    10: 'X',
+    20: 'XX',
+    30: 'XXX',
+    40: 'XL',
+    50: 'L'
     };
 
+// Разделяем время на часы и минуты
 function splitStringTime(stringTime) {
 
     var splitTimeResult = [];
@@ -33,22 +34,23 @@ function splitStringTime(stringTime) {
         return splitTimeResult;
 }
 
+// Ковертиуем и 'склеиваем' время
 function glueTime(dH, uH, dM, uM) {
 
-        var romanResult;
+    var romanResult;
 
-        if ((dH === 0) && (uM === 0)) {
+    if ((dH === 0) && (uM === 0)) {
 
-            romanResult = romanNumber[uH] + ":" + romanNumber[dM];
+        romanResult = romanNumer[uH] + ":" + romanNumer[dM];
 
-        } else {
+    } else {
 
-            romanResult = romanNumber[dH] + romanNumber[uH] + ":" + romanNumber[dM] + romanNumber[uM];
+        romanResult = romanNumer[dH] + romanNumer[uH] + ":" + romanNumer[dM] + romanNumer[uM];
 
-        }
-
-        return romanResult;
     }
+
+    return romanResult;
+}
 
 function checkTimeOnNaN(hrs, min) {
 
@@ -64,7 +66,7 @@ function checkTimeOnCorrect(hrs, min) {
     var hours = parseInt(hrs, 10);
     var minutes = parseInt(min, 10);
 
-    return hours >= 0 && hours < 24 && minutes >=0 && minutes < 60;
+    return hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60;
 
 }
 
@@ -81,7 +83,7 @@ function checkTimeOnUndf(time) {
 
 function checkTimeOnNull(time) {
 
-    return typeof time === null;
+    return time === null;
 }
 
 /**
@@ -90,59 +92,41 @@ function checkTimeOnNull(time) {
  */
 function romanTime(time) {
 
-    try {
-
     var splitDate = time.split(':');
     var getHour = splitDate[0];
     var getMinutes = splitDate[1];
-    var ERROR_MESSAGE = 'Incorrect time!';
+    var ERROR_MESSAGE = 'Incorrect time format!';
 
-        if (!checkTimeOnType(time)) {
+    if (!checkTimeOnType(time)) {
 
-            throw new TypeError(ERROR_MESSAGE);
+        throw new TypeError(ERROR_MESSAGE);
 
-        }
+    }
 
-        if (!checkTimeOnCorrect(getHour, getMinutes)) {
+    if (!checkTimeOnCorrect(getHour, getMinutes) || checkTimeOnNull(time)) {
 
-            throw new TypeError(ERROR_MESSAGE);
+        throw new TypeError(ERROR_MESSAGE);
 
-        }
+    }
 
-        if (checkTimeOnUndf(time)) {
+    if (checkTimeOnUndf(time) || checkTimeOnNaN(getHour, getMinutes)) {
 
-            throw new TypeError(ERROR_MESSAGE);
+        throw new TypeError(ERROR_MESSAGE);
 
-        }
-
-        if (checkTimeOnNaN(getHour, getMinutes)) {
-
-            throw new TypeError(ERROR_MESSAGE);
-
-        }
-
-        if (checkTimeOnNull(time)) {
-
-            throw new TypeError(ERROR_MESSAGE);
-
-        }
-
+    }
+        //console.log(checkTimeOnNull(time));
 
     var HH = splitStringTime(getHour);
-    var dozensHours = parseInt(HH[0] * 10, 10);
-    var unitHours = parseInt(HH[1]);
+    var dozensHours = parseInt(HH[0] * 10, 10); // Десятки в часах
+    var unitHours = parseInt(HH[1]); // Единицы в часах
 
     var MM = splitStringTime(getMinutes);
-    var dozensMinutes = parseInt(MM[0] * 10, 10);
-    var unitMinutes = parseInt(MM[1]);
+    var dozensMinutes = parseInt(MM[0] * 10, 10); // Десятки в минутах
+    var unitMinutes = parseInt(MM[1]); // Единицы в минутах
 
     time = glueTime(dozensHours, unitHours, dozensMinutes, unitMinutes);
 
     return time;
-
-    } catch (e) {
-
-    }
 
 }
 
