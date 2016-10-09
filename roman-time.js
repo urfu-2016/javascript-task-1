191846e9c9fa455e9bd8657adba374a463c0a297
chrighter <christina.ilyenko@yandex.ru>
2016-10-09 22:13:38 +0600

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -5,8 +5,8 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 var arabic = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60];
-var roman = ["N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", 
-             "IX", "X", "XX", "XXX", "XL", "L", "LX"];
+var roman = ["N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII",
+        "IX", "X", "XX", "XXX", "XL", "L", "LX"];
 
 var hour;
 var minute;
@@ -18,8 +18,8 @@ function isCorrect(hour, minute) {
     if (hour.length > 2 || minute.length > 2) {
         return false;
     }
-    if (parseInt(hour, 10) > 23 || parseInt(hour, 10) < 0 
-        || parseInt(minute, 10) > 59 || parseInt(minute, 10) < 0) {
+    if (parseInt(hour, 10) > 23 || parseInt(hour, 10) < 0 ||
+            parseInt(minute, 10) > 59 || parseInt(minute, 10) < 0) {
         return false;
     }
 
@@ -30,15 +30,15 @@ function translate(number) {
     var i = 0;
     if (number === 0) {
         romanNumber += "N";
+
         return romanNumber;
     }
+
     for (i = 0; i < arabic.length; i += 1) {
         if (number < arabic[i]) {
             number -= arabic[i - 1];
             romanNumber += roman[i - 1];
             if (number !== 0) {
-                //return romanNumber;
-            //} else {
                 i = 0;
                 translate(number);
             }
