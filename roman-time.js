941061a15745c661ee9140efd37ed3020d611880
ADruzhinin <mcfriend@yandex.ru>
2016-10-09 16:37:44 +0500

    First try

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,42 @@
 'use strict';
 
-/**
- * @param {String} time – время в формате HH:MM (например, 09:05)
- * @returns {String} – время римскими цифрами (IX:V)
- */
-function romanTime(time) {
-    // Немного авторского кода и замечательной магии
+var romanNumbers = ["N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+var romanDecade = ["X", "XX", "XXX", "XL", "L"];
+
+function getRomanType(number) {
+    var decade = (number - number % 10) / 10;
+    var units = number % 10;
+    var time = "";
+    if (number === 0) {
+        time += "N";
+
+        return time;
+    }
+    if (decade > 0) {
+        time += romanDecade[decade - 1];
+    }
+    if (units > 0) {
+        time += romanNumbers[units];
+    }
+
     return time;
 }
 
+function romanTime(time) {
+    var numbers = time.split(":");
+    var hours = 0;
+    var minutes = 0;
+    try {
+        hours = parseInt(numbers[0]);
+        minutes = parseInt(numbers[1]);
+    } catch (e) {
+        console.error(e.message);
+    }
+    if (hours >= 24 || hours < 0 || minutes >= 60 || minutes < 0) {
+        throw new TypeError('Неверное время');
+    }
+    time = getRomanType(hours) + ":" + getRomanType(minutes);
+
+    return time;
+}
 module.exports = romanTime;
