e5388ec477f4ea07fa3a250ce7651a29f60ff0b3
NovoselovaNatalia <zaika0206_95@mail.ru>
2016-10-09 17:04:18 +0500

    исправление №2

--- roman-time.js
+++ roman-time.js
@@ -1,56 +1,19 @@
 'use strict';
 
 /**
- * @param {String} romTime – арабское число
+ * @param {String} arabTime – арабское число
  * @returns {String} – число римскими цифрами
  */
-function tenDigits(romTime) {
-    if (parseInt(romTime, 10) !== 0) {
-        switch (Math.floor(romTime / 10)) {
-            case 0:
-                return '';
-            case 1:
-                return 'X';
-            case 2:
-                return 'XX';
-            case 3:
-                return 'XXX';
-            case 4:
-                return 'XL';
-            case 5:
-                return 'L';
-        }
-    } else {
+function arabToRoman(arabTime) {
+    var tenDigits = ["", "X", "XX", "XXX", "XL", "L"];
+    var oneDigits = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    if (parseInt(arabTime, 10) === 0) {
         return 'N';
     }
-}
-function oneDigits(romTime) {
-    if (parseInt(romTime, 10) !== 0) {
-        switch (romTime % 10) {
-            case 0:
-                return '';
-            case 1:
-                return 'I';
-            case 2:
-                return 'II';
-            case 3:
-                return 'III';
-            case 4:
-                return 'IV';
-            case 5:
-                return 'V';
-            case 6:
-                return 'VI';
-            case 7:
-                return 'VII';
-            case 8:
-                return 'VIII';
-            case 9:
-                return 'IX';
-        }
-    } else {
-        return '';
-    }
+    var res = tenDigits[Math.floor(arabTime / 10)];
+    res = res + oneDigits[arabTime % 10];
+
+    return res;
 }
 
 /**
@@ -58,15 +21,12 @@ function oneDigits(romTime) {
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    var timeArr = time.split(':');
-    if (timeArr.length <= 2 && time.length <= 5) {
-        var hours = timeArr[0];
-        var minutes = timeArr[1];
-    }
-    if (hours < 24 && minutes < 60 && hours >= 0 && minutes >= 0) {
-        return tenDigits(hours) + oneDigits(hours) + ':' + tenDigits(minutes) + oneDigits(minutes);
+    var tArr = time.split(':');
+    if (tArr.length <= 2 && time.length <= 5) {
+        if (tArr[0] < 24 && tArr[1] < 60 && tArr[0] >= 0 && tArr[1] >= 0) {
+            return arabToRoman(tArr[0]) + ':' + arabToRoman(tArr[1]);
+        }
     }
     throw new TypeError('TypeError');
 }
-
 module.exports = romanTime;
