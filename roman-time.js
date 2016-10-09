20aa19373b96f0ee8054b9032e5952d6d9976013
kmn75 <sokerin-timofey@yandex.ru>
2016-10-09 11:10:04 +0500

    fix2

--- roman-time.js
+++ roman-time.js
@@ -12,15 +12,18 @@ function translationFromArabicToRoman(number) {
         return 'N';
     }
     var alphabet = { 'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90,
-        'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1 },
-    i,
-    rom = '';
-    for ( i in alphabet ) {
-        while (number >= alphabet[i]) {
+        'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1 };
+    var i;
+    var rom = '';
+    for (i in alphabet) {
+        if ({}.hasOwnProperty.call(alphabet, i)) {
+            while (number >= alphabet[i]) {
             rom = rom + i;
             number = number - alphabet[i];
         }
+        }
     }
+
     return rom;
 }
 
@@ -30,11 +33,11 @@ function romanTime(time) {
             throw new TypeError('Неверное время!');
         }
         time = time.split(':');
-        var hour = parseInt(time[0]),
-        minute = parseInt(time[1]);
+        var hour = parseInt(time[0]);
+        var minute = parseInt(time[1]);
+
         return translationFromArabicToRoman(hour) + ':' + translationFromArabicToRoman(minute);
-    }
-    catch (e) {
+    } catch (e) {
         return e;
     }
 }
