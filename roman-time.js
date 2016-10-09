934a45b7b93af406abecd54335acfd634a37bc8e
kmn75 <sokerin-timofey@yandex.ru>
2016-10-09 10:11:05 +0500

    fixx

--- roman-time.js
+++ roman-time.js
@@ -5,36 +5,36 @@ function isTime(time) {
     if (regexn.test(time)) {
         return true;
     }
-    else {
-        return false;
-    }
 }
 
 function translationFromArabicToRoman(number) {
     if (!number) {
         return 'N';
     }
-    var alphabet = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 
-    'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1 },
+    var alphabet = { 'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90,
+        'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1 },
     i,
     rom = '';
-    for (i in alphabet) {
+    for ( i in alphabet ) {
         while (number >= alphabet[i]) {
             rom = rom + i;
             number = number - alphabet[i];
+        }
     }
-  }
-  return rom;
+    return rom;
 }
 
 function romanTime(time) {
     try {
-        if (!isTime(time)) throw new TypeError('Неверное время!');
-		time = time.split(':');
-        var hour = parseInt(time[0]), minute = parseInt(time[1]);
+        if (!isTime(time)) {
+            throw new TypeError('Неверное время!');
+        }
+        time = time.split(':');
+        var hour = parseInt(time[0]),
+        minute = parseInt(time[1]);
         return translationFromArabicToRoman(hour) + ':' + translationFromArabicToRoman(minute);
     }
-    catch(e) {
+    catch (e) {
         return e;
     }
 }
