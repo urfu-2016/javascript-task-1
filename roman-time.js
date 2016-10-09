e34bae4a0ba10ea8f2bb808bfba80fe0ab6b4550
imatasov <ivan.matasov@yandex.ru>
2016-10-09 20:05:54 +0500

    Исправлены стилевый ошибки

--- roman-time.js
+++ roman-time.js
@@ -1,17 +1,6 @@
 'use strict';
 
-/**
- * @param {String} time – время в формате HH:MM (например, 09:05)
- * @returns {String} – время римскими цифрами (IX:V)
- */
-function romanTime(time) {
-
-    var splitDate   = time.split(':');
-    var getHour     = splitDate[0];
-    var getMinutes  = splitDate[1];
-
-    //console.info(getHour);
-    var romanNumber = {
+var romanNumber = {
         0: 'N',
         1: 'I',
         2: 'II',
@@ -29,56 +18,132 @@ function romanTime(time) {
         50: 'L'
     };
 
-    try{
-        if(isNaN(getHour) && isNaN(getMinutes))  throw new TypeError('Неверный форма времени! Строка');
-        if(getHour > 23)                         throw new TypeError('Нверный формат времени! Неверное время');
-        if((splitDate[0].length + splitDate[1].length) > 4) throw new TypeError('Неверный формат времени! time.length > 4');
-        if(getHour === undefined)                throw new TypeError('Неверный формат времени! hh is undefined');
-        if(getMinutes === undefined)             throw new TypeError('Неверный формат времени! mm is undefined');
+function splitStringTime(stringTime) {
 
-    //Разбиваем отдельно строку на минуты и часы
-    function splitStringTime(stringTime){
-        var splitTimeResult = [];
+    var splitTimeResult = [];
 
-        for(var i = 0; i < stringTime.length; i++) {
-            var splitHour = stringTime.charAt(i);
-            splitTimeResult.push(splitHour);
-        }
+    for (var i = 0; i < stringTime.length; i++) {
+
+        var splitHour = stringTime.charAt(i);
+
+        splitTimeResult.push(splitHour);
+
+    }
 
         return splitTimeResult;
-    }
+}
 
-    var HH = splitStringTime(getHour); // Часы
-    var dozensHours  = parseInt(HH[0] * 10, 10); //Выделяем десятки в часах *h:mm
-    var unitHours = parseInt(HH[1]); //Выделяем единицы в часах h*:mm
+function glueTime(dH, uH, dM, uM) {
 
-    var MM = splitStringTime(getMinutes); // Минуты hh:**
-    var dozensMinutes = parseInt(MM[0] * 10, 10); //Выделяем десятки в минутах hh:*m
-    var unitMinutes = parseInt(MM[1]); // Выделяем единицы в минутых hh:m*
+        var romanResult;
 
-    //Склеиваем время
-    function glueTime(dH, uH, dM, uM ) {
+        if ((dH === 0) && (uM === 0)) {
 
-        if((dH == 0 ) && (uM == 0)) {
+            romanResult = romanNumber[uH] + ":" + romanNumber[dM];
 
-            var roman =romanNumber[uH] + ":" + romanNumber[dM];
-        }
-        else {
+        } else {
 
-            var roman = romanNumber[dH] + romanNumber[uH] + ":" + romanNumber[dM] + romanNumber[uM];
+            romanResult = romanNumber[dH] + romanNumber[uH] + ":" + romanNumber[dM] + romanNumber[uM];
+
         }
 
-        return roman;
+        return romanResult;
     }
 
-    var time = glueTime(dozensHours, unitHours, dozensMinutes, unitMinutes); //Результат
+function checkTimeOnNaN(hrs, min) {
+
+    var hours = parseInt(hrs, 10);
+    var minutes = parseInt(min, 10);
+
+    return isNaN(hours) || isNaN(minutes);
+
+}
+
+function checkTimeOnCorrect(hrs, min) {
+
+    var hours = parseInt(hrs, 10);
+    var minutes = parseInt(min, 10);
+
+    return hours >= 0 && hours < 24 && minutes >=0 && minutes < 60;
+
+}
+
+function checkTimeOnType(time) {
+
+    return typeof time === 'string';
+
+}
+
+function checkTimeOnUndf(time) {
+
+    return typeof time === undefined;
+}
+
+function checkTimeOnNull(time) {
+
+    return typeof time === null;
+}
+
+/**
+ * @param {String} time – время в формате HH:MM (например, 09:05)
+ * @returns {String} – время римскими цифрами (IX:V)
+ */
+function romanTime(time) {
+
+    try {
+
+    var splitDate = time.split(':');
+    var getHour = splitDate[0];
+    var getMinutes = splitDate[1];
+    var ERROR_MESSAGE = 'Incorrect time!';
+
+        if (!checkTimeOnType(time)) {
+
+            throw new TypeError(ERROR_MESSAGE);
+
+        }
+
+        if (!checkTimeOnCorrect(getHour, getMinutes)) {
+
+            throw new TypeError(ERROR_MESSAGE);
+
+        }
+
+        if (checkTimeOnUndf(time)) {
+
+            throw new TypeError(ERROR_MESSAGE);
+
+        }
+
+        if (checkTimeOnNaN(getHour, getMinutes)) {
+
+            throw new TypeError(ERROR_MESSAGE);
+
+        }
+
+        if (checkTimeOnNull(time)) {
+
+            throw new TypeError(ERROR_MESSAGE);
+
+        }
+
+
+    var HH = splitStringTime(getHour);
+    var dozensHours = parseInt(HH[0] * 10, 10);
+    var unitHours = parseInt(HH[1]);
+
+    var MM = splitStringTime(getMinutes);
+    var dozensMinutes = parseInt(MM[0] * 10, 10);
+    var unitMinutes = parseInt(MM[1]);
+
+    time = glueTime(dozensHours, unitHours, dozensMinutes, unitMinutes);
 
     return time;
 
-    } catch(e) {
-            console.log(e);
-        }
+    } catch (e) {
+
+    }
+
 }
 
 module.exports = romanTime;
-
