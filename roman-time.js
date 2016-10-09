4a3d048e80a3425efddcb80021a86683f0703712
Matvey <xaxalox2@gmail.com>
2016-10-09 21:19:42 +0500

    Fix errors

--- roman-time.js
+++ roman-time.js
@@ -9,44 +9,32 @@
 function romanTime(time) {
     var tempTime = time.split(':');
     if (!(tempTime[0] < 0 || tempTime[0] > 23 || tempTime[1] < 0 || tempTime[1] > 59)) {
-        var firstRomanNumbers = {0:'N', 1:'I', 2:'II', 3:'III', 4:'IV',
-                                 5:'V', 6:'VI', 7:'VII', 8:'VIII', 9:'IX', 10:'X'};
-        if(tempTime[0] <= 10) {
+        var firstRomanNumbers = { 0: 'N', 1: 'I', 2: 'II', 3: 'III', 4: 'IV',
+                                 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX', 10: 'X' };
+        if (tempTime[0] <= 10) {
             tempTime[0] = firstRomanNumbers[tempTime[0]];
         }
-        else {
-            if(tempTime[0] <= 20) {
-                temptime[0] = 'X' + firstRomanNumbers[tempTime[0] - 10];
-            }
-            else {
-                    tempTime[0] = 'XX' + firstRomanNumbers[tempTime[0] - 20];
-            }
+        if (tempTime[0] <= 20) {
+            temptime[0] = 'X' + firstRomanNumbers[tempTime[0] - 10];
+        } else {
+            tempTime[0] = 'XX' + firstRomanNumbers[tempTime[0] - 20];
         }
-        if(tempTime[1] <= 10) {
+        if (tempTime[1] <= 10) {
             tempTime[1] = firstRomanNumbers[tempTime[0]];
         }
-        else {
-            if(tempTime[1] <= 20) {
-                tempTime[1] = 'X' + firstRomanNumbers[tempTime[0] - 10];
-            }
-            else {
-                if(tempTime[1] <= 30) {
-                    tempTime[1] = 'XX' + firstRomanNumbers[tempTime[0] - 20];
-                }
-                else {
-                    if(tempTime[1] <= 40) {
-                        tempTime[1] = 'XXX' + firstRomanNumbers[tempTime[0] - 30];
-                    }
-                    else {
-                        if(tempTime[1] <= 50) {
-                            tempTime[1] = 'XL' + firstRomanNumbers[tempTime[0] - 40];
-                        }
-                        else {
-                            tempTime[1] = 'L' + firstRomanNumbers[tempTime[0] - 50];
-                        }
-                    }
-                }
-            }
+        if (tempTime[1] <= 20) {
+            tempTime[1] = 'X' + firstRomanNumbers[tempTime[0] - 10];
+        }
+        if (tempTime[1] <= 30) {
+            tempTime[1] = 'XX' + firstRomanNumbers[tempTime[0] - 20];
+        }
+        if (tempTime[1] <= 40) {
+            tempTime[1] = 'XXX' + firstRomanNumbers[tempTime[0] - 30];
+        }
+        if (tempTime[1] <= 50) {
+            tempTime[1] = 'XL' + firstRomanNumbers[tempTime[0] - 40];
+        } else {
+            tempTime[1] = 'L' + firstRomanNumbers[tempTime[0] - 50];
         }
     }
     else {
