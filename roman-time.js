63132ad9bee16de2466b670d7c420b0f738bb83a
AlexSm13 <79068122307@yandex.ru>
2016-10-09 05:23:18 +0500

    Roman

--- roman-time.js
+++ roman-time.js
@@ -19,9 +19,8 @@ function errorChecking(time) {
     for (var i = 0; i < partOfTime.length; i++) {
         if (isNaN(parseInt(partOfTime[i])) || parseInt(partOfTime[i]) < 0) {
             throw new TypeError('Неверное время');
-        } else if (parseInt(partOfTime[0]) > 23 || parseInt(partOfTime[1]) > 59) {
-            throw new TypeError('Неверное время');
-        } else if (Number(partOfTime[i]) - Math.floor(partOfTime[i]) === 0) {
+        }
+        if (parseInt(partOfTime[0]) > 23 || parseInt(partOfTime[1]) > 59) {
             throw new TypeError('Неверное время');
         }
     }
@@ -66,6 +65,9 @@ function getTens(romeTime, tens) {
 }
 
 function checkNull(numb, rom) {
+    if (Number(numb) - Math.floor(numb) != 0) {
+        throw new TypeError('Неверное время');
+    }
     if (parseInt(numb) === 0) {
         return 'N';
     }
