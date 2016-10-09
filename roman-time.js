7aa20ee6c0685dfc7c39075afd880adf41f4fd4d
Zeace <pern2500@ya.ru>
2016-10-09 19:39:46 +0500

    изменена ф-я перевода

--- roman-time.js
+++ roman-time.js
@@ -9,16 +9,18 @@ function romanTime(time) {
     var hour = null;
     var minute = null;
     var arrayOfTime = time.split(':');
+    var hourArabic = parseInt(arrayOfTime[0], 10);
+    var minuteArabic = parseInt(arrayOfTime[1], 10);
     if (parseInt(arrayOfTime[0], 10) === 0) {
         hour = 'N';
-    } else if (isNaN(parseInt(arrayOfTime[0], 10)) || parseInt(arrayOfTime[0]) > 23 || parseInt(arrayOfTime[0]) < 0) {
+    } else if (isNaN(hourArabic) || hourArabic > 23 || hourArabic < 0) {
         throw new TypeError('Неверное время');
     } else if (!isNaN(parseInt(arrayOfTime[0], 10))) {
         hour = parseNumber(parseInt(arrayOfTime[0], 10));
     }
-    if (parseInt(arrayOfTime[1],10) === 0) {
+    if (parseInt(arrayOfTime[1], 10) === 0) {
         minute = 'N';
-    } else if (isNaN(parseInt(arrayOfTime[1], 10)) || parseInt(arrayOfTime[1]) > 59 || parseInt(arrayOfTime[1]) < 0) {
+    } else if (isNaN(minuteArabic) || minuteArabic > 59 || minuteArabic < 0) {
         throw new TypeError('Неверное время');
     } else {
         minute = parseNumber(parseInt(arrayOfTime[1], 10));
@@ -29,43 +31,18 @@ function romanTime(time) {
 }
 
 function parseNumber(num) {
+    var arabic = [1, 4, 5, 9, 10, 40, 50];
+    var romanic = ['I','IV','V','IX','X','XL','L'];
     var roman = '';
-    var arrayOfSymbol = [];
-    arrayOfSymbol[0] = (num - num % 50) / 50;
-    num %= 50;
-    arrayOfSymbol[1] = (num - num % 10) / 10;
-    num %= 10;
-    arrayOfSymbol[2] = (num - num % 5) / 5;
-    arrayOfSymbol[3] = num % 5;
-    if (arrayOfSymbol[0] === 1) {
-        roman += 'L';
-    }
-    var i = null;
-    if (arrayOfSymbol[1] !== 0 && arrayOfSymbol[1] !== 4) {
-        for (i = arrayOfSymbol[1]; i !== 0; i--) {
-            roman += 'X';
-        }
-    }
-    if (arrayOfSymbol[1] === 4) {
-        roman = roman.substring(0, roman.length - 1);
-        roman += 'XL';
-    }
-    if (arrayOfSymbol[2] !== 0) {
-        for (i = arrayOfSymbol[2]; i !== 0; i--) {
-            roman += 'V';
-        }
-    }
-    if (arrayOfSymbol[3] !== 0 &&  arrayOfSymbol[3] !== 4) {
-        for (i = arrayOfSymbol[3]; i !== 0; i--) {
-            roman += 'I';
-        }
-    }
-    if (arrayOfSymbol[3] === 4) {
-        if (roman[roman.length - 1] === 'V') {
-            roman = roman.substring(0, roman.length - 1);
-            roman += 'IX';
+    var index = arabic.length - 1;
+    while(num > 0)
+    {
+        if(num >= arabic[index])
+        {
+            roman += romanic[index];
+            num -= arabic[index];
         } else {
-            roman += 'IV';
+            index--;
         }
     }
 
