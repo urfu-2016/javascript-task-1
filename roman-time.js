a075d420a4c87100e33cbe488773cd9ae8ca22b5
Миннибаева Генриетта <henryminnib@gmail.com>
2016-10-09 21:59:37 +0500

    решение

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,30 @@
 'use strict';
-
+function ConvertToRomanNum(num){
+	var romanNum = ['N','I','II','III','IV','V','VI','VII','VIII','IX', 'X', 'XX','XXX','XL','L'];
+	if(num < 11){
+	num  = romanNum[num];
+	}
+	else if(num % 10 == 0){
+			num = romanNum[((num-num%10)/10+9)]; 
+		 }
+		else { num = romanNum[((num-num%10)/10+9)]+ romanNum[num%10];}
+	return num;
+}
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
+    var arrHoursMin= time.split(':');
+	var hours = parseInt(arrHoursMin[0], 10);
+	var min = parseInt(arrHoursMin[1], 10);
+	if(hours >= 0 && hours < 24 && min >= 0 && min < 60){
+	hours = ConvertToRomanNum(hours);
+	min = ConvertToRomanNum(min);
+	time = hours +':'+ min;
     return time;
+	}
+	throw new TypeError('Неверное время');
 }
 
 module.exports = romanTime;
