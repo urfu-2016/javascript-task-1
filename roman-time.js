334959346f6793d54156def1f39d2f5ec9ff7e13
Uliana1997 <uliana-kvashnina2012@yandex.ru>
2016-10-09 23:33:29 +0600

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -6,7 +6,34 @@
  */
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    return time;
+    var t = time.split(':');
+
+    if (isNaN(parseInt(t[0])) || isNaN(parseInt(t[1])) || t[0]>23 || t[1]>59 || t[0]<0 || t[1]<0){
+    	throw new TypeError('Неверный ввод!');
+    }
+    else{
+    	var hh = parseInt(t[0]);
+    	var mm = parseInt(t[1]);
+    	var hours = convert(hh);
+    	var min = convert(mm);
+    	if(isNaN(hours) ==  false){hours = "N"};
+    	if(isNaN(min) == false){min = "N"};
+        time = hours + ':' + min;
+    
+    }
+ return time;
+}
+
+function convert(num) {
+    var lookup = {L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}, roman = '', i;
+    for ( i in lookup ) {
+        while ( num >= lookup[i] ) {
+            roman += i;
+            num -= lookup[i];
+        }
+    }
+    
+    return roman;
 }
 
 module.exports = romanTime;
