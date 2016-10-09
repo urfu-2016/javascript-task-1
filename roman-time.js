0b930729844f6c7cb4781380e812aa5a9f6941e6
Uliana1997 <uliana-kvashnina2012@yandex.ru>
2016-10-10 00:35:52 +0600

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,34 @@
 'use strict';
 
-/**
- * @param {String} time – время в формате HH:MM (например, 09:05)
- * @returns {String} – время римскими цифрами (IX:V)
- */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    var t = time.split(':');
+
+    if (isNaN(Number(t[0])) || isNaN(Number(t[1])) || t[0] > 23 || t[1] > 59 || t[0] < 0 || t[1] < 0){
+        throw new TypeError('Неверный ввод!');
+    }
+    else {
+        var hh = parseInt(t[0]);
+        var mm = parseInt(t[1]);
+        var hours = convert(hh);
+        var min = convert(mm);
+        if(isNaN(hours) ===  false){hours = "N"}
+        if(isNaN(min) === false){min = "N"}
+        time = hours + ':' + min; 
+         }
+ return time;
+
+}
+
+function convert(num) {
+    var lookup = {L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1}, roman = '', i;
+    for ( i in lookup ) {
+        while ( num >= lookup[i] ) {
+            roman += i;
+            num -= lookup[i];
+        }
+    }
+    return roman;
+
 }
 
 module.exports = romanTime;
