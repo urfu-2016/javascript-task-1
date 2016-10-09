6685fb45651940cbd29fae70a858cb1a3c4f5e56
Zeace <pern2500@ya.ru>
2016-10-09 20:19:15 +0500

    вынесены все функции

--- roman-time.js
+++ roman-time.js
@@ -6,34 +6,63 @@
  */
 
 function romanTime(time) {
-    var hour = null;
-    var minute = null;
+
     var arrayOfTime = time.split(':');
     var hourArab = parseInt(arrayOfTime[0], 10);
     var minArab = parseInt(arrayOfTime[1], 10);
-    if (isNaN(hourArab) || hourArab > 23 || hourArab < 0 || isNaN(minArab) || minArab > 59 || minArab < 0) {
+    if (checkHour(hourArab) + checkMinute(minArab) > 0) {
         throw new TypeError('Неверное время');
     }
+    var hourRoman = hourChange(hourArab);
+    var minRoman = minChange(minArab);
+    time = hourRoman + ':' + minRoman;
+
+    return time;
+}
+
+function checkHour(hourArab) {
+    if (isNaN(hourArab) || hourArab > 23 || hourArab < 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+function checkMinute(minArab) {
+    if (isNaN(minArab) || minArab > 59 || minArab < 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+function hourChange(hourArab) {
+    var hour = null;
     if (hourArab === 0) {
         hour = 'N';
     } else {
         hour = parseNumber(hourArab);
     }
+
+    return hour;
+}
+
+function minChange(minArab) {
+    var minute = null;
     if (minArab === 0) {
         minute = 'N';
     } else {
         minute = parseNumber(minArab);
     }
-    time = hour + ':' + minute;
 
-    return time;
+    return minute;
 }
 
 function parseNumber(num) {
     var arabic = [1, 4, 5, 9, 10, 40, 50];
     var romanic = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
     var roman = '';
-    var index = arabic.length -1;
+    var index = arabic.length - 1;
     while (num !== 0) {
         if (num >= arabic[index]) {
             num -= arabic[index];
