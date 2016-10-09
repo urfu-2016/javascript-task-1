67aa036ab1828219b9f26623ce2b413090c79ae0
imatasov <ivan.matasov@yandex.ru>
2016-10-10 00:47:14 +0500

    Убраны некоторые недочеты

--- index.js
+++ index.js
@@ -11,5 +11,12 @@ console.info(romanTime('00:00'));
 // Выведет 'XXIII:LIX'
 console.info(romanTime('23:59'));
 
+console.info(romanTime('18:48'));
+
 // Выбросится ошибка [TypeError: Неверное время]
 console.info(romanTime('24:00'));
+
+console.info(romanTime('123:414'));
+
+
+console.info(romanTime('17:20'));
