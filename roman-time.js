307e13927df9db00664fc01370bcff0bf64b2f95
Лилия Шаламова < >
2016-10-10 23:19:59 +0500

    Синтаксис 5

--- roman-time.js
+++ roman-time.js
@@ -15,8 +15,7 @@ function romanTime(time) {
     } catch (e) {
         throw new TypeError("Некорректное время");
     }
-    if (isNaN(arabicHour) || isNaN(arabicMinute) || arabicHour > 23 ||
-     arabicMinute > 59 || arabicHour < 0 || arabicMinute < 0) {
+    if (isNaN(arabicHour) || isNaN(arabicMinute) || arabicHour > 23 || arabicMinute > 59 || arabicHour < 0 || arabicMinute < 0) {
         throw new TypeError("Некорректное время");
     }
     var result = "";
