b0ce1de05bf062657839531578b956288a52d581
Firsov Kolya <kolya.firsov@gmail.com>
2016-10-11 02:55:35 +0500

    Исправление табуляции 4

--- roman-time.js
+++ roman-time.js
@@ -5,7 +5,8 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    var arr = time.split(':'), hour = parseInt(arr[0]);
+    var arr = time.split(':'), 
+        hour = parseInt(arr[0]);
     var minutes = parseInt(arr[1]);
     var romanNumber = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII',
                        'VIII', 'IX', 'X', 'XX', 'XXX', 'XL', 'L', 'LX'];
@@ -15,16 +16,16 @@ function romanTime(time) {
     if (hour <= 10) {
         timeHour = romanNumber[arabicNumber.indexOf(hour)];
     } else {
-        timeHour = romanNumber[arabicNumber.indexOf(parseInt(hour / 10) * 10)] 
-        + romanNumber[arabicNumber.indexOf(hour % 10)];
+        timeHour = romanNumber[arabicNumber.indexOf(parseInt(hour / 10) * 10)] +
+        romanNumber[arabicNumber.indexOf(hour % 10)];
     }
 
     var timeMinutes;
     if (minutes <= 10) {
         timeMinutes = romanNumber[arabicNumber.indexOf(minutes)];
     } else {
-        timeMinutes = romanNumber[arabicNumber.indexOf(parseInt(minutes / 10) * 10)] 
-        + romanNumber[arabicNumber.indexOf(minutes % 10)];
+        timeMinutes = romanNumber[arabicNumber.indexOf(parseInt(minutes / 10) * 10)] +
+        romanNumber[arabicNumber.indexOf(minutes % 10)];
     }
 
     if (isNaN(hour) || isNaN(minutes) || hour > 23 || minutes >= 60) {
@@ -32,6 +33,7 @@ function romanTime(time) {
     } else {
         time = timeHour + ':' + timeMinutes;
     }
+    
     return time;
 }
 
