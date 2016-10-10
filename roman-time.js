b1598799e74448dfb018ebdd26e74b23f3425265
Mikhail Pliskovsky <snoward@yandex.ru>
2016-10-10 23:03:38 +0500

    Перевод времени в римский формат

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,72 @@
 'use strict';
 
+function isCorrectValue(value, minRange, maxRange) {
+    return !(isNaN(value) || value < minRange || value > maxRange);
+}
+
+function isCorrectHour(hour) {
+    return isCorrectValue(hour, 0, 23);
+}
+
+function isCorrectMinute(minute) {
+    return isCorrectValue(minute, 0, 59);
+}
+
+var dict = {
+    0: "N",
+    1: "I",
+    2: "II",
+    3: "III",
+    4: "IV",
+    5: "V",
+    6: "VI",
+    7: "VII",
+    8: "VIII",
+    9: "IX",
+    10: "X",
+    20: "XX",
+    30: "XXX",
+    40: "XL",
+    50: "L"
+};
+
+function decToRoman(value) {
+    var firstDigit = Math.floor(value / 10);
+    var secondDigit = value % 10;
+
+    if (firstDigit === 0) {
+        return dict[secondDigit];
+    }
+
+    if (secondDigit === 0) {
+        return dict[firstDigit * 10];
+    }
+
+    return dict[firstDigit * 10] + dict[secondDigit];
+}
+
+
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    if (!time) {
+        throw new TypeError("Неверное время");
+    }
+
+    var data = time.split(":");
+    var hour = parseInt(data[0]);
+    var minute = parseInt(data[1]);
+
+    if (!isCorrectHour(hour) || !isCorrectMinute(minute)) {
+        throw new TypeError("Неверное время");
+    }
+
+    var romanHour = decToRoman(hour);
+    var romanMinute = decToRoman(minute);
+
+    return romanHour + ':' + romanMinute;
 }
 
 module.exports = romanTime;
