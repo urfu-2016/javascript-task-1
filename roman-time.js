05c94c2b0afba3af6913092bd7cf81400e415411
Alex768 <feniwe@gmail.com>
2016-10-10 23:20:50 +0500

    Линтинг romanTime.js

--- roman-time.js
+++ roman-time.js
@@ -5,45 +5,55 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    if(time == null || time == undefined || typeof time != 'string' || time.indexOf(':') == -1) {
+    if (!isTimeFormatCorrect(time)) {
         throwError(8, time);
     }
     var timeParts = time.split(':');
-    if(isNaN(timeParts[0]) || isNaN(timeParts[1])) {
+    if (isNaN(timeParts[0]) || isNaN(timeParts[1])) {
         throwError(12, time);
     }
     var hours = parseInt(timeParts[0]);
     var minutes = parseInt(timeParts[1]);
-    if(hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
+    if (isTimeOutOfRange(hours, minutes)) {
         throwError(17, time);
     }
+    
     return getRomanRepresentation(hours) + ':' + getRomanRepresentation(minutes);
+}
+
+function isTimeFormatCorrect(time) {
+    return (time !== null || time !== undefined || typeof time === 'string' || time.indexOf(':') !== -1);
+}
+
+function isTimeOutOfRange(hours, minutes) {
+    return (hours < 0 || hours > 23 || minutes < 0 || minutes > 59);
 }
 
 function throwError(line, incomingData) {
     throw new TypeError('incorrect data(' + line + '): ' + incomingData);
 }
 
-var romanDigits = [ '', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX' ];
+var romanDigits = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
 
 function getRomanRepresentation(number) {
-    if(number == 0) {
+    if (number === 0) {
         return 'N';
     }
     var result = '';
-    if(parseInt((number / 50)) > 0) {
+    if (parseInt((number / 50)) > 0) {
         result += 'L';
     }
     number = number % 50;
     var xDigitsCount = parseInt(number / 10);
-    if(xDigitsCount == 4) {
+    if (xDigitsCount === 4) {
         result = 'XL';
     } else {
-        for(var i = 1; i <= xDigitsCount; i++) {
+        for (var i = 1; i <= xDigitsCount; i++) {
             result += 'X';
         }
     }
     result += romanDigits[number % 10];
+    
     return result;
 }
 
