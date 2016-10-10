7f4736e2361a024b0a560ca1866dabb59a1bdfb2
Irina <irinachkas.96@gmail.com>
2016-10-11 01:58:04 +0500

    task2

--- roman-time.js
+++ roman-time.js
@@ -5,8 +5,8 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    var reTime = /\d\d[:]\d\d/
-    if (time.search(reTime) == -1) {
+    var reTime = /\d\d[:]\d\d/;
+    if (time.search(reTime) === -1) {
         throw new TypeError();
     }
     var hoursAndMinutes = time.split(':');
@@ -16,29 +16,29 @@ function romanTime(time) {
         throw new TypeError();
     }
     var decimal = {
-    '0': '',
-    '1': 'I',
-    '2': 'II',
-    '3': 'III',
-    '4': 'IV',
-    '5': 'V',
-    '6': 'VI',
-    '7': 'VII',
-    '8': 'VIII',
-    '9': 'IX'
+        '0': '',
+        '1': 'I',
+        '2': 'II',
+        '3': 'III',
+        '4': 'IV',
+        '5': 'V',
+        '6': 'VI',
+        '7': 'VII',
+        '8': 'VIII',
+        '9': 'IX'
     };
     var tens = {
-    '0': '',
-    '1': 'X',
-    '2': 'XX',
-    '3': 'XXX',
-    '4': 'XL',
-    '5': 'L',
-    '6': 'LX'
+        '0': '',
+        '1': 'X',
+        '2': 'XX',
+        '3': 'XXX',
+        '4': 'XL',
+        '5': 'L',
+        '6': 'LX'
     };
 
-    return (hours == 0 ? 'N' : tens[Math.floor(hours / 10)] + decimal[hours % 10]) + ':' + 
-           (minutes == 0 ? 'N' : tens[Math.floor(minutes / 10)] + decimal[minutes % 10]);
+    return (hours === 0 ? 'N' : tens[Math.floor(hours / 10)] + decimal[hours % 10]) + ':' +
+           (minutes === 0 ? 'N' : tens[Math.floor(minutes / 10)] + decimal[minutes % 10]);
 }
 
 module.exports = romanTime;
