07e231a28bca06c88e550aa720ec20cb9262e302
KozlovAlexanderS <aleksanderkozlov@yandex.ru>
2016-10-11 01:24:13 +0500

    изминение

--- roman-time.js
+++ roman-time.js
@@ -1,41 +1,26 @@
 'use strict';
 
 /**
- * @param {String} time – время в формате HH:MM (например, 09:05)
+ * @param {String} arab – арабское число
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function arabicToRoman(arab)
-{ 
+function arabicToRoman(arab){
     var tenDigits = ["", "X", "XX", "XXX", "XL", "L"];
     var oneDigits = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-    if (parseInt(arab, 10)===0)
-    {
+    if (parseInt(arab, 10) === 0){
         return 'N';
     }
-    else
-    {
-        var res = tenDigits[Math.floor(arab / 10)] + oneDigits[arab % 10];
-        return res;
+    var res = tenDigits[Math.floor(arab / 10)] + oneDigits[arab % 10];
+    return res;
+}
+
+function romanTime(time){
+    var b = time.split(':');
+    if (b.length <= 2 && time.length <= 5){
+        if (b[0] < 24 && b[0] >= 0 && b[1] < 60 && b[1] >= 0){
+            return arabicToRoman(b[0]) + ':' + arabicToRoman(b[1]);
+        }
     }
+    throw new TypeError('Error');
 }
-
-function romanTime(time) {
-        var b = time.split(':');
-        if (b.length <= 2 && time.length <= 5)
-        {
-            if (b[0] < 24 && b[0] >= 0 && b[1] < 60 && b[1] >= 0 )
-            {
-                 return arabicToRoman(b[0]) + ':' + arabicToRoman(b[1]);
-            }
-        }
-        try
-        { 
-            throw new TypeError('Error');
-        }
-        catch (e) {}
-
-}
-
 module.exports = romanTime;
-
-
