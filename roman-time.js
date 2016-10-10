fccaf989e5fb6b20862cc89855fdb400c100e249
Лера <onahochetnamars@yandex.ru>
2016-10-10 17:59:05 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -4,40 +4,40 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function toRoman (x, Res) {
+function toRoman(x, Res) {
     if (x >= 50) {
         return toRoman (x - 50, Res + 'L');
     }
     if (x >= 40) {
-            return toRoman (x - 40, Res + 'XL');
+        return toRoman (x - 40, Res + 'XL');
     }
     if (x >= 10) {
-            return toRoman (x - 10, Res + 'X');
+        return toRoman (x - 10, Res + 'X');
     }
     if (x >= 9) {
-            return toRoman (x - 9, Res + 'IX');
+        return toRoman (x - 9, Res + 'IX');
     }
     if (x >= 5) {
-            return toRoman (x - 5, Res + 'V');
+        return toRoman (x - 5, Res + 'V');
     }
     if (x >= 4) {
-            return toRoman (x - 4, Res + 'IV');
+        return toRoman (x - 4, Res + 'IV');
     }
     if (x >= 1) {
-            return toRoman (x - 1, Res + 'I')
+        return toRoman (x - 1, Res + 'I');
     }
     if (Res === '') {
-            return "N";
+        return "N";
     }
-    
+
     return Res;
 }
 
-function correctTime (hours, minutes) {
+function correctTime(hours, minutes) {
     return (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59);
 }
 
-function romanTime (time) {
+function romanTime(time) {
     var partTime = time.split (':');
     if (partTime.length === 2) {
         var hours = parseInt (partTime[0], 10);
@@ -47,7 +47,7 @@ function romanTime (time) {
         }
     }
 
-    return TypeError ("Неверное время");
+    return new TypeError ("Неверное время");
 }
 
 module.exports = romanTime;
