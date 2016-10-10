8dde3289d897be37be3c577bb622a924ec6da09b
Лилия Шаламова < >
2016-10-10 22:40:25 +0500

    Первый вариант

--- roman-time.js
+++ roman-time.js
@@ -6,7 +6,41 @@
  */
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    return time;
+    var timeArray = time.split(':');
+    try
+    {
+        var arabicHour = parseInt(timeArray[0]);
+        var arabicMinute = parseInt(timeArray[1]);
+    }
+    catch (e)
+    {
+        throw new TypeError("Некорректное время");
+    }
+    if (isNaN(arabicHour) || isNaN(arabicMinute) || arabicHour > 23 || arabicMinute > 59)
+    {
+    	throw new TypeError("Некорректное время");
+    }
+    var result = "";
+    result = convertTime(arabicHour) + ":" + convertTime(arabicMinute);
+
+    return result;
+}
+
+function convertTime(number)
+{
+    var dozens = {0:"", 1:"X", 2:"XX", 3:"XXX", 4:"XL", 5:"L"};
+    var units = {0:"", 1:"I", 2:"II", 3:"III", 4:"IV", 5:"V", 6:"VI", 7:"VII", 8:"VIII", 9:"IX"};
+    var result = "";
+    if (number == 0)
+    {
+    	result = "N";
+    }
+    else
+    {
+        result = dozens[parseInt(number / 10)] + units[parseInt(number % 10)];
+    }
+
+    return result;
 }
 
 module.exports = romanTime;
