a49de1b7c269cdc287515dd60da4a476bffc908f
Daniilmaslov <danilmaslove@mail.ru>
2016-10-10 22:44:11 +0500

    Добавлен первый вариант решения

--- roman-time.js
+++ roman-time.js
@@ -6,7 +6,32 @@
  */
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    return time;
+    var hoursMin = time.split(':');
+    var hours = parseInt(hoursMin[0], 10);
+    var min = parseInt(hoursMin[1], 10);
+    if (hours < 24 && min < 60 && hoursMin[0].length === 2 && hoursMin[1].length === 2) {
+        hours = convertToRomanNumber(hours);
+        min = convertToRomanNumber(min);
+        time = hours + ':' + min;
+        return time;
+    }
+    throw new TypeError('Неверное время');
+}
+
+function convertToRomanNumber(number) {
+    var romanNumber = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+    var romanNumberTens = ['X', 'XX', 'XXX', 'XL', 'L'];
+    if (number < 11) {
+        number = romanNumber[number];
+    } 
+    else {
+        var tempNum = romanNumberTens[(number - number % 10) / 10 - 1];
+        if (number % 10 != 0) {
+            number = tempNum + romanNumber[number % 10];
+        }
+        else number = tempNum;
+    }
+    return number;
 }
 
 module.exports = romanTime;
