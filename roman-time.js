e26411cdaee2e88227fae45b86483949b3394879
Лилия Шаламова < >
2016-10-10 22:49:32 +0500

    Синтаксис 1

--- roman-time.js
+++ roman-time.js
@@ -4,13 +4,16 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function romanTime(time) {
+function romanTime(time) 
+{
     // Немного авторского кода и замечательной магии
     var timeArray = time.split(':');
+    var arabicHour = 0;
+    var arabicMinute = 0;
     try
     {
-        var arabicHour = parseInt(timeArray[0]);
-        var arabicMinute = parseInt(timeArray[1]);
+        arabicHour = parseInt(timeArray[0]);
+        arabicMinute = parseInt(timeArray[1]);
     }
     catch (e)
     {
@@ -18,7 +21,7 @@ function romanTime(time) {
     }
     if (isNaN(arabicHour) || isNaN(arabicMinute) || arabicHour > 23 || arabicMinute > 59)
     {
-    	throw new TypeError("Некорректное время");
+        throw new TypeError("Некорректное время");
     }
     var result = "";
     result = convertTime(arabicHour) + ":" + convertTime(arabicMinute);
@@ -28,12 +31,12 @@ function romanTime(time) {
 
 function convertTime(number)
 {
-    var dozens = {0:"", 1:"X", 2:"XX", 3:"XXX", 4:"XL", 5:"L"};
-    var units = {0:"", 1:"I", 2:"II", 3:"III", 4:"IV", 5:"V", 6:"VI", 7:"VII", 8:"VIII", 9:"IX"};
+    var dozens = { 0: "", 1: "X", 2: "XX", 3: "XXX", 4: "XL", 5: "L" };
+    var units = { 0: "", 1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX" };
     var result = "";
-    if (number == 0)
+    if (number === 0)
     {
-    	result = "N";
+        result = "N";
     }
     else
     {
