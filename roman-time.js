d7c381355ee1d01d31b72a1a7849b478e98f9065
zokeer <zokeerxd@gmail.com>
2016-10-10 19:12:13 +0500

    Дмитрий Голубев

--- roman-time.js
+++ roman-time.js
@@ -1,53 +1,70 @@
 'use strict';
 
 function TryParseInt(str, defaultValue) {
-    return parseInt(str, 10) == str ? parseInt(str) : defaultValue
+    return parseInt(str, 10) == str ? parseInt(str) : defaultValue;
 }
 
-function isValidTime(time){
-    if (time == undefined){
-        return false
-    }
-    var splitted = time.split(':')
-    if (splitted.length !== 2){
-        return false;
-    }
+function isValidTime2(time){
+    var splitted = time.split(':');
     for (var i = splitted.length - 1; i >= 0; i--) {
         if (TryParseInt(splitted[i], null) === null){
+
             return false
         }       
-    }
+    };
     if (parseInt(splitted[0]) > 23){
-        return false
-    }
+
+        return false;
+    };
     if (parseInt(splitted[1]) > 59) {
-        return false
-    }
-    return true
+
+        return false;
+    };
+
+    return true;
+}
+
+function isValidTime1(time){
+    if (time === undefined){
+
+        return false;
+    };
+    var splitted = time.split(':');
+    if (splitted.length !== 2){
+
+        return false;
+    };
+
+    return true;
 }
 
 function castToRoman(twoDigits){
-    var answer = ""
+    var answer = "";
     var digitsCast = { 0: "", 1: "I", 2: "II", 3: "III", 4: "IV",
-               5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX" }
-    if (twoDigits === "00")
-        return "N"
+               5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX" };
+    if (twoDigits === "00"){
+
+        return "N";
+    };
     for (var i = 0; i < parseInt(twoDigits.charAt(0)); i++) {
-        answer += "X"
-    }
-    answer += digitsCast[parseInt(twoDigits.charAt(1))]
-    return answer
+        answer += "X";
+    };
+    answer += digitsCast[parseInt(twoDigits.charAt(1))];
+
+    return answer;
 }
 
 function castHours(hours){
-    var answer = castToRoman(hours)
-    return answer
+    var answer = castToRoman(hours);
+
+    return answer;
 }
 
 function castMinutes(minutes){
-    var answer = castToRoman(minutes)
-    answer.replace("XXXXX", "L")
-    return answer
+    var answer = castToRoman(minutes);
+    answer.replace("XXXXX", "L");
+
+    return answer;
 }
 
 /**
@@ -56,9 +73,10 @@ function castMinutes(minutes){
  */
 
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    if (isValidTime(time)){
-        return castHours(time.split(':')[0]) + ":" + castMinutes(time.split(':')[1])
+    if (isValidTime1(time)){
+        if (isValidTime2(time))
+
+            return castHours(time.split(':')[0]) + ":" + castMinutes(time.split(':')[1]);
     }
     throw new TypeError("Неверное время")
 }
