06a719e3b5e07c339a1cf399a660ebc369781913
axel86rus <lopuchaleksey@mail.ru>
2016-10-10 11:28:31 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -1,38 +1,39 @@
 'use strict';
 
 function isValiable(hour, minute) {
-    if (isNaN(minute)|| isNaN(hour) || hour > 23 || hour < 0 || minute > 59 || minute < 0) {
-        throw new TypeError("Неверное время")
-    }
+    return hour > 23 || hour < 0 || minute > 59 || minute < 0;
 }
 
 function roman(units, dozens) {
     var res;
-    var romanUnits = ['','I','II','III','IV','V','VI','VII','VIII','IX'];
-    var romanDozens = ['','X','XX','XXX','XL','L'];
-    if (units == 0){
+    var romanUnits = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    var romanDozens = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+    if (units === 0) {
         res = 'N';
-    }else{
-        res = romanDozens[(units - units % 10)/10] + romanUnits[units % 10]
+    } else {
+        res = romanDozens[(units - units % 10) / 10] + romanUnits[units % 10];
     }
     res += ':';
-    if (dozens == 0){
-        res += 'N'
-    }else{
-        res += romanDozens[(dozens - dozens % 10)/10] + romanUnits[dozens % 10]
+    if (dozens === 0) {
+        res += 'N';
+    } else {
+        res += romanDozens[(dozens - dozens % 10) / 10] + romanUnits[dozens % 10];
     }
-    return res
+
+    return res;
 }
 
 function romanTime(time) {
     time = time.split(':');
     var hour = Number(time[0]);
     var minute = Number(time[1]);
+    if (isNaN(minute) || isNaN(hour) || isValiable(hour, minute)) {
+        throw new TypeError("Неверное время");
+    } else {
+        time = roman(hour, minute);
+    }
 
-    isValiable(hour, minute);
-    time  =  roman(hour, minute);
     return time;
 }
-
 module.exports = romanTime;
 
