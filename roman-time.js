500cf4dee1b99f449a3d36c2b06191a48f3f13a0
Firsov Kolya <kolya.firsov@gmail.com>
2016-10-11 03:57:13 +0500

    Исправление табуляции 9

--- roman-time.js
+++ roman-time.js
@@ -5,42 +5,42 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    var arr = time.split(':'),
-        hour = parseInt(arr[0]),
-        minutes = parseInt(arr[1]),
-        romanNumber = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII',
+    var romanNumber = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII',
                        'VIII', 'IX', 'X', 'XX', 'XXX', 'XL', 'L', 'LX'],
         arabicNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60];
-
-    var timeHour,
-        hourDec,
-        hourUnit;
-    if (hour <= 10) {
-        timeHour = romanNumber[arabicNumber.indexOf(hour)];
-    } else {
-    	hourDec = parseInt(hour / 10) * 10;
-    	hourUnit = hour % 10;
-        timeHour = romanNumber[arabicNumber.indexOf(hourDec)] +
-        romanNumber[arabicNumber.indexOf(hourUnit)];
-    }
-
-    var timeMinutes,
-        minutesDec,
-        minutesUnit;
-    if (minutes <= 10) {
-        timeMinutes = romanNumber[arabicNumber.indexOf(minutes)];
-    } else {
-        minutesDec = parseInt(minutes / 10) * 10;
-        minutesUnit = minutes % 10;
-        timeMinutes = romanNumber[arabicNumber.indexOf(minutesDec)] +
-        romanNumber[arabicNumber.indexOf(minutesUnit)];
-    }
-
+    
+    var arr = time.split(':'),
+        hour = parseInt(arr[0]),
+        minutes = parseInt(arr[1]);
+        
     if (isNaN(hour) || hour > 23 || isNaN(minutes)|| minutes >= 60) {
         time = new TypeError('Неверное время');
     } else {
-        time = timeHour + ':' + timeMinutes;
-    }
+    	var timeHour,
+        hourDec,
+        hourUnit;
+        if (hour <= 10) {
+            timeHour = romanNumber[arabicNumber.indexOf(hour)];
+        } else {
+            hourDec = parseInt(hour / 10) * 10;
+            hourUnit = hour % 10;
+            timeHour = romanNumber[arabicNumber.indexOf(hourDec)] +
+            romanNumber[arabicNumber.indexOf(hourUnit)];
+        }
+
+        var timeMinutes,
+        minutesDec,
+        minutesUnit;
+        if (minutes <= 10) {
+            timeMinutes = romanNumber[arabicNumber.indexOf(minutes)];
+        } else {
+            minutesDec = parseInt(minutes / 10) * 10;
+            minutesUnit = minutes % 10;
+            timeMinutes = romanNumber[arabicNumber.indexOf(minutesDec)] +
+            romanNumber[arabicNumber.indexOf(minutesUnit)];
+        }
+            time = timeHour + ':' + timeMinutes;
+        }
 
     return time;
 }
