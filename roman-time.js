27ec7309fcd2f8f7b02df9c59313e1b87b023804
Лера <onahochetnamars@yandex.ru>
2016-10-10 20:56:46 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -4,32 +4,44 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function toRoman(x, Res) {
-    if (x >= 50) {
-        return toRoman (x - 50, Res + 'L');
-    }
-    if (x >= 40) {
-        return toRoman (x - 40, Res + 'XL');
-    }
-    if (x >= 10) {
-        return toRoman (x - 10, Res + 'X');
-    }
-    if (x >= 9) {
-        return toRoman (x - 9, Res + 'IX');
-    }
-    if (x >= 5) {
-        return toRoman (x - 5, Res + 'V');
-    }
-    if (x >= 4) {
-        return toRoman (x - 4, Res + 'IV');
-    }
-    if (x >= 1) {
-        return toRoman (x - 1, Res + 'I');
-    }
-    if (Res === '') {
+function toRoman(x) {
+    //var arab = [0,1,4,5,9,10,40,50];
+    //var roman = ["N","I","IV","V","IX","X","XL","L"];
+    if (x === 0) {
         return "N";
     }
-
+    var Res = '';
+    var a = x % 10;
+    var b = x - a;
+    while (a > 0) {
+        if (a == 9) {
+            Res = "IX";
+            a = 0;
+        }
+        if (a >= 5) {
+            Res = Res + "V";
+            a = a - 5;
+        }
+        if (a >= 4) {
+            Res = Res + "IV";
+            a = a - 4;
+        }
+        if (a >= 1) {
+            Res = Res + "I";
+            a = a - 1;
+        }
+    }
+    if (b == 50) {
+        return "L" + Res;
+    }
+    if (b == 40) {
+        return "XL" + Res;
+    }
+    while (b > 0) {
+        if (b >= 10) {
+            Res = "I" + Res;
+        }
+    }
     return Res;
 }
 
@@ -43,7 +55,7 @@ function romanTime(time) {
         var hours = parseInt (partTime[0], 10);
         var minutes = parseInt (partTime[1], 10);
         if (correctTime (hours, minutes)) {
-            return toRoman (hours, '') + ':' + toRoman (minutes, '');
+            return toRoman (hours) + ':' + toRoman (minutes);
         }
     }
 
