335ede4465e6369099343ef181d2dfae9155807f
Firsov Kolya <kolya.firsov@gmail.com>
2016-10-11 04:21:23 +0500

    Исправление табуляции 10

--- roman-time.js
+++ roman-time.js
@@ -4,43 +4,33 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function romanTime(time) {
+
+function translate(timeRoman) {
     var romanNumber = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII',
                        'VIII', 'IX', 'X', 'XX', 'XXX', 'XL', 'L', 'LX'],
         arabicNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60];
-    
+
+    var timeTranslate;
+    if (timeRoman <= 10) {
+        timeTranslate = romanNumber[arabicNumber.indexOf(timeRoman)];
+    } else {
+        timeTranslate = romanNumber[arabicNumber.indexOf(parseInt(timeRoman / 10) * 10)] +
+        romanNumber[arabicNumber.indexOf(timeRoman % 10)];
+    }
+
+    return timeTranslate;
+}
+
+function romanTime(time) {
     var arr = time.split(':'),
         hour = parseInt(arr[0]),
         minutes = parseInt(arr[1]);
-        
+
     if (isNaN(hour) || hour > 23 || isNaN(minutes)|| minutes >= 60) {
         time = new TypeError('Неверное время');
     } else {
-    	var timeHour,
-        hourDec,
-        hourUnit;
-        if (hour <= 10) {
-            timeHour = romanNumber[arabicNumber.indexOf(hour)];
-        } else {
-            hourDec = parseInt(hour / 10) * 10;
-            hourUnit = hour % 10;
-            timeHour = romanNumber[arabicNumber.indexOf(hourDec)] +
-            romanNumber[arabicNumber.indexOf(hourUnit)];
-        }
-
-        var timeMinutes,
-        minutesDec,
-        minutesUnit;
-        if (minutes <= 10) {
-            timeMinutes = romanNumber[arabicNumber.indexOf(minutes)];
-        } else {
-            minutesDec = parseInt(minutes / 10) * 10;
-            minutesUnit = minutes % 10;
-            timeMinutes = romanNumber[arabicNumber.indexOf(minutesDec)] +
-            romanNumber[arabicNumber.indexOf(minutesUnit)];
-        }
-            time = timeHour + ':' + timeMinutes;
-        }
+        time = translate(hour) + ':' + translate(minutes);
+    }
 
     return time;
 }
