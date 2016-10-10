e615bed033da66daea1c978e3f4d55910fb3605e
Artem Borzov <artem.borz@yandex.ru>
2016-10-11 01:05:26 +0500

    first test

--- roman-time.js
+++ roman-time.js
@@ -4,9 +4,28 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
+
+var UNITS = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+var TENS = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+var TIME_PATTERN = /^(([0-1][0-9])|(2[0-3])):[0-5][0-9]$/;
+
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    time = String(time)
+    if (!TIME_PATTERN.test(time)) {
+        throw new TypeError();
+    }
+
+    var [hours, minutes] = time.split(':').map(Number).map(toRoman);
+
+    return hours + ":" + minutes;
+}
+
+function toRoman(number) {
+    if (number === 0) {
+        return 'N';
+    }
+
+    return TENS[Math.floor(number / 10)] + UNITS[number % 10]
 }
 
 module.exports = romanTime;
