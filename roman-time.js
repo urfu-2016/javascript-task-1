8f753e34d7a67a53208d1b9ef901387606c706f6
DariaKineva <Kineva-97@yandex.ru>
2016-10-10 23:37:08 +0500

    Перевод времени в римскую систему исчисления

--- roman-time.js
+++ roman-time.js
@@ -5,7 +5,13 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
+	var h,m;
+	var roman = ["N","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII"];
+	if(!time) {
+ throw new TypeError("Неверное время");}
+	h = Number(time[0])*10 + Number(time[1]);
+	m = Number(time[3])*10 + Number(time[4]);
+	var time = roman[h%12]+":"+roman[m%12];
     return time;
 }
 
