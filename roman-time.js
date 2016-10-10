cfff9d2713ee9f456e9a2e4ded3854e924544a91
Uliana1997 <uliana-kvashnina2012@yandex.ru>
2016-10-10 10:40:37 +0500

    Lint

--- roman-time.js
+++ roman-time.js
@@ -1,30 +1,56 @@
 'use strict';
 
+/**
+ * @param {String} time – время в формате HH:MM (например, 09:05)
+ * @returns {String} – время римскими цифрами (IX:V)
+ */
 function romanTime(time) {
-    var t = time.split(':');
+    var t = checkTime(time);
+    var hh = t[0];
+    var mm = t[1];
+    var hours = convert(hh);
+    var min = convert(mm);
+    if (isNaN(hours) === false) {
+        hours = "N";
+    }
+    if (isNaN(min) === false) {
+        min = "N";
+    }
 
-    if (isNaN(Number(t[0])) || isNaN(Number(t[1])) || t[0] > 23 || t[1] > 59 || t[0] < 0 ||
-     t[1] < 0) {
-        throw new TypeError('Неверный ввод!');
-    }
-    else {
-        var hh = parseInt(t[0]);
-        var mm = parseInt(t[1]);
-        var hours = convert(hh);
-        var min = convert(mm);
-        if (isNaN(hours) === false) { hours = "N"; }
-        if (isNaN(min) === false) { min = "N"; }
-    }
+    time = hours + ':' + min;
 
     return time;
 }
 
+function checkTime(time) {
+    var t = time.split(':');
+    t[0] = Number(t[0]);
+    t[1] = Number(t[1]);
+    checkHours(t[0]);
+    checkMinutes(t[1]);
+
+    return t;
+}
+
+function checkHours(h) {
+    if (h > 23 || h < 0 || isNaN(h)) {
+        throw new TypeError('Неверный ввод!');
+    }
+}
+
+function checkMinutes(m) {
+    if (m > 59 || m < 0 || isNaN(m)) {
+        throw new TypeError('Неверный ввод!');
+    }
+}
+
 function convert(num) {
-    var lookup = { L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+    var lookupSymb = ['L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    var lookup = [50, 40, 10, 9, 5, 4, 1];
     var roman = '';
-    for (var i in lookup) {
+    for (var i = 0; i < lookup.length; i++) {
         while (num >= lookup[i]) {
-            roman += i;
+            roman += lookupSymb[i];
             num -= lookup[i];
         }
     }
