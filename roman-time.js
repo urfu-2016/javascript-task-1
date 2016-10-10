b26f4d68f462e8a105318fa90c795da69bddb118
AlexeyCherepanov <alex.nvs.96@yandex.ru>
2016-10-10 11:44:26 +0500

    final

--- roman-time.js
+++ roman-time.js
@@ -5,97 +5,47 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 
-
-
+var unitsRoman = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+var tensRoman = ["", "X", "XX", "XXX", "XL", "L"];
 
 function convertUnitsRoman(units) {
-    var roman;
-    switch (units) {
-        case 0:
-            roman = "";
-            break;
-        case 1:
-            roman = "I";
-            break;
-        case 2:
-            roman = "II";
-            break;
-        case 3:
-            roman = "III";
-            break;
-        case 4:
-            roman = "IV";
-            break;
-        case 5:
-            roman = "V";
-            break;
-        case 6:
-            roman = "VI";
-            break;
-        case 7:
-            roman = "VII";
-            break;
-        case 8:
-            roman = "VIII";
-            break;
-        case 9:
-            roman = "IX";
-            break;
-    }
-    return roman
+    return unitsRoman[units];
 }
 
 function convertTensRoman(tens) {
-    var roman;
-    switch (tens) {
-        case 0:
-            roman = "";
-            break;
-        case 1:
-            roman = "X";
-            break;
-        case 2:
-            roman = "XX";
-            break;
-        case 3:
-            roman = "XXX";
-            break;
-        case 4:
-            roman = "XL";
-            break;
-        case 5:
-            roman = "L";
-            break;
+    return tensRoman[tens];
+}
+
+function timeInRoman(time) {
+    var splitedTime = time.split("");
+    var hourTen = convertTensRoman(parseInt(splitedTime[0], 10));
+    var hourUnits = convertUnitsRoman(parseInt(splitedTime[1], 10));
+    var minuteTen = convertTensRoman(parseInt(splitedTime[2], 10));
+    var minuteUnits = convertUnitsRoman(parseInt(splitedTime[3], 10));
+    if (hourTen + hourUnits === "") {
+        hourUnits = "N";
     }
-    return roman
+    if (minuteTen + minuteUnits === "") {
+        minuteUnits = "N";
+    }
+    time = hourTen + hourUnits + ":" + minuteTen + minuteUnits;
+
+    return time;
 }
 
 function romanTime(time) {
-
-    if (!/^([0-2][0-3]|[0-1]\d):[0-5]\d$/.test(time)) {
+    if (!/^(2[0-3]||[0-1]\d):[0-5]\d$/.test(time)) {
         throw new TypeError("Неверное время");
     }
-
-
-    if (/^([0-2][0-3]|[0-1]\d):[0-5]\d$/.test(time)) {
+    if (time === "00:00") {
+        time = "N:N";
+    }
+    if (/^(2[0-3]|[0-1]\d):[0-5]\d$/.test(time)) {
         time = time.replace(/:/, "");
-        var splitedTime = time.split("");
-        var hourTen = convertTensRoman(parseInt(splitedTime[0]));
-        var hourUnits = convertUnitsRoman(parseInt(splitedTime[1]));
-        var minuteTen = convertTensRoman(parseInt(splitedTime[2]));
-        var minuteUnits = convertUnitsRoman(parseInt(splitedTime[3]));
-        if (hourTen + hourUnits == "") {
-            hourUnits = "N";
-        }
-        if (minuteTen + minuteUnits == "") {
-            minuteUnits = "N";
-        }
-        time = hourTen + hourUnits + ":" + minuteTen + minuteUnits;
-
+        time = timeInRoman(time);
     }
 
     return time;
-
 }
 
 
