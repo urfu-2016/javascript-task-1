9e2baed741a532a0016ed13403c0f20587a59382
VeraTimox <v.timoxina123@yandex.ru>
2016-10-10 19:48:23 +0500

    Вера Тимохина

--- roman-time.js
+++ roman-time.js
@@ -4,26 +4,33 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function isTime(time){
-    if (time === undefined){
+function isTime(time) {
+    var trueTime = false;
+    if (time === undefined) {
         throw new TypeError();
     }
     if (time.charAt(2) === ":") {
-        for (var i = 0; i < time.length; i++) {
-            if (parseInt(time.charAt(i)) !== parseInt(time.charAt(i)) && i !== 2) {
-                throw new TypeError();
-            }
-            else if (parseInt(time.charAt(0) + time.charAt(1)) > 23) {
-                throw new TypeError();
-            }
-            else if (parseInt(time.charAt(3) + time.charAt(4)) > 59) {
-                throw new TypeError();
-            }
-        }
+        trueTime = isNumber(time);
     }
     else {
         throw new TypeError();
     }
+    
+    return trueTime;
+}
+function isNumber(time) {
+    for (var i = 0; i < time.length; i++) {
+        if (parseInt(time.charAt(i)) !== parseInt(time.charAt(i)) && i !== 2) {
+            throw new TypeError();
+        }
+        else if (parseInt(time.charAt(0) + time.charAt(1)) > 23) {
+            throw new TypeError();
+        }
+        else if (parseInt(time.charAt(3) + time.charAt(4)) > 59) {
+            throw new TypeError();
+        }
+    }
+    
     return true;
 }
 function reformInArabic(units, one, five) {
@@ -46,21 +53,26 @@ function reformInArabic(units, one, five) {
         }
     }
     else if (units === 9) {
-        romanUnits = "I" + "X";
+        romanUnits = "I";
+        romanUnits = romanUnits + "X";
     }
+    
     return romanUnits;
 }
 function isZero(a, b) {
     if (parseInt(a) === 0 && parseInt(b) === 0) {
         return true;
     }
+    
     return false;
 }
 function romanTime(time) {
+    var hours = "";
+    var minutes = "";
     if (isTime(time)) {
-        var hours = reformInArabic(parseInt(time.charAt(0)), "X", "L") +
+        hours = reformInArabic(parseInt(time.charAt(0)), "X", "L") +
         reformInArabic(parseInt(time.charAt(1)), "I", "V");
-        var minutes = reformInArabic(parseInt(time.charAt(3)), "X", "L") +
+        minutes = reformInArabic(parseInt(time.charAt(3)), "X", "L") +
         reformInArabic(parseInt(time.charAt(4)), "I", "V");
         if (isZero(time.charAt(0), time.charAt(1))) {
             hours = "N";
@@ -69,6 +81,7 @@ function romanTime(time) {
             minutes = "N";
         }
     }
+
     return hours + ":" + minutes;
 }
 module.exports = romanTime;
