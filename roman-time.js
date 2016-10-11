71699803157adf780a0b37f977b8696d48b13ea7
Burgundia <plusha.pliusha@yandex.ru>
2016-10-11 11:16:43 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -6,7 +6,15 @@
  */
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    return time;
+    if (time.Length > 5 || time === undefined || time === null)
+        throw new TypeError();
+    var parsedTime = time.split(':');
+    var hours = parseInt(parsedTime[0], 10);
+    var min = parseInt(parsedTime[1], 10);
+    if (isNan(hours) || isNan(min))
+        throw new TypeError();
+    var rom = ["N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"];
+    return rom[hours % 12] + ":" + rom[min % 12];
 }
 
 module.exports = romanTime;
