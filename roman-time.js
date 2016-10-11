4bb0e554bb9736f89cebb14e510b85f4882c7060
Bystrova Viktoria <bvvvvb94.94@mail.ru>
2016-10-11 22:57:26 +0500

    Перевод времени в римское

--- roman-time.js
+++ roman-time.js
@@ -5,8 +5,37 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    if (typeof time !== 'string') {
+        throw new TypeError('Неверное время');
+    }
+
+    var arr = time.split(':');
+    var hours = parseFloat(arr[0]);
+    var minutes = parseFloat(arr[1]);
+
+    if ((isNaN(hours)) || (isNaN(minutes)) || isWrongTime(hours, minutes)) {
+        throw new TypeError('Неверное время');
+    }
+
+    return convertToRoman(hours) + ':' + convertToRoman(minutes);
+}
+
+function isWrongTime(hours, minutes) {
+    return (hours !== parseInt(hours)) || (minutes !== parseInt(minutes)) ||
+        (hours < 0) || (hours > 23) || (minutes < 0) || (minutes > 59);
+}
+
+function convertToRoman(number) {
+    if (number === 0) {
+        return 'N';
+    }
+
+    var tens = parseInt(number / 10);
+    var ones = number % 10;
+    var romanTens = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+    var romanOnes = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+
+    return romanTens[tens] + romanOnes[ones];
 }
 
 module.exports = romanTime;
