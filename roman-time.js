fa692a7d7eded71a6fe3ef3f754ac0cbfc30bca4
danlunin <daniellunin@mail.ru>
2016-10-12 01:52:56 +0500

    fixed complexity

--- roman-time.js
+++ roman-time.js
@@ -1,63 +1,68 @@
 'use strict';
+
 function checkForErrors(time, numbers){
-    if (time.length !== 5 || numbers.length !== 2 || numbers[0].length !== 2 || numbers[1].length !== 2) {
-        throw new TypeError('Неверное время');
-    }
     try {
         parseInt(numbers[0], 10);
         parseInt(numbers[1], 10);
-    } catch(e) {
+    } catch (e) {
         throw new TypeError('Неверное время');
     }
-    if (parseInt(numbers[0], 10) > 23 || parseInt(numbers[0], 10) <0){
+    if (time.length !== 5 || numbers.length !== 2 || numbers[0].length !== 2 || numbers[1].length !== 2) {
         throw new TypeError('Неверное время');
     }
-    if (parseInt(numbers[1], 10) > 59 || parseInt(numbers[1], 10) <0) {
+    else if (parseInt(numbers[0], 10) > 23 || parseInt(numbers[0], 10) < 0) {
+        throw new TypeError('Неверное время');
+    }
+    else if (parseInt(numbers[1], 10) > 59 || parseInt(numbers[1], 10) < 0) {
         throw new TypeError('Неверное время');
     }
 }
+
+function parseTime(numbers, finalString, romanianDict, k) {
+    var justNumber = parseInt(numbers[k], 10);
+    if (justNumber > 50) {
+        justNumber -= 50;
+        finalString += 'L';
+    }
+    else if (justNumber > 40) {
+        justNumber -= 40;
+        finalString += 'IL';
+    }
+    var amountOfX = (justNumber - justNumber % 10) / 10;
+    for (var i = 0; i < amountOfX; i++) {
+        finalString += 'X';
+    }
+    justNumber = justNumber % 10;
+    if (justNumber !== 0) {
+        finalString += romanianDict[justNumber];
+    }
+    else {
+        finalString += 'N';
+    }
+    if (k < 1) {
+        finalString += ':';
+    }
+    return finalString
+
+}
 function romanTime(time) {
     var romanianDict = {
-        1 : 'I',
-        2 : 'II',
-        3 : 'III',
-        4 : 'IV',
-        5 : 'V',
-        6 : 'VI',
-        7 : 'VII',
-        8 : 'VIII',
-        9 : 'IX',
-        10 : 'X'
+        1: 'I',
+        2: 'II',
+        3: 'III',
+        4: 'IV',
+        5: 'V',
+        6: 'VI',
+        7: 'VII',
+        8: 'VIII',
+        9: 'IX',
+        10: 'X'
     };
     var numbers = time.split(':');
     checkForErrors(time, numbers);
-    var finalString='';
-    for (var k =0; k<2;k++) {
-        var justNumber = parseInt(numbers[k], 10);
-        if (justNumber === 0) {
-            finalString += 'N';
-        }
-        else {
-            if (justNumber > 50) {
-                justNumber -= 50;
-                finalString += 'L';
-            }
-            else if (justNumber > 40) {
-                justNumber -= 40;
-                finalString += 'IL';
-            }
-            var amountOfX = (justNumber - justNumber % 10) / 10;
-            for (var i = 0; i < amountOfX; i++) {
-                finalString += 'X';
-            }
-            justNumber = justNumber % 10;
-            if (justNumber !== 0) {
-                finalString += romanianDict[justNumber];
-            }
-        }
-        if (k<1) {
-            finalString += ':';
-        }
+    var finalString = '';
+    for (var j =0; j < 2; j++) {
+        finalString = parseTime(numbers, finalString, romanianDict, j)
     }
     return finalString;
 }
