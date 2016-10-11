a076f14c266b72a41a1cd9b7213c76d2f4b30800
Mrbowtiesmind <kuzn-eg@mail.ru>
2016-10-11 15:55:10 +0500

    Переработанный вариант решения

--- roman-time.js
+++ roman-time.js
@@ -5,38 +5,35 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    try {
-         if (time.length != 5) throw new TypeError();
-         var hour = time.substr(0,2);
-         var min = time.substr(3,5);
-         if (isNaN(hour) === true) throw new TypeError();
-         if (isNaN(min) === true) throw new TypeError();
-                            }
-         catch (err) {throw new TypeError();}        
-    var RomHour = '';
-    var RomMin = '';
-    var arab_mind = [1,4,5,9,10,40,50];
-    var rom_mind = ["I","IV","V","IX","X","XL","L"];
-    var n = arab_mind.length-1;
-    if (hour === 0) RomHour = "N";
-    if (min === 0) RomMin = "N";
-    while (hour>0) {
-        if (hour >= arab_mind[n]){
-            RomHour += rom_mind[n];
-            hour -= arab_mind[n];
+           try {
+         if (time.length!=5){throw new TypeError()};
+         var hour=time.substr(0, 2);
+         var min=time.substr(3, 5);
+         if (isNaN(hour)===true){throw new TypeError()};
+         if (isNaN(min)===true){throw new TypeError()};}
+         catch (err){throw new TypeError();}
+    var min = time.substr(3,5);   
+    var ArabMind = [1, 4, 5, 9, 10, 40, 50];
+    var RomMind = ["I", "IV", "V", "IX", "X", "XL", "L"];
+    var n=ArabMind.length - 1;
+    if (hour == 0){RomHour = "N"};
+    if (min == 0){RomMin = "N"};
+    while (hour > 0) {
+        if (hour >= ArabMind[n]) {
+            RomHour += RomMind[n];
+            hour -= ArabMind[n];
         }
-        else n--;//второй цикл
+        else {n--};
     }
-    var k = arab_mind.length-1;
-       while (min>0) {
-        if (min >= arab_mind[k]){
-            RomMin += rom_mind[k];
-            min -= arab_mind[k];
+    var k = ArabMind.length - 1;
+    while (min > 0) {
+        if (min >= ArabMind[k]){
+            RomMin += RomMind[k];
+            min -= ArabMind[k];
         }
-        else k--;
+        else {k--};
     }
+    document.write(RomHour + ":" + RomMin)
     return time;
 }
-
 module.exports = romanTime;
