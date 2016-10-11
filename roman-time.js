5f5b0655217faa6a60476787b4869cd7854ffe5a
Малахов Александр <ySunural@yandex.ru>
2016-10-11 15:23:26 +0600

    v2 - map заменил объектом

--- roman-time.js
+++ roman-time.js
@@ -8,23 +8,18 @@ function romanTime(time) {
     // Немного авторского кода и замечательной магии
     try {
         // var timeArr = [2];
-        var hours;
-        var minutes;
-        var hoursRom;
-        var minutesRom;
-
         // timeArr = time.split(':', 2);
-        hours = parseInt((time.split(':', 2))[0], 10);
-        minutes = parseInt((time.split(':', 2))[1], 10);
+        var hours = parseInt((time.split(':', 2))[0], 10);
+        var minutes = parseInt((time.split(':', 2))[1], 10);
 
         if (isNaN(hours) || isNaN(minutes) || hours > 23 ||
             hours < 0 || minutes > 59 || minutes < 0) {
             throw new TypeError('TypeError: Неверное время');
         }
-        else {
-            hoursRom = convertNum(hours);
-            minutesRom = convertNum(minutes);
-            time = hoursRom + ":" + minutesRom;}
+
+        var hoursRom = convertNum(hours);
+        var minutesRom = convertNum(minutes);
+        time = hoursRom + ":" + minutesRom;
     }
     catch (e) {
         console.info('TypeError: Неверное время');
@@ -35,22 +30,24 @@ function romanTime(time) {
 
 function convertNum(num) {
     // Карта соответствия чисел
-    var accordMap = new Map();
-    accordMap.set(0, '');
-    accordMap.set(1, 'I');
-    accordMap.set(2, 'II');
-    accordMap.set(3, 'III');
-    accordMap.set(4, 'IV');
-    accordMap.set(5, 'V');
-    accordMap.set(6, 'VI');
-    accordMap.set(7, 'VII');
-    accordMap.set(8, 'VIII');
-    accordMap.set(9, 'IX');
-    accordMap.set(10, 'X');
-    accordMap.set(20, 'XX');
-    accordMap.set(30, 'XXX');
-    accordMap.set(40, 'XL');
-    accordMap.set(50, 'L');
+    var accordMap = {
+        0: '',
+        1: 'I',
+        2: 'II',
+        3: 'III',
+        4: 'IV',
+        5: 'V',
+        6: 'VI',
+        7: 'VII',
+        8: 'VIII',
+        9: 'IX',
+        10: 'X',
+        20: 'XX',
+        30: 'XXX',
+        40: 'XL',
+        50: 'L',
+    };
+
     // остаток от деления (для начала равен самому числу)
     num = parseInt(num, 10);
 
@@ -60,10 +57,10 @@ function convertNum(num) {
 
     var dec;
     var unit;
-
+    var rom;
     dec = (parseInt(num / 10)) * 10;
     unit = num % 10;
-    var rom = accordMap.get(dec) + accordMap.get(unit);
+    rom = accordMap[dec] + accordMap[unit];
 
     return rom;
 }
