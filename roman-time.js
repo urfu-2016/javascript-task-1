b84425decd1b4def7d801fc63175c4149403fbc5
KrazyD <Svalovdmitrii@mail.ru>
2016-10-12 02:45:46 +0500

    Новая строчка перед коментами

--- roman-time.js
+++ roman-time.js
@@ -1,10 +1,5 @@
 'use strict';
 
-/**
- * @param {String} time – время в формате HH:MM (например, 09:05)
- * @returns {String} – время римскими цифрами (IX:V)
- */
-
 function romanTime(time) {
     if (checkCorrectTime(time.split(''))) {
         var splitTime = time.split(':');
