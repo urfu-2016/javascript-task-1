3a114a0ee8aaf16519cbb7cf81a6c9083265f9f0
Irina <irinachkas.96@gmail.com>
2016-10-11 18:28:21 +0500

    test6

--- roman-time.js
+++ roman-time.js
@@ -6,43 +6,45 @@
  */
 
 function romanTime(time) {
-    if (!checkTime(time,hours,minutes)) {
-        throw new TypeError()
-    };
+    if (!checkTime(time)) {
+        throw new TypeError();
+    }
     var hoursAndMinutes = time.split(':');
     var hours = parseInt(hoursAndMinutes[0]);
     var minutes = parseInt(hoursAndMinutes[1]);
+
     return (createNewString(hours)) + ':' +
            (createNewString(minutes));
 }
 
 function createNewString(number) {
-  var decimal = {
-        '0': '',
-        '1': 'I',
-        '2': 'II',
-        '3': 'III',
-        '4': 'IV',
-        '5': 'V',
-        '6': 'VI',
-        '7': 'VII',
-        '8': 'VIII',
-        '9': 'IX'
+    var decimal = {
+      '0': '',
+      '1': 'I',
+      '2': 'II',
+      '3': 'III',
+      '4': 'IV',
+      '5': 'V',
+      '6': 'VI',
+      '7': 'VII',
+      '8': 'VIII',
+      '9': 'IX'
   };
-  var tens = {
-        '0': '',
-        '1': 'X',
-        '2': 'XX',
-        '3': 'XXX',
-        '4': 'XL',
-        '5': 'L',
-        '6': 'LX'
+    var tens = {
+      '0': '',
+      '1': 'X',
+      '2': 'XX',
+      '3': 'XXX',
+      '4': 'XL',
+      '5': 'L'
   };
-    return number === 0 ? 'N' : tens[Math.floor(number / 10)] + decimal[number % 10]
+
+    return number === 0 ? 'N' : tens[Math.floor(number / 10)] + decimal[number % 10];
 }
 
 function checkTime(time) {
-    var reTime = /^(([0-1][0-9])|(2[0-3]))[:][0-5][0-9]$/;
+    var reTime = /^(([0,1]\d)|(2[0-3]))[:][0-5]\d$/;
+
     return (reTime.test(String(time)));
 }
 
