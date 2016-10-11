c3d1f167b68606dcdb4818664f5966761e81f911
Мамонов Антон <graf.M.96@yandex.ru>
2016-10-12 03:28:22 +0500

    after eslint

--- roman-time.js
+++ roman-time.js
@@ -8,60 +8,62 @@
 var arabicNumbers = [0, 1, 4, 5, 9, 10, 40, 50];
 var romanNumbers = ['N', 'I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
 
-
-
-function checkStringToStandartTime(time){
-    if (time.length  != 5)
+function checkStringToStandartTime(time) {
+    if (time.length !== 5) {
         return false;
-    return (!isNaN(parseInt(time[0])) && !isNaN(parseInt(time[1]))
-            && time[2] === ":" && !isNaN(parseInt(time[3]))
-            && !isNaN(parseInt(time[4])));
+    }
+
+    return (!isNaN(parseInt(time[0])) && !isNaN(parseInt(time[1])) &&
+            time[2] === ':' && !isNaN(parseInt(time[3])) &&
+            !isNaN(parseInt(time[4])));
 }
 
 
-function checkToCorrect(time){
-    if (typeof(time) != 'string')
+function checkToCorrect(time) {
+    if (typeof(time) !== 'string') {
         return false;
-    if (!checkStringToStandartTime(time))
+    }
+    if (!checkStringToStandartTime(time)) {
         return false;
+    }
     var timeInArray = time.split(':');
     var hours = parseInt(timeInArray[0]);
     var minutes = parseInt(timeInArray[1]);
-    return 0 <= hours && hours <= 23 && 0 <= minutes && minutes <= 59;
+
+    return hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59;
 }
 
 
-function translateArabNumberToRoman(number){
-    if(!number)
+function translateArabNumberToRoman(number) {
+    if (!number) {
         return romanNumbers[0];
+    }
     var arabicNumberInStr = '';
     var indexOfRomanNumber = romanNumbers.length - 1;
-    while(number > 0)
-    {
-        if(number >= arabicNumbers[indexOfRomanNumber])
-        {
+    while (number > 0) {
+        if (number >= arabicNumbers[indexOfRomanNumber]) {
             arabicNumberInStr += romanNumbers[indexOfRomanNumber];
             number -= arabicNumbers[indexOfRomanNumber];
-        }
-        else
-        {
+        } else {
             indexOfRomanNumber--;
         }
-    
     }
+
     return arabicNumberInStr;
 }
 
 
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    if (!checkToCorrect(time))
+    if (!checkToCorrect(time)) {
         throw new TypeError();
+    }
     var timeInArray = time.split(':');
     var hours = parseInt(timeInArray[0]);
     var minutes = parseInt(timeInArray[1]);
     var hourInRoman = translateArabNumberToRoman(hours);
     var minutesInRoman = translateArabNumberToRoman(minutes);
+
     return hourInRoman + ':' + minutesInRoman;
 }
 
