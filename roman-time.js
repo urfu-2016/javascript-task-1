499192e98d81a7b398956541bba81814b182c5e9
Bizilizi <kabara.bizilizi@gmail.com>
2016-10-11 23:42:21 +0500

    Code Style

--- roman-time.js
+++ roman-time.js
@@ -1,33 +1,47 @@
 'use strict';
 
 /**
- * @param {String} time – âðåìÿ â ôîðìàòå HH:MM (íàïðèìåð, 09:05)
- * @returns {String} – âðåìÿ ðèìñêèìè öèôðàìè (IX:V)
+ * @param {String} element – перевод из арабской в римскую
+ * @returns {String} – римские цифры
  */
 function concatinationTime(element) {
-    var onesList = ['N','I','II','III','IV','V','VI','VII','VIII','IX'];
-    var tensList = ['','X','XX','XXX','XL','L','LX'];
+    var onesList = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    var tensList = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX'];
 
-    if (element == 0) return 'N';
-    else return tensList[Math.floor(element)] + onesList[element % 10];
+    if (element === 0) {
+        return 'N';
+    } else {
+        return tensList[Math.floor(element / 10)] + onesList[element % 10];
+    }
 }
 
+/**
+ * @param {String} time – время в формате HH:MM (например, 09:05)
+ * @returns {String} – время римскими цифрами (IX:V)
+ */
 function romanTime(time) {
     var regexp = /[0-9][0-9]:[0-9][0-9]+$/i;
 
     if (regexp.test(time)) {
 
-        var timeList = time.split(':').map(function (listElement){return parseInt(listElement,10)});
+        var timeList = time.split(':').map(function(listElement) {
+            return parseInt(listElement, 10)
+        });
 
-        var firstElement =  timeList[0];
+        var firstElement = timeList[0];
         var secondElement = timeList[1];
 
         if (firstElement >= 0 && firstElement <= 24 && secondElement >= 0 && secondElement <= 60) {
-          console.log(concatinationTime(firstElement) + ':' + concatinationTime(secondElement));
-          return;
-        } else throw new TypeError;
+            console.log(concatinationTime(firstElement) + ':' + concatinationTime(secondElement));
+            return;
+        } else {
+            throw new TypeError;
+        }
 
-    } else throw new TypeError;
+    } else {
+        throw new TypeError;
+    }
 }
 
+romanTime('12:50 ');
 module.exports = romanTime;
