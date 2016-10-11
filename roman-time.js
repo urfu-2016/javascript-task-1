f10dd5aa1df0aee3e336c56588cb97efa7786ce4
Firsov Kolya <kolya.firsov@gmail.com>
2016-10-11 07:11:02 +0500

    7:10

--- roman-time.js
+++ roman-time.js
@@ -47,7 +47,7 @@ function notNull(time) {
 }
 
 function notNan(arr) {
-    if (arr[0] === ' ' || arr[1] === ' ' || arr[0].length > 2 || arr[1].length > 2) {
+    if (arr[0] === ' ' || arr[1] === ' ' || arr[0].length !== 2 || arr[1].length !== 2) {
         return true;
     }
 
