5bfa41587aac0cef50b0ecd8676a4a7bc3ae7293
FairTex <makarov906@gmail.com>
2016-10-12 02:55:01 +0500

    Xmmmm...

--- roman-time.js
+++ roman-time.js
@@ -1,36 +1,26 @@
 'use strict';
 
+var magicDigits = [50, 40, 10, 9, 5, 4, 1];
+var dict = { 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I' };
+
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 
-var magicDigits = [50, 40, 10, 9, 5, 4, 1];
-var dict = { 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I' };
-
 function romanTime(time) {
-    var hour = time.split(':')[0];
-    var minute = time.split(':')[1];
     if (isCorrect(time)) {
-        return toRoman(hour) + ':' + toRoman(minute);
+        return toRoman(time.split(':')[0]) + ':' + toRoman(time.split(':')[1]);
     }
     throw new TypeError('Неверное время', 'roman-time.js', 16);
 }
 
 function isCorrect(time) {
-    var hour = time.split(':')[0];
-    var minute = time.split(':')[1];
+    var hour = parseInt(time.split(':')[0]);
+    var minute = parseInt(time.split(':')[1]);
 
-    hour = parseInt(hour);
-    minute = parseInt(minute);
-
-    if (isNaN(hour) || isNaN(minute)) {
-        return false;
-    }
-    else if (hour < 0 || hour > 23 || minute < 0 || minute > 59) {
-        return false;
-    }
-    else return true;
+    return (!isNaN(hour) && !isNaN(minute) &&
+        hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59);
 }
 
 function toRoman(digit) {
@@ -47,18 +37,6 @@ function toRoman(digit) {
             }
         }
     });
-
-    // for (var i = 0; i < magicDigits.length; i++) {
-    //     var current = magicDigits[i];
-    //     if (digit >= current) {
-    //         var a = parseInt(digit / current);
-    //         digit -= a * current;
-    //         while (a > 0) {
-    //             str += dict[current];
-    //             a--;
-    //         }
-    //     }
-    // }
 
     return str === '' ? 'N' : str;
 }
