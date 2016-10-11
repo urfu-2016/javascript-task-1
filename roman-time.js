7c8b80687958d11828b1e2954f3425cee576c359
KrazyD <Svalovdmitrii@mail.ru>
2016-10-12 02:41:21 +0500

    Нашел свою ошибку 2

--- roman-time.js
+++ roman-time.js
@@ -4,6 +4,7 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
+
 function romanTime(time) {
     if (checkCorrectTime(time.split(''))) {
         var splitTime = time.split(':');
