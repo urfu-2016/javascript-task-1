d0813a13c1178e9426742a2b3468fcaf66e39983
Irina <irinachkas.96@gmail.com>
2016-10-11 18:31:27 +0500

    test7

--- roman-time.js
+++ roman-time.js
@@ -19,25 +19,25 @@ function romanTime(time) {
 
 function createNewString(number) {
     var decimal = {
-      '0': '',
-      '1': 'I',
-      '2': 'II',
-      '3': 'III',
-      '4': 'IV',
-      '5': 'V',
-      '6': 'VI',
-      '7': 'VII',
-      '8': 'VIII',
-      '9': 'IX'
-  };
+        '0': '',
+        '1': 'I',
+        '2': 'II',
+        '3': 'III',
+        '4': 'IV',
+        '5': 'V',
+        '6': 'VI',
+        '7': 'VII',
+        '8': 'VIII',
+        '9': 'IX'
+    };
     var tens = {
-      '0': '',
-      '1': 'X',
-      '2': 'XX',
-      '3': 'XXX',
-      '4': 'XL',
-      '5': 'L'
-  };
+        '0': '',
+        '1': 'X',
+        '2': 'XX',
+        '3': 'XXX',
+        '4': 'XL',
+        '5': 'L'
+    };
 
     return number === 0 ? 'N' : tens[Math.floor(number / 10)] + decimal[number % 10];
 }
