9bedaa25bbaec47e19028ab16e4758c5f843d998
FairTex <makarov906@gmail.com>
2016-10-12 01:31:00 +0500

    First commit

--- roman-time.js
+++ roman-time.js
@@ -4,9 +4,47 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
+
+var magicDigits = [50, 40, 10, 9, 5, 4, 1];
+var dict = { 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I' };
+
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    var hour = time.split(':')[0];
+    var minute = time.split(':')[1];
+    if (isCorrect(time))
+        return toRoman(hour) + ':' + toRoman(minute);
+    throw new TypeError("Неверное время", "roman-time.js", 16);
+}
+
+function isCorrect(time) {
+    var hour = time.split(':')[0];
+    var minute = time.split(':')[1];
+
+    hour = parseInt(hour);
+    minute = parseInt(minute);
+
+    if (isNaN(hour) || isNaN(minute))
+        return false;
+    if (hour < 0 || hour > 23 || minute < 0 || minute > 59)
+        return false;
+    return true;
+}
+
+function toRoman(digit) {
+    digit = parseInt(digit);
+    var str = '';
+    for (var i = 0; i < magicDigits.length; i++) {
+        var current = magicDigits[i];
+        if (digit >= current) {
+            var a = parseInt(digit / current);
+            digit -= a * current;
+            while (a > 0) {
+                str += dict[current];
+                a--;
+            }
+        }
+    }
+    return str == '' ? 'N' : str;
 }
 
 module.exports = romanTime;
