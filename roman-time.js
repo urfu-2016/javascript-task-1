9588156641b387bc22b7918e50c2a3d2d89ba353
Burgundia <plusha.pliusha@yandex.ru>
2016-10-11 18:19:28 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -6,23 +6,41 @@
  */
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    if (time.Length > 5 || time === undefined || time === null) {
-        throw new TypeError();
-    }
+    checkTime(time);
     if (!time.includes(':')) {
         throw new TypeError();
     }
     var parsedTime = time.split(':');
     var hours = parseInt(parsedTime[0], 10);
     var min = parseInt(parsedTime[1], 10);
+    var digits = ["N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    var decade = ["X", "XX", "XXX", "XL", "L"];
+    var romTime;
+    return time;
+}
+
+function checkTime(time) {
+    checkNull(time);
+    checkNumb(time);
+    checkNaN(time);
+}
+
+function checkNaN(time) {
     if (Number.isNaN(hours) || Number.isNaN(min)) {
         throw new TypeError();
     }
-    if (hours > 23 || hours < 0 || min > 59 || min <0) {
-        throw new TypeError()
-    }
+}
 
-    return time;
+function checkNumb(time) {
+    if (hours > 23 || hours < 0 || min > 59 || min < 0) {
+        throw new TypeError();
+    }
+}
+
+function checkNull(time) {
+    if (time.Length > 5 || time === undefined || time === null) {
+        throw new TypeError();
+    }  
 }
 
 module.exports = romanTime;
