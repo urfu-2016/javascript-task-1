714418916a85f9d0cf8f754dd07053537a6c5603
Rubtsov Ilya <Rubthov-Ilya1997@yandex.ru>
2016-10-11 22:16:53 +0500

    Римские числа ч.4

--- roman-time.js
+++ roman-time.js
@@ -7,40 +7,42 @@
 var romanArrayTens = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
 var romanArrayOnes = ['X', 'XX', 'XXX', 'XL', 'L'];
 var splitTime;
-var separator = ':';
-function translateToRoman(string) {
-    if (string[1] === 0) {
-        if (string[0] === 0) {
+function translateToRoman(string, index) {
+    if (string[1] === '0') {
+        if (string[0] === '0') {
             string = romanArrayTens[0];
+        } else {
+            string = romanArrayOnes[parseInt(splitTime[index][0]) - 1];
         }
-        string = romanArrayOnes[parseInt(splitTime[0][0]) - 1];
-    }
-    if (string[0] === 0) {
-        string = romanArrayTens[parseInt(splitTime[0])];
+    } else if (string[0] === '0') {
+        string = romanArrayTens[parseInt(splitTime[index][1])];
     } else {
-        string = romanArrayOnes[parseInt(splitTime[0][0]) - 1] +
-            romanArrayTens[parseInt(splitTime[0][1])];
+        string = romanArrayOnes[parseInt(splitTime[index][0]) - 1] +
+            romanArrayTens[parseInt(splitTime[index][1])];
     }
 
     return string;
 }
 var minutesInteger;
 var hoursInteger;
+var romanHours;
+var romanMinutes;
+var separator = ':';
 function romanTime(time) {
     splitTime = time.split(separator);
     hoursInteger = parseInt(splitTime[0]);
     minutesInteger = parseInt(splitTime[1]);
     if (hoursInteger < 24 && hoursInteger >= 0) {
-        translateToRoman(splitTime[0]);
+        romanHours = translateToRoman(splitTime[0], 0);
         if (minutesInteger < 60 && minutesInteger >= 0) {
-            translateToRoman(splitTime[1]);
+            romanMinutes = translateToRoman(splitTime[1], 1);
         } else {
             throw new TypeError('TypeError: Неверное время');
         }
     } else {
         throw new TypeError('TypeError: Неверное время');
     }
-    time = splitTime[0] + ':' + splitTime[1];
+    time = romanHours + ':' + romanMinutes;
 
     return time;
 }
