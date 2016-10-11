b1bed031582730d7b4eaed9bfe12f5faa1d6d93d
danlunin <daniellunin@mail.ru>
2016-10-12 01:25:46 +0500

    semicolon

--- roman-time.js
+++ roman-time.js
@@ -1,63 +1,65 @@
 'use strict';
-
-function romanTime(time) {
-    var romanianDict = {
-        1:'I',
-        2:'II',
-        3:'III',
-        4:'IV',
-        5:'V',
-        6:'VI',
-        7:'VII',
-        8:'VIII',
-        9:'IX',
-        10:'X'
-    };
-    var numbers = time.split(':');
-    if (time.length != 5 || numbers.length !=2 || numbers[0].length != 2 || numbers[1].length !=2) {
-        throw new TypeError('Неверное время')
+function checkForErrors(time, numbers){
+    if (time.length !== 5 || numbers.length !== 2 || numbers[0].length !== 2 || numbers[1].length !== 2) {
+        throw new TypeError('Неверное время');
     }
-    try{
+    try {
         parseInt(numbers[0], 10);
         parseInt(numbers[1], 10);
-    }catch(e) {
-        throw new TypeError('Неверное время')
+    } catch(e) {
+        throw new TypeError('Неверное время');
     }
     if (parseInt(numbers[0], 10) > 23 || parseInt(numbers[0], 10) <0){
-        throw new TypeError('Неверное время')
+        throw new TypeError('Неверное время');
     }
     if (parseInt(numbers[1], 10) > 59 || parseInt(numbers[1], 10) <0) {
-        throw new TypeError('Неверное время')
+        throw new TypeError('Неверное время');
     }
+}
+function romanTime(time) {
+    var romanianDict = {
+        1 : 'I',
+        2 : 'II',
+        3 : 'III',
+        4 : 'IV',
+        5 : 'V',
+        6 : 'VI',
+        7 : 'VII',
+        8 : 'VIII',
+        9 : 'IX',
+        10 : 'X'
+    };
+    var numbers = time.split(':');
+    checkForErrors(time, numbers);
     var finalString='';
     for (var k =0; k<2;k++) {
         var justNumber = parseInt(numbers[k], 10);
-        if (justNumber == 0) {
-            finalString += 'N'
+        if (justNumber === 0) {
+            finalString += 'N';
         }
         else {
             if (justNumber > 50) {
                 justNumber -= 50;
-                finalString += 'L'
+                finalString += 'L';
             }
             else if (justNumber > 40) {
                 justNumber -= 40;
-                finalString += 'IL'
+                finalString += 'IL';
             }
             var amountOfX = (justNumber - justNumber % 10) / 10;
             for (var i = 0; i < amountOfX; i++) {
                 finalString += 'X';
             }
             justNumber = justNumber % 10;
-            if (justNumber !=0) {
+            if (justNumber !== 0) {
                 finalString += romanianDict[justNumber];
             }
         }
-        if (k<1)finalString += ':'
+        if (k<1) {
+            finalString += ':';
+        }
     }
     return finalString;
 }
-
-
 
 module.exports = romanTime;
