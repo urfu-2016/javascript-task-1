5be1d1c0d78d34663034c36a2be86625cda3eece
Rubtsov Ilya <Rubthov-Ilya1997@yandex.ru>
2016-10-11 09:41:52 +0500

    Римские числа исправление ч.1

--- roman-time.js
+++ roman-time.js
@@ -4,67 +4,67 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-var switch_time;
 new TypeError(["TypeError: Неверное время"]);
-function romanTens(switch_time) {
-    if (switch_time[0] === '0'){
-        switch_time[1] = 'N';
+
+function romanTens(switchTime) {
+    if (switchTime[0] === '0') {
+        switchTime[1] = 'N';
     }
-    switch (switch_time[1]){
+    switch (switchTime[1]) {
         case '0' :
             break;
         case '1' :
-            switch_time[1] = 'I';
+            switchTime[1] = 'I';
             break;
         case '2' :
-            switch_time[1] = 'II';
+            switchTime[1] = 'II';
             break;
         case '3' :
-            switch_time[1] = 'III';
+            switchTime[1] = 'III';
             break;
         case '4' :
-            switch_time[1] = 'IV';
+            switchTime[1] = 'IV';
             break;
         case '5' :
-            switch_time[1] = 'V';
+            switchTime[1] = 'V';
             break;
         case '6' :
-            switch_time[1] = 'VI';
+            switchTime[1] = 'VI';
             break;
         case '7' :
-            switch_time[1] = 'VII';
+            switchTime[1] = 'VII';
             break;
         case '8' :
-            switch_time[1] = 'VIII';
+            switchTime[1] = 'VIII';
             break;
         case '9' :
-            switch_time[1] = 'IX';
+            switchTime[1] = 'IX';
             break;
         default: TypeError();
     }
 
-    return switch_time;
+    return switchTime;
 }
 var hours;
 var minutes;
 var separator = ':';
 var i = 0;
 function romanTime(time) {
-    try{
-        while (time[i] !== separator){
+    try {
+        while (time[i] !== separator) {
             hours[i] = time[i];
             i++;
         }
         i++;
-        while (i  < time.length){
+        while (i < time.length) {
             minutes[i] = time[i];
             i++;
         }
     } catch (e) {
-        console.log(e instanceof TypeError);
-        console.log(e.message);
+        Console.log(e instanceof TypeError);
+        Console.log(e.message);
     }
-    switch (hours[0]){
+    switch (hours[0]) {
         case '1' :
             hours[0] = 'X';
             romanTens(hours);
@@ -78,7 +78,7 @@ function romanTime(time) {
             break;
         default: TypeError();
     }
-    switch (minutes[0]){
+    switch (minutes[0]) {
         case '1' :
             minutes[0] = 'X';
             romanTens(minutes);
