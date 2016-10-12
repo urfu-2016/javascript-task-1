6b2fd3c5c92c4f4fdd4f88c563acee32b6c25e1d
Sycheva Anastasia <za_sycha@mail.ru>
2016-10-13 01:40:21 +0500

    RomanTime N:N problems

--- roman-time.js
+++ roman-time.js
@@ -4,8 +4,8 @@ function convertToRoman(hours, minutes, halfTime) {
     var rightNumbers = { '00': 'N', '0': '', '1': 'I', '2': 'II', '3': 'III', '4': 'IV',
     '5': 'V', '6': 'VI', '7': 'VII', '8': 'VIII', '9': 'IX' };
     var leftNumbers = { '00': 'N', '1': 'X', '2': 'XX', '3': 'XXX', '4': 'XL', '5': 'L' };
-    halfTime[0] = leftNumbers[hours[0]].concat(rightNumbers[hours[1]]);
-    halfTime[1] = leftNumbers[minutes[0]].concat(rightNumbers[minutes[1]]);
+    halfTime[0] = leftNumbers[hours[0]] + rightNumbers[hours[1]];
+    halfTime[1] = leftNumbers[minutes[0]] + rightNumbers[minutes[1]];
 
     return (halfTime);
 }
