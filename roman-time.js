4d775735ef2d3c77ef4b1bc76986852cab73f6fc
DariaKineva <Kineva-97@yandex.ru>
2016-10-12 23:29:36 +0500

    Работа с ошибками

--- roman-time.js
+++ roman-time.js
@@ -5,14 +5,15 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    var h,m;
-	var roman = [ "N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII" ];
+    var h;
+    var m;
+	var roman = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII'];
 	if (!time || time.length > 5) {
  throw new TypeError( "Неверное время" );
 }
 	h = Number(time[0]) * 10 + Number(time[1]);
 	m = Number(time[3]) * 10 + Number(time[4]);
-	var time = roman[h%12] + ":" + roman[m%12];
+	var time = roman[h % 12] + ":" + roman[m % 12];
     return time;
 }
 
