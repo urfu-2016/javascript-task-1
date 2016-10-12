75e31a271de4a9a65498fa150296c90559155732
Степан <stepanus97@yandex.ru>
2016-10-12 23:55:57 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -4,9 +4,7 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    var number = {};
+var number = {};
     number[0] = '';
     number[1] = 'I';
     number[2] = 'II';
@@ -22,6 +20,8 @@ function romanTime(time) {
     number[30] = 'XXX';
     number[40] = 'XL';
     number[50] = 'L';
+function romanTime(time) {
+    // Немного авторского кода и замечательной магии
     if (typeof time === 'undefined' || typeof time === 'object') {
         throw new TypeError();
     }
@@ -30,42 +30,48 @@ function romanTime(time) {
     for (var i = 0; i < time.length; i++) {
         if (time.charAt(i) === ':') {
             a = time.substring(0, i);
-            b = time.substring(i + 1, time.length);           
+            b = time.substring(i + 1, time.length);
             break;
         }
     }
-    if (boool(a, b)) {
+    if (isCorrectTime(a, b)) {
         throw new TypeError();
     }
-    var a1 = a % 10;
-    if (a1 === 0 && a - a1 === 0) {
-        a = 'N';
-    }
-    else {
-        a = String(number[a - a1] + '' + number[a1]);
-    }
-    var b1 = b % 10;
-    if (b1 === 0 && b - b1 === 0) {
-        b = 'N';
-    }
-    else {
-        b = String(number[b - b1] + '' + number[b1]);
-    }
-    time = a + ':' + b;    
+    a = toRomanNumbers(a);
+    b = toRomanNumbers(b);  
+    time = a + ':' + b;
     return time;
 }
 
-function boool(a, b) {
+function isCorrectTime(a, b) {
     if (parseInt(a, 10) !== Number(a)) {
         return true;
     }
     if (parseInt(b, 10) !== Number(b)) {
         return true;
     }
+    if (isCorrectNumbers(a, b)) {
+        return true;
+    }
+    return false;
+}
+
+function isCorrectNumbers(a, b) {
     if (a < 0 || a > 23 || b < 0 || b > 59) {
         return true;
-    }    
+    }
     return false;
+}
+
+function toRomanNumbers(x) {
+    var x1 = x % 10;
+    if (x1 === 0 && x - x1 === 0) {
+        x = 'N';
+    }
+    else {
+        x = String(number[x - x1] + '' + number[x1]);
+    }
+    return x;
 }
 
 module.exports = romanTime;
