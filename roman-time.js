d66ab779c88c058d0b1f650894a79697d4637cf0
Sycheva Anastasia <za_sycha@mail.ru>
2016-10-13 00:09:16 +0500

    RomanTime biiiig style prolbems

--- roman-time.js
+++ roman-time.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function convertToRoman(hours, minutes, halfTime) {
-    var rightNumbers = { 0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V',
-		6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX' };
+    var rightNumbers = { 0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX' };
     var leftNumbers = { 1: 'X', 2: 'XX', 3: 'XXX', 4: 'XL', 5: 'L' };
     halfTime[0] = leftNumbers[hours[0]].concat(rightNumbers[hours[1]]);
     halfTime[1] = leftNumbers[minutes[0]].concat(rightNumbers[minutes[1]]);
@@ -12,37 +11,36 @@ function convertToRoman(hours, minutes, halfTime) {
 
 function isCorrectNumbers(hours, minutes) {
 	try {
-        if (hours[0] >= 0 && hours[0] <= 2 &&
-            hours[1] >= 0 && hours[1] <= 9 &&
-            minutes[0] >= 0 && minutes[0] <= 5 &&
-            minutes[1] >= 0 && minutes[1] <= 9 &&
-            !isNaN(hours[0]) && !isNaN(hours[1]) &&
-            !isNaN(minutes[0]) && !isNaN(minutes[1])) {
+    if (hours[0] >= 0 && hours[0] <= 2 &&
+    hours[1] >= 0 && hours[1] <= 9 &&
+    minutes[0] >= 0 && minutes[0] <= 5 &&
+    minutes[1] >= 0 && minutes[1] <= 9 &&
+    !isNaN(hours[0]) && !isNaN(hours[1]) &&
+    !isNaN(minutes[0]) && !isNaN(minutes[1])) {
 
-            return true;
+        return true;
         }
 
-        return false;
+    return false;
     } catch (e) {
-
-        return false;
+    return false;
     }
 }
 
 function isCorrectTime(halfTime, time) {
 	try {
-	    if ((time.length === 5 && halfTime.length === 2) &&
-            (halfTime[0] >= 0 && halfTime[0] <= 23 &&
-            halfTime[1] >= 0 && halfTime[1] <= 59 &&
-            !isNaN(halfTime[0]) && !isNaN(halfTime[1]))) {
+    if ((time.length === 5 && halfTime.length === 2) &&
+        (halfTime[0] >= 0 && halfTime[0] <= 23 &&
+        halfTime[1] >= 0 && halfTime[1] <= 59 &&
+        !isNaN(halfTime[0]) && !isNaN(halfTime[1]))) {
 
-	        return true;
-        }
+        return true;
+    }
 
-        return false;
+    return false;
     } catch (e) {
 
-        return false;
+    return false;
     }
 }
 
