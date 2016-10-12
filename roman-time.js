8b8c85d86ffa4fe1da37be04c47cafcfd1fd7dae
Sergey Kniga <Sergey Kniga>
2016-10-12 14:40:35 +0500

    Решение

--- roman-time.js
+++ roman-time.js
@@ -5,8 +5,34 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    var tokens = time.split(':');
+
+    var hours = parseInt(tokens[0], 10);
+    var minutes = parseInt(tokens[1], 10);
+
+    if (isNaN(hours) || isNaN(minutes) ||
+            hours < 0 || hours > 23 ||
+            minutes < 0 || minutes > 60) {
+        throw new Error('TypeError: Неверное время');
+    }
+
+    return toRoman(hours) + ':' + toRoman(minutes);
+}
+
+function toRoman(number) {
+    var ones = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    var tens = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX'];
+    var zero = 'N';
+
+    var result = '';
+    result += ones[number % 10];
+    result += tens[(number / 10) % 10];
+
+    if (result === '') {
+        result = zero;
+    }
+
+    return result;
 }
 
 module.exports = romanTime;
