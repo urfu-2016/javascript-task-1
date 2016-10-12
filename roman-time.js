df5e91fd46815d493c127fb3631de9a7985495ea
LopatinNikolai <lopatin.nikolai@mail.ru>
2016-10-12 23:19:28 +0500

    №9

--- roman-time.js
+++ roman-time.js
@@ -11,28 +11,50 @@ function roman(time) {
     var a = timeSplit[0].split('');
     parseInt(a[0], 10);
     parseInt(a[1], 10);
-    if (a[0] == 0 && a[1] == 0) { var hours = cifri[0]; }
-    if (a[0] == 0 && a[1] !== 0) { hours = cifri[a[1]]; }
-    if (a[0] !== 0 && a[1] !== 0) { hours = cifrii[(a[0])] + cifri[a[1]]; }
-    if (a[0] !== 0 && a[1] == 0) { hours = cifrii[a[0]]; }
+    if (a[0] == 0 && a[1] == 0) { 
+        var hours = cifri[0];
+    }
+    if (a[0] == 0 && a[1] !== 0) {
+        hours = cifri[a[1]];
+    }
+    if (a[0] !== 0 && a[1] !== 0) {
+        hours = cifrii[(a[0])] + cifri[a[1]];
+    }
+    if (a[0] !== 0 && a[1] == 0) {
+        hours = cifrii[a[0]];
+    }
     
     return hours;
 }
 function romanTime(time) {
     var timeSplit =time.split(':');
-    if (timeSplit[0] > 23 || timeSplit[0] < 0 || timeSplit[1] > 59 || timeSplit[1] < 0 ){ throw new TypeError(); }
-    if (isNaN(timeSplit[0]) || isNaN(timeSplit[1])){ throw new TypeError(); }
-    if (timeSplit[0] == NaN || timeSplit[0] === undefined || timeSplit[1] == NaN || timeSplit[1] === undefined ){ throw new TypeError();}   
-    var b = timeSplit[1].split('');   
-    parseInt(b[0],10);
-    parseInt(b[1],10);
-    if (b[0] == 0 && b[1] == 0){ var minutes = cifri[0]; }
-    if (b[0] == 0 && b[1] !== 0){ minutes = cifri[b[1]]; }
-    if (b[0] !== 0 && b[1] !== 0){ minutes = cifrii[(b[0]/10)] + cifri[b[1]]; }
-    if (b[0] !== 0 && b[1] == 0){ minutes = cifrii[b[0]]; }
-    time = roman(time) + ':' + minutes; 
+    if (timeSplit[0] > 23 || timeSplit[0] < 0 || timeSplit[1] > 59 || timeSplit[1] < 0 ){
+        throw new TypeError();
+    }
+    if (isNaN(timeSplit[0]) || isNaN(timeSplit[1])){
+        throw new TypeError();
+    }
+    if (timeSplit[0] == NaN || timeSplit[0] === undefined || timeSplit[1] == NaN || timeSplit[1] === undefined ){
+        throw new TypeError();
+    }   
+    var b = timeSplit[1].split('');
+    parseInt(b[0], 10);
+    parseInt(b[1], 10);
+    if (b[0] == 0 && b[1] == 0) {
+        var minutes = cifri[0];
+    }
+    if (b[0] == 0 && b[1] !== 0) {
+        minutes = cifri[b[1]];
+    }
+    if (b[0] !== 0 && b[1] !== 0) {
+        minutes = cifrii[(b[0]/10)] + cifri[b[1]];
+    }
+    if (b[0] !== 0 && b[1] == 0) {
+        minutes = cifrii[b[0]];
+    }
+    time = roman(time) + ':' + minutes;
     
-    return time; 
+    return time;
 }
 
 module.exports = romanTime;
