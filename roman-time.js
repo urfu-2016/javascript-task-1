5ec3f3a8b0722e3a1529776896246188b9f71efb
Mrbowtiesmind <kuzn-eg@mail.ru>
2016-10-12 09:27:44 +0500

    Разбиение на две функции с учётом последней лекции

--- roman-time.js
+++ roman-time.js
@@ -4,36 +4,27 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function romanTime(time) {
-           try {
-         if (time.length!=5){throw new TypeError()};
-         var hour=time.substr(0, 2);
-         var min=time.substr(3, 5);
-         if (isNaN(hour)===true){throw new TypeError()};
-         if (isNaN(min)===true){throw new TypeError()};}
-         catch (err){throw new TypeError();}
-    var min = time.substr(3,5);   
-    var ArabMind = [1, 4, 5, 9, 10, 40, 50];
+function convert(TransNumbers){
+	var ArabMind = [1, 4, 5, 9, 10, 40, 50];
     var RomMind = ["I", "IV", "V", "IX", "X", "XL", "L"];
-    var n=ArabMind.length - 1;
-    if (hour == 0){RomHour = "N"};
-    if (min == 0){RomMin = "N"};
-    while (hour > 0) {
-        if (hour >= ArabMind[n]) {
-            RomHour += RomMind[n];
-            hour -= ArabMind[n];
+	var n=ArabMind.length - 1;
+	if ((TransNumbers == '0') || (TransNumbers == '00')) {
+	return 'N';
+	while (TransNumbers > 0) {
+        if (TransNumbers >= ArabMind[n]){
+            RomNumber += RomMind[n];
+            TransNumbers -= ArabMind[n];
         }
         else {n--};
     }
-    var k = ArabMind.length - 1;
-    while (min > 0) {
-        if (min >= ArabMind[k]){
-            RomMin += RomMind[k];
-            min -= ArabMind[k];
-        }
-        else {k--};
-    }
-    document.write(RomHour + ":" + RomMin)
-    return time;
-}
+	return RomNumber;
+};
+function romanTime(time) {
+    var timeSet = time.split(':');
+	var ValidMinutes = (timeSet[1].length === 2) && (timeSet[1] >= 0) && (timeSet[1] <= 59);
+	var ValidHours = (timeSet[0].length === 2) && (timeSet[0] >= 0)&&(timeSet[0] <= 23);	
+    if (ValidHours && ValidHours && timeSet.length === 2) {
+         return Convert(timeSet[0]) + ':' + Convert(timeSet[1]);
+      }
+	throw new TypeError('TypeError');}
 module.exports = romanTime;
