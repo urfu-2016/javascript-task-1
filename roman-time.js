8c12cc1307b894472e9e202bdbc208ea21c301ab
Barbanyagra Vladislav <contego@yandex.ru>
2016-10-12 21:56:32 +0500

    first solution

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,56 @@
 'use strict';
 
+function toRoman(x) {
+    var res = '';
+
+    if (x === 0) {
+        res = 'N';
+    }
+    if (x >= 50) {
+        x -= 50;
+        res += 'L';
+    }
+    while (x >= 10) {
+        x -= 10;
+        res += 'X';
+    }
+    if (x >= 9) {
+        x -= 9;
+        res += 'IX';
+    }
+    if (x >= 5) {
+        x -= 5;
+        res += 'V';
+    }
+    if (x >= 4) {
+        x -= 4;
+        res += 'IV';
+    }
+    while (x >= 1) {
+        x -= 1;
+        res += 'I';
+    }
+
+    return res;
+}
+
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    time = String(time);
+    if (!/^\d\d:\d\d$/.test(time)) {
+        throw new TypeError("Incorrect time");
+    }
+
+    var hh = +time.slice(0, 2), mm = +time.slice(3, 5);
+
+    if (hh >= 24 || mm >= 60) {
+        throw new TypeError("Incorrect time");
+    }
+
+    return time + " " + toRoman(hh) + ':' + toRoman(mm);
 }
 
 module.exports = romanTime;
