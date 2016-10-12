9d1a66980e71f1c2a32f9393f3170ddc9c8c78ea
Савенков Илья Ильич <savenkov@kontur>
2016-10-12 19:45:43 +0500

    Конвертация из Арабской записи чисел в Римскую

--- roman-time.js
+++ roman-time.js
@@ -5,8 +5,35 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    var parseTime = time.match( /(\d{2}):((\d{2}))/ );
+    if(time === null) {
+        throw new TypeError('Неверное время', 'roman-time.js', 10);
+    }
+    var hour = +parseTime[1];
+    var min = +parseTime[2];
+    if(hour < 0 || hour > 23 || min < 0 || min > 59) {
+        throw new TypeError('Неверное время', 'roman-time.js', 10);
+    }
+
+    function convertArabiсToRoman(integer) {
+        if(integer === 0){
+            return 'N';
+        }
+        var romanNumerals = [[1,'I'], [4,'IV'], [5,'V'], [9,'IX'], [10, 'X'], [40, 'XL'], [50, 'L']];
+        var roman = '';
+        var i = romanNumerals.length - 1;
+        while (integer > 0) {
+            if (integer >= romanNumerals[i][0]) {
+                roman += romanNumerals[i][1];
+                integer -= romanNumerals[i][0];
+            }
+            else i--;
+         }
+        return roman;
+    }
+
+    return convertArabiсToRoman(hour) + ":" + convertArabiсToRoman(min);
+
 }
 
 module.exports = romanTime;
