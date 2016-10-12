b1db35cae13d9125d72fbca18417d7619d88bdc3
Sycheva Anastasia <za_sycha@mail.ru>
2016-10-13 01:15:35 +0500

    RomanTime parse to int problems

--- roman-time.js
+++ roman-time.js
@@ -4,18 +4,18 @@ function convertToRoman(hours, minutes, halfTime) {
     var rightNumbers = { 0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV',
     5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX' };
     var leftNumbers = { 1: 'X', 2: 'XX', 3: 'XXX', 4: 'XL', 5: 'L' };
-    halfTime[0] = leftNumbers[hours[0]].concat(rightNumbers[hours[1]]);
-    halfTime[1] = leftNumbers[minutes[0]].concat(rightNumbers[minutes[1]]);
+    halfTime[0] = leftNumbers[Number(hours[0])].concat(rightNumbers[Number(hours[1])]);
+    halfTime[1] = leftNumbers[Number(minutes[0])].concat(rightNumbers[Number(minutes[1])]);
 
     return (halfTime);
 }
 
 function isCorrectNumbers(hours, minutes) {
     try {
-        if (hours[0] >= 0 && hours[0] <= 2 &&
-        hours[1] >= 0 && hours[1] <= 9 &&
-        minutes[0] >= 0 && minutes[0] <= 5 &&
-        minutes[1] >= 0 && minutes[1] <= 9 &&
+        if (Number(hours[0]) >= 0 && Number(hours[0]) <= 2 &&
+        Number(hours[1]) >= 0 && Number(hours[1]) <= 9 &&
+        Number(minutes[0]) >= 0 && Number(minutes[0]) <= 5 &&
+        Number(minutes[1]) >= 0 && Number(minutes[1]) <= 9 &&
         !isNaN(hours[0]) && !isNaN(hours[1]) &&
         !isNaN(minutes[0]) && !isNaN(minutes[1])) {
 
@@ -53,8 +53,8 @@ function romanTime(time) {
     var hours;
     var minutes;
     if (isCorrectTime(halfTime, time)) {
-        hours = Number(halfTime[0].split(''));
-        minutes = Number(halfTime[1].split(''));
+        hours = halfTime[0].split('');
+        minutes = halfTime[1].split('');
         if (isCorrectNumbers(hours, minutes)) {
             time = convertToRoman(hours, minutes, halfTime).join(':');
         } else {
