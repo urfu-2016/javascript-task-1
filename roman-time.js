1a42ec04a6bebf1dac55feb02cbbda1a49c809d1
Barbanyagra Vladislav <contego@yandex.ru>
2016-10-12 23:30:22 +0500

    third solution

--- roman-time.js
+++ roman-time.js
@@ -11,8 +11,8 @@ function toRoman(x) {
         [9, 'IX'],
         [5, 'V'],
         [4, 'IV'],
-        [1, 'I'],
-    ])
+        [1, 'I']
+    ]);
 
     var res = '';
     for (var part of parts) {
