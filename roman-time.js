432bb27c33d69bb13dd447b254ebf00983e6e2c4
Savenkov Ilya <slaeme@yandex.ru>
2016-10-12 20:01:07 +0500

    Конвертация из Арабской записи чисел в Римскую

--- roman-time.js
+++ roman-time.js
@@ -6,20 +6,20 @@
  */
 function romanTime(time) {
     var parseTime = time.match( /(\d{2}):((\d{2}))/ );
-    if(time === null) {
+    if (time === null) {
         throw new TypeError('Неверное время', 'roman-time.js', 10);
     }
-    var hour = +parseTime[1];
-    var min = +parseTime[2];
-    if(hour < 0 || hour > 23 || min < 0 || min > 59) {
+    var hour = Number(parseTime[1]);
+    var min = Number(parseTime[2]);
+    if (hour < 0 || hour > 23 || min < 0 || min > 59) {
         throw new TypeError('Неверное время', 'roman-time.js', 10);
     }
 
     function convertArabiсToRoman(integer) {
-        if(integer === 0){
+        if (integer === 0){
             return 'N';
         }
-        var romanNumerals = [[1,'I'], [4,'IV'], [5,'V'], [9,'IX'], [10, 'X'], [40, 'XL'], [50, 'L']];
+        var romanNumerals = [[1, 'I'], [4, 'IV'], [5, 'V'], [9, 'IX'], [10, 'X'], [40, 'XL'], [50, 'L']];
         var roman = '';
         var i = romanNumerals.length - 1;
         while (integer > 0) {
@@ -28,12 +28,11 @@ function romanTime(time) {
                 integer -= romanNumerals[i][0];
             }
             else i--;
-         }
+        }
         return roman;
     }
 
-    return convertArabiсToRoman(hour) + ":" + convertArabiсToRoman(min);
-
+    return convertArabiсToRoman(hour) + ':' + convertArabiсToRoman(min);
 }
 
 module.exports = romanTime;
