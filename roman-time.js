e30d1e72c0bb3cdad2b10d6e1355bc2beefce859
Sycheva Anastasia <za_sycha@mail.ru>
2016-10-12 23:27:28 +0500

    RomanTime problem

--- roman-time.js
+++ roman-time.js
@@ -1,11 +1,71 @@
 'use strict';
 
+function convertToRoman(hours, minute, halfTime) {
+	var rightNumbers = { 0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 
+		6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX' };
+    var leftNumbers = { 1: 'X', 2: 'XX', 3: 'XXX', 4: 'XL', 5: 'L' };
+    
+    //h+h
+    halfTime[0] = leftNumbers[hours[0]].concat(rightNumbers[hours[1]]);
+    //m+m
+    halfTime[1] = leftNumbers[minutes[0]].concat(rightNumbers[minutes[1]]);
+    //hh+mm
+    return (halfTime);
+}
+
+function isCorrectNumbers(hours, minutes) {
+	try {
+        if (hours[0] >= 0 && hours[0] <= 2 &&
+            hours[1] >= 0 && hours[1] <= 9 &&
+            minutes[0] >= 0 && minutes[0] <= 5 &&
+            minutes[1] >= 0 && minutes[1] <= 9 &&
+            !isNaN(hours[0]) && !isNaN(hours[1]) &&
+            !isNaN(minutes[0]) && !isNaN(minutes[1])) {
+
+            return true;
+        }
+        return false;
+    } 
+    catch (e) {
+        return false;
+    }
+}
+
+function isCorrectTime(halfTime, time) {
+	try {
+        if ((time.length === 5 && halfTime.length === 2) &&
+        	(halfTime[0] >= 0 && halfTime[0] <= 23 &&
+        	 halfTime[1] >= 0 && halfTime[1] <= 59 &&
+        	 !isNaN(halfTime[0]) && !isNaN(halfTime[1])) {
+            
+            return true;
+        }
+        return false;
+    } 
+    catch (e) {
+        return false;
+    }
+}
+
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
+
+	//hh, mm
+    var halfTime = time.split(':');
+    //h, h
+    //leftNumber, rightNumber
+    var hours = Number(halfTime[0].split(''));
+    //m, m
+    //leftNumber, rightNumber
+    var minutes = Number(halfTime[1].split(''));
+
+    if (isCorrectTime(halfTime, time) && isCorrectNumbers(hours, minutes)) {
+    } else {
+    	throw new TypeError('Неверный формат времени');
+    }
     return time;
 }
 
