6d2a440d94ba4c78f8ec340f6347efa8952c1138
DariaKineva <Kineva-97@yandex.ru>
2016-10-12 23:00:59 +0500

    Задача с римскими числами

--- roman-time.js
+++ roman-time.js
@@ -6,13 +6,13 @@
  */
 function romanTime(time) {
     var h,m;
-	var roman = ["N","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII"];
+	var roman = [ "N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII" ];
 	if (!time || time.length > 5) {
- throw new TypeError("Неверное время");
+ throw new TypeError( "Неверное время" );
 }
-	h = Number(time[0])*10 + Number(time[1]);
-	m = Number(time[3])*10 + Number(time[4]);
-	var time = roman[h%12]+":"+roman[m%12];
+	h = Number(time[0]) * 10 + Number(time[1]);
+	m = Number(time[3]) * 10 + Number(time[4]);
+	var time = roman[h%12] + ":" + roman[m%12];
     return time;
 }
 
