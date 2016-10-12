b7f697913340e6c88ee6d98ee5e046ce9de16340
Arthur Liss <artli@e1.ru>
2016-10-12 14:35:31 +0500

    Fix more styling issues

--- roman-time.js
+++ roman-time.js
@@ -24,14 +24,14 @@ function isIntegerFromRange(number, min, max) {
 function naturalNumberToRomanNumeral(number) {
     if (!isIntegerFromRange(number, 0, 3999)) {
         throw new TypeError('The input is not a natural number from [0; 3999]');
-	}
+    }
 
     var arabic = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
     var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
 
     if (number === 0) {
         return 'N';
-	}
+    }
 
     var result = '';
     for (var i = arabic.length - 1; i >= 0; i--) {
@@ -52,23 +52,25 @@ function naturalNumberToRomanNumeral(number) {
 function romanTime(time) {
     if (typeof time !== 'string') {
         throw new TypeError('The parameter is not a string');
-	}
+    }
 
     var parts = time.split(':');
     if (parts.length !== 2) {
         throw new TypeError(
             'The parameter is divided by colons into a number of parts other than two');
-	}
+    }
 
     var hours = parseInt(parts[0], 10);
     var minutes = parseInt(parts[1], 10);
 
     if (!isIntegerFromRange(hours, 0, 23)) {
-        throw new TypeError('The hours part is not a valid natural number or lies outside of [0; 23]');
-	}
+        throw new TypeError(
+            'The hours part is not a valid natural number or lies outside of [0; 23]');
+    }
     if (!isIntegerFromRange(minutes, 0, 59)) {
-        throw new TypeError('The minutes part is not a valid natural number or lies outside of [0; 59]');
-	}
+        throw new TypeError(
+            'The minutes part is not a valid natural number or lies outside of [0; 59]');
+    }
 
     return (
         naturalNumberToRomanNumeral(hours) + 
