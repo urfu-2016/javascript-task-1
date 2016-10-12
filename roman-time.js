1d99ce298dec8c64d94d37f976bd99d73541ec70
Варюхина Арина КН-302 <arinavaruhina@gmail.com>
2016-10-12 21:29:34 +0500

    Исправление ошибок

--- roman-time.js
+++ roman-time.js
@@ -9,72 +9,76 @@ function romanTime(time) {
     checkInput(time);
     var hours = time.split(':')[0];
     var minutes = time.split(':')[1];
-	time = setRomanNumerals(hours);
-	time += " : " + setRomanNumerals(minutes);
+    time = setRomanNumerals(hours);
+    time += ' : ' + setRomanNumerals(minutes);
+
     return time;
 }
 
 
-function checkInput(time) {	
+function checkInput(time) {
     if (!time) {
-    	throw new TypeError();
+        throw new TypeError();
     }
-    regLetter = /[^0-9:]/;
-    if (time.search(regLetter) != -1) {
-    	throw new TypeError();
+    var regLetter = /[^0-9:]/;
+    if (time.search(regLetter) !== -1) {
+        throw new TypeError();
     }
     if (time.length > 5) {
-    	throw new TypeError();
-    }    	
+        throw new TypeError();
+    }       
     if (hours > 23 || minutes > 59) {
-    	throw new TypeError();
-	}
+        throw new TypeError();
+    }
 }
 
 
 function setRomanNumerals1_9(number){
-	if (number==='1' || number==='2'|| number==='3'){
-		for (var i = 0; i < Number(number) ; i++){
-			result += "I";
-		}
-	}
-	if (number==='4'){
-		result += "IV";
-	}
-	if (number==='5' || number==='6'|| number==='7'|| number==='8'){
-		result += "V";
-		for (var i = 0; i < Number(number%5) ; i++){
-			result += "I";
-		}
-	}
-	if (number==='9'){
-		result += "IX";
-	}
-	return result;
+    var result = ''
+    if (number === '1' || number === '2'|| number === '3'){
+        for (var i = 0; i < Number(number) ; i++){
+            result += 'I';
+        }
+    }
+    if (number==='4'){
+        result += 'IV';
+    }
+    if (number==='5' || number==='6'|| number==='7'|| number==='8'){
+        result += 'V';
+        for (var i = 0; i < Number(number%5) ; i++){
+            result += 'I';
+        }
+    }
+    if (number==='9'){
+        result += 'IX';
+    }
+
+    return result;
 }
 
 
 function setRomanNumerals(number){
-	result = "";
-	if (number<10){
-		setRomanNumerals1_9(number);
-	}
-	if (number>=10 && number<=19){
-		result += "X" + setRomanNumerals1_9(number[1]);
-	}
-	if (number>=20 && number<=29){
-		result += "XX" + setRomanNumerals1_9(number[1]);
-	}
-	if (number>=30 && number<=39){
-		result += "XXX" + setRomanNumerals1_9(number[1]);
-	}
-	if (number>=40 && number<=49){
-		result += "XL" + setRomanNumerals1_9(number[1]);
-	}
-	if (number>=50 && number<=59){
-		result += "L" + setRomanNumerals1_9(number[1]);
-	}
-	return result;
+    var result = ';'
+    if (number<10){
+        setRomanNumerals1_9(number);
+    }
+    if (number>=10 && number<=19){
+        result += 'X' + setRomanNumerals1_9(number[1]);
+    }
+    if (number>=20 && number<=29){
+        result += 'XX' + setRomanNumerals1_9(number[1]);
+    }
+    if (number>=30 && number<=39){
+        result += 'XXX' + setRomanNumerals1_9(number[1]);
+    }
+    if (number>=40 && number<=49){
+        result += 'XL' + setRomanNumerals1_9(number[1]);
+    }
+    if (number>=50 && number<=59){
+        result += 'L' + setRomanNumerals1_9(number[1]);
+    }
+
+    return result;
 }
 
 
