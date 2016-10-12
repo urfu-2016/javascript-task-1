34e433190a2cd4c64b46cab64de252b04a085e1d
Степан <stepanus97@yandex.ru>
2016-10-12 22:58:25 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -22,16 +22,13 @@ function romanTime(time) {
     number[30] = 'XXX';
     number[40] = 'XL';
     number[50] = 'L';
-    if (typeof time === 'undefined' || typeof time === 'object') 
-    {
+    if (typeof time === 'undefined' || typeof time === 'object') {
         throw new TypeError();
     }
     var a = '';
     var b = '';
-    for (var i = 0; i < time.length; i++)
-    {
-        if (time.charAt(i) === ':')
-        {
+    for (var i = 0; i < time.length; i++) {
+        if (time.charAt(i) === ':') {
             a = time.substring(0, i);
             b = time.substring(i + 1, time.length);
             if (parseInt(a, 10) !== +a) throw new TypeError();
@@ -43,16 +40,33 @@ function romanTime(time) {
     }
     var a1 = a % 10;
     var a2 = a - a1;
-    if (a1 === 0 && a2 === 0) a = 'N';
-    else if (a1 === 0) a = number[a2];
-    else if (a2 === 0) a = number[a1];
-    else a = number[a2] + '' + number[a1];
+    if (a1 === 0 && a2 === 0) {
+        a = 'N';
+    }
+    else if (a1 === 0) {
+        a = number[a2];
+    }
+    else if (a2 === 0) {
+        a = number[a1];
+    }
+    else {
+        a = number[a2] + '' + number[a1];
+    }
     var b1 = b % 10;
     var b2 = b - b1;
-    if (b1 === 0 && b2 === 0) b = 'N';
-    else if (b1 === 0) b = number[b2];
-    else if (b2 === 0) b = number[b1];
-    else b = number[b2] + '' + number[b1];
+    if (b1 === 0 && b2 === 0) {
+        b = 'N';
+    }
+    else if (b1 === 0) 
+    {
+        b = number[b2];
+    }
+    else if (b2 === 0) {
+        b = number[b1];
+    }
+    else {
+        b = number[b2] + '' + number[b1];
+    }
     time = a + ':' + b;
     return time;
 }
