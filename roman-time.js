9e265e8d26b9bb87cc629417e5e69d3d59df9720
Mrbowtiesmind <kuzn-eg@mail.ru>
2016-10-12 20:49:06 +0500

    FinalVersion2

--- roman-time.js
+++ roman-time.js
@@ -1,21 +1,21 @@
 'use strict';
 
 function convert(TransNumbers) {
-     var firstDigital = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
-     var secondDigits = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
-     if (parseInt(TransNumbers, 10) === 0) {
-         return 'N';
+    var firstDigital = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+    var secondDigits = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    if (parseInt(TransNumbers, 10) === 0) {
+        return 'N';
      }
-     var trans = firstDigital[Math.floor(TransNumbers / 10)];
-     trans = trans + secondDigits[TransNumbers % 10];
-     return trans;
- }
+    var trans = firstDigital[Math.floor(TransNumbers / 10)];
+    trans = trans + secondDigits[TransNumbers % 10];
+    return trans;
+}
 function romanTime(time) {
     var timeSet = time.split(':');
-	var ValidMinutes = (timeSet[1].length === 2) && (timeSet[1] >= 0) && (timeSet[1] <= 59);
-	var ValidHours = (timeSet[0].length === 2) && (timeSet[0] >= 0)&&(timeSet[0] <= 23);	
-    if (ValidHours && ValidHours && timeSet.length === 2) {
-         return сonvert(timeSet[0]) + ':' + сonvert(timeSet[1]);
-      }
-	throw new TypeError('TypeError');}
+    var ValidMinutes = (timeSet[1].length === 2) && (timeSet[1] >= 0) && (timeSet[1] <= 59);
+    var ValidHours = (timeSet[0].length === 2) && (timeSet[0] >= 0)&&(timeSet[0] <= 23);
+    if (ValidHours && ValidMinutes && timeSet.length === 2) {
+        return сonvert(timeSet[0]) + ':' + сonvert(timeSet[1]);
+    }
+    throw new TypeError('TypeError');}
 module.exports = romanTime;
