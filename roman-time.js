19c1b020513a1119fb1ff4380bd99429ef53377b
Степан <stepanus97@yandex.ru>
2016-10-12 22:30:55 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -6,6 +6,55 @@
  */
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
+    var number = {};
+	number[0] = 'N';
+	number[1] = 'I';
+	number[2] = 'II';
+	number[3] = 'III';
+	number[4] = 'IV';
+	number[5] = 'V';
+	number[6] = 'VI';
+	number[7] = 'VII';
+	number[8] = 'VIII';
+	number[9] = 'IX';
+	number[10] = 'X';
+	number[20] = 'XX';
+	number[30] = 'XXX';
+	number[40] = 'XL';
+	number[50] = 'L';
+	if (typeof time == 'undefined' ) throw new TypeError();
+	if (typeof time == 'object' ) throw new TypeError();
+	var a = '';
+	var b = '';
+    for (var i = 0; i< time.length; i++)
+	{
+		if (time.charAt(i) == ':')
+		{
+			a = time.substring(0,i);
+			b = time.substring(i+1,time.length);
+			if (parseInt(a,10)!=+a) throw new TypeError();
+			if (parseInt(b,10)!=+b) throw new TypeError();
+			if (a<0 || a>23) throw new TypeError();
+			if (b<0 || b>59) throw new TypeError();
+			break;
+		}		
+	}
+	var a1 = a%10;
+	var a2 = a-a1;
+	if (a1 == 0 && a2 == 0) a = 'N';
+	else if (a1 == 0) a = number[a2];
+	else if (a2 == 0) a = number[a1];
+	else a = number[a2]+''+number[a1];
+	
+	var b1 = b%10;
+	var b2 = b-b1;
+	if (b1 == 0 && b2 == 0) b = 'N';
+	else if (b1 == 0) b = number[b2];
+	else if (b2 == 0) b = number[b1];
+	else b = number[b2]+''+number[b1];
+	
+	time = a+':'+b;
+	
     return time;
 }
 
