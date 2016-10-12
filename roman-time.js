b81d5af590e5ed6337dce9132e1bfe335f046213
Заусова Екатерина <kate0497@yandex.ru>
2016-10-12 22:39:59 +0500

    создала отдельную функцию

--- roman-time.js
+++ roman-time.js
@@ -5,88 +5,52 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    var hour = ['X', 'XX', 'XXX', 'XL', 'L'];
-    var minute = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
-
-    //проверка на лишние символы
-    if ((isNaN(parseInt(time[0], 10)) === true) || (isNaN(parseInt(time[1], 10)) === true) || (isNaN(parseInt(time[3], 10)) === true) || (isNaN(parseInt(time[4], 10)) === true)) {
+// Немного авторского кода и замечательной магии
+// проверка на лишние символы
+    if ((isNaN(parseInt(time[0], 10)) === true) || (isNaN(parseInt(time[1], 10)) === true) ||
+        (isNaN(parseInt(time[3], 10)) === true) || (isNaN(parseInt(time[4], 10)) === true)) {
         console.info(parseInt(time.slice(3, 5)));
-        throw new TypeError;
-    }
-
-    else {
-
-        //проверка на допустимые значения
-        if ((time.length === 5) && (parseInt(time.slice(0, 2)) < 24) && (parseInt(time.slice(3, 5)) < 60)) {
-
+        throw new TypeError();
+    } else {
+ // проверка на допустимые значения
+        if ((time.length === 5) && (parseInt(time.slice(0, 2)) < 24) &&
+            (parseInt(time.slice(3, 5)) < 60)) {
             var pattern = '';
-
-            var firstHour = parseInt(time[0], 10);
-
-            if (firstHour === 0)
-            {
-
-                pattern = 'N';
-
-                var secondHour = parseInt(time[1], 10);
-
-                if (secondHour != 0)
-                {
-                    pattern = minute[secondHour - 1];
-                }
-            }
-            else {
-
-                pattern = hour[firstHour - 1];
-
-                var secondHour = parseInt(time[1], 10);
-
-                if (secondHour != 0)
-                {
-                    pattern += minute[secondHour - 1];
-                }
-
-            }
-
+            pattern = patternOwn(pattern,time,0);
             pattern = pattern + ':';
-
-            var firstMinute = parseInt(time[3], 10);
-
-            if (firstMinute === 0) {
-
-                pattern += 'N';
-
-                var secondMinute = parseInt(time[4], 10);
-
-                if (secondMinute != 0)
-                {
-                    pattern += minute[secondMinute - 1];
-                }
-            }
-
-            else {
-
-                pattern += hour[firstMinute - 1];
-
-                var secondMinute = parseInt(time[4], 10);
-                if (secondMinute != 0) {
-                    pattern += minute[secondMinute - 1];
-                }
-            }
-
+            pattern+=patternOwn(pattern,time,3);
+           
             time = pattern;
-        }
-
-        else
+        } else
              {
-            throw new TypeError;
+            throw new TypeError();
              }
-
     }
-
     return time;
 }
+function patternOwn(pattern,time,n) {
+    var hour = ['X', 'XX', 'XXX', 'XL', 'L'];
+    var minute = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+    var firstHour = parseInt(time[n], 10);
+    if (firstHour === 0) {
 
+        pattern = 'N';
+
+        var secondHour = parseInt(time[n + 1], 10);
+        if (secondHour !== 0) {
+            pattern = minute[secondHour - 1];
+        }
+    } else {
+
+        pattern = hour[firstHour - 1];
+
+        var secondHour2 = parseInt(time[n + 1], 10);
+        if (secondHour2 !== 0) {
+            pattern += minute[secondHour2 - 1];
+        }
+
+    }
+
+    return pattern;
+}
 module.exports = romanTime;
-
