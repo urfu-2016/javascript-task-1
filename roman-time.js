d34b94ee1bc7d17973a3a7aa011476a6acaa8baa
Mrbowtiesmind <kuzn-eg@mail.ru>
2016-10-12 21:00:17 +0500

    FinalVersion4

--- roman-time.js
+++ roman-time.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function convert(TransNumbers) {
+function convertTo(TransNumbers) {
     var firstDigital = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
     var secondDigits = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
     if (parseInt(TransNumbers, 10) === 0) {
@@ -15,7 +15,7 @@ function romanTime(time) {
     var ValidMinutes = (timeSet[1].length === 2) && (timeSet[1] >= 0) && (timeSet[1] <= 59);
     var ValidHours = (timeSet[0].length === 2) && (timeSet[0] >= 0) && (timeSet[0] <= 23);
     if (ValidHours && ValidMinutes && timeSet.length === 2) {
-        return сonvert(timeSet[0]) + ':' + сonvert(timeSet[1]);
+        return convertTo(timeSet[0]) + ':' + convertTo(timeSet[1]);
     }
     throw new TypeError('TypeError');
 }
