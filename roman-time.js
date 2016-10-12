7c09c168df637f6bb6c75157e378f5a6e6906e8c
Barbanyagra Vladislav <contego@yandex.ru>
2016-10-12 23:28:50 +0500

    first solution

--- roman-time.js
+++ roman-time.js
@@ -1,34 +1,25 @@
 'use strict';
 
 function toRoman(x) {
-    var res = '';
-
     if (x === 0) {
-        res = 'N';
+        return 'N';
     }
-    if (x >= 50) {
-        x -= 50;
-        res += 'L';
-    }
-    while (x >= 10) {
-        x -= 10;
-        res += 'X';
-    }
-    if (x >= 9) {
-        x -= 9;
-        res += 'IX';
-    }
-    if (x >= 5) {
-        x -= 5;
-        res += 'V';
-    }
-    if (x >= 4) {
-        x -= 4;
-        res += 'IV';
-    }
-    while (x >= 1) {
-        x -= 1;
-        res += 'I';
+
+    var parts = new Map([
+        [50, 'L'],
+        [10, 'X'],
+        [9, 'IX'],
+        [5, 'V'],
+        [4, 'IV'],
+        [1, 'I'],
+    ])
+
+    var res = '';
+    for (var part of parts) {
+        while (x >= part[0]) {
+            x -= part[0];
+            res += part[1];
+        }
     }
 
     return res;
@@ -41,16 +32,17 @@ function toRoman(x) {
 function romanTime(time) {
     time = String(time);
     if (!/^\d\d:\d\d$/.test(time)) {
-        throw new TypeError("Incorrect time");
+        throw new TypeError('Incorrect time');
     }
 
-    var hh = +time.slice(0, 2), mm = +time.slice(3, 5);
+    var hh = Number(time.slice(0, 2));
+    var mm = Number(time.slice(3, 5));
 
     if (hh >= 24 || mm >= 60) {
-        throw new TypeError("Incorrect time");
+        throw new TypeError('Incorrect time');
     }
 
-    return time + " " + toRoman(hh) + ':' + toRoman(mm);
+    return toRoman(hh) + ':' + toRoman(mm);
 }
 
 module.exports = romanTime;
