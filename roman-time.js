edabb137ecb6e1faa0cbc1200222cfd9b5c02e8a
Варюхина Арина КН-302 <arinavaruhina@gmail.com>
2016-10-12 21:37:36 +0500

    Исправила ошибки2

--- roman-time.js
+++ roman-time.js
@@ -9,6 +9,9 @@ function romanTime(time) {
     checkInput(time);
     var hours = time.split(':')[0];
     var minutes = time.split(':')[1];
+    if (hours > 23 || minutes > 59) {
+        throw new TypeError();
+    }
     time = setRomanNumerals(hours);
     time += ' : ' + setRomanNumerals(minutes);
 
@@ -26,30 +29,27 @@ function checkInput(time) {
     }
     if (time.length > 5) {
         throw new TypeError();
-    }       
-    if (hours > 23 || minutes > 59) {
-        throw new TypeError();
     }
 }
 
 
-function setRomanNumerals1_9(number){
+function setRomanNumerals19(number) {
     var result = ''
-    if (number === '1' || number === '2'|| number === '3'){
-        for (var i = 0; i < Number(number) ; i++){
+    if (number === '1' || number === '2'|| number === '3') {
+        for (var i = 0; i < Number(number) ; i++) {
             result += 'I';
         }
     }
-    if (number==='4'){
+    if (number==='4') {
         result += 'IV';
     }
-    if (number==='5' || number==='6'|| number==='7'|| number==='8'){
+    if (number==='5' || number==='6'|| number==='7'|| number==='8') {
         result += 'V';
-        for (var i = 0; i < Number(number%5) ; i++){
+        for (var i = 0; i < Number(number%5) ; i++) {
             result += 'I';
         }
     }
-    if (number==='9'){
+    if (number==='9') {
         result += 'IX';
     }
 
@@ -57,25 +57,25 @@ function setRomanNumerals1_9(number){
 }
 
 
-function setRomanNumerals(number){
+function setRomanNumerals(number) {
     var result = ';'
-    if (number<10){
-        setRomanNumerals1_9(number);
+    if (number<10) {
+        setRomanNumerals19(number);
     }
-    if (number>=10 && number<=19){
-        result += 'X' + setRomanNumerals1_9(number[1]);
+    if (number>=10 && number<=19) {
+        result += 'X' + setRomanNumerals19(number[1]);
     }
-    if (number>=20 && number<=29){
-        result += 'XX' + setRomanNumerals1_9(number[1]);
+    if (number>=20 && number<=29) {
+        result += 'XX' + setRomanNumerals19(number[1]);
     }
-    if (number>=30 && number<=39){
-        result += 'XXX' + setRomanNumerals1_9(number[1]);
+    if (number>=30 && number<=39) {
+        result += 'XXX' + setRomanNumerals19(number[1]);
     }
-    if (number>=40 && number<=49){
-        result += 'XL' + setRomanNumerals1_9(number[1]);
+    if (number>=40 && number<=49) {
+        result += 'XL' + setRomanNumerals19(number[1]);
     }
-    if (number>=50 && number<=59){
-        result += 'L' + setRomanNumerals1_9(number[1]);
+    if (number>=50 && number<=59) {
+        result += 'L' + setRomanNumerals19(number[1]);
     }
 
     return result;
