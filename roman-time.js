3bbf0a5b7c93204ccb333947a501289d650a0d77
Sycheva Anastasia <za_sycha@mail.ru>
2016-10-13 00:25:13 +0500

    RomanTime sooooo biiiig style prolbems

--- roman-time.js
+++ roman-time.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function convertToRoman(hours, minutes, halfTime) {
-    var rightNumbers = { 0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX' };
+    var rightNumbers = { 0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 
+    5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX' };
     var leftNumbers = { 1: 'X', 2: 'XX', 3: 'XXX', 4: 'XL', 5: 'L' };
     halfTime[0] = leftNumbers[hours[0]].concat(rightNumbers[hours[1]]);
     halfTime[1] = leftNumbers[minutes[0]].concat(rightNumbers[minutes[1]]);
@@ -10,37 +11,37 @@ function convertToRoman(hours, minutes, halfTime) {
 }
 
 function isCorrectNumbers(hours, minutes) {
-	try {
-    if (hours[0] >= 0 && hours[0] <= 2 &&
-    hours[1] >= 0 && hours[1] <= 9 &&
-    minutes[0] >= 0 && minutes[0] <= 5 &&
-    minutes[1] >= 0 && minutes[1] <= 9 &&
-    !isNaN(hours[0]) && !isNaN(hours[1]) &&
-    !isNaN(minutes[0]) && !isNaN(minutes[1])) {
+    try {
+        if (hours[0] >= 0 && hours[0] <= 2 &&
+        hours[1] >= 0 && hours[1] <= 9 &&
+        minutes[0] >= 0 && minutes[0] <= 5 &&
+        minutes[1] >= 0 && minutes[1] <= 9 &&
+        !isNaN(hours[0]) && !isNaN(hours[1]) &&
+        !isNaN(minutes[0]) && !isNaN(minutes[1])) {
 
-        return true;
+            return true;
         }
 
-    return false;
-    } catch (e) {
-    return false;
+        return false;
+        } catch (e) {
+            return false;
     }
 }
 
 function isCorrectTime(halfTime, time) {
-	try {
-    if ((time.length === 5 && halfTime.length === 2) &&
-        (halfTime[0] >= 0 && halfTime[0] <= 23 &&
-        halfTime[1] >= 0 && halfTime[1] <= 59 &&
-        !isNaN(halfTime[0]) && !isNaN(halfTime[1]))) {
+    try {
+        if ((time.length === 5 && halfTime.length === 2) &&
+            (halfTime[0] >= 0 && halfTime[0] <= 23 &&
+            halfTime[1] >= 0 && halfTime[1] <= 59 &&
+            !isNaN(halfTime[0]) && !isNaN(halfTime[1]))) {
 
-        return true;
-    }
+            return true;
+        }
 
-    return false;
-    } catch (e) {
+        return false;
+        } catch (e) {
 
-    return false;
+        return false;
     }
 }
 
@@ -53,7 +54,7 @@ function romanTime(time) {
     var hours = Number(halfTime[0].split(''));
     var minutes = Number(halfTime[1].split(''));
     if (isCorrectTime(halfTime, time) && isCorrectNumbers(hours, minutes)) {
-    	time = convertToRoman(hours, minutes, halfTime).join(':');
+        time = convertToRoman(hours, minutes, halfTime).join(':');
     } else {
         throw new TypeError('Неверный формат времени');
     }
