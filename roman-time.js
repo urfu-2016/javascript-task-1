f5ef4284c6d5093f1503b3747e70cfdcc44f03ec
Степан <stepanus97@yandex.ru>
2016-10-12 22:53:40 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -22,36 +22,38 @@ function romanTime(time) {
     number[30] = 'XXX';
     number[40] = 'XL';
     number[50] = 'L';
-    if (typeof time == 'undefined' ) throw new TypeError();
-    if (typeof time == 'object' ) throw new TypeError();
+    if (typeof time === 'undefined' || typeof time === 'object') 
+    {
+        throw new TypeError();
+    }
     var a = '';
     var b = '';
-    for (var i = 0; i< time.length; i++)
+    for (var i = 0; i < time.length; i++)
     {
-        if (time.charAt(i) == ':')
+        if (time.charAt(i) === ':')
         {
-            a = time.substring(0,i);
-            b = time.substring(i+1,time.length);
-            if (parseInt(a,10)!=+a) throw new TypeError();
-            if (parseInt(b,10)!=+b) throw new TypeError();
-            if (a<0 || a>23) throw new TypeError();
-            if (b<0 || b>59) throw new TypeError();
+            a = time.substring(0, i);
+            b = time.substring(i + 1, time.length);
+            if (parseInt(a, 10) !== +a) throw new TypeError();
+            if (parseInt(b, 10) != +b) throw new TypeError();
+            if (a < 0 || a > 23) throw new TypeError();
+            if (b < 0 || b > 59) throw new TypeError();
             break;
         }		
     }
-    var a1 = a%10;
-    var a2 = a-a1;
-    if (a1 == 0 && a2 == 0) a = 'N';
-    else if (a1 == 0) a = number[a2];
-    else if (a2 == 0) a = number[a1];
-    else a = number[a2]+''+number[a1];
-    var b1 = b%10;
-    var b2 = b-b1;
-    if (b1 == 0 && b2 == 0) b = 'N';
-    else if (b1 == 0) b = number[b2];
-    else if (b2 == 0) b = number[b1];
-    else b = number[b2]+''+number[b1];
-    time = a+':'+b;
+    var a1 = a % 10;
+    var a2 = a - a1;
+    if (a1 === 0 && a2 === 0) a = 'N';
+    else if (a1 === 0) a = number[a2];
+    else if (a2 === 0) a = number[a1];
+    else a = number[a2] + '' + number[a1];
+    var b1 = b % 10;
+    var b2 = b - b1;
+    if (b1 === 0 && b2 === 0) b = 'N';
+    else if (b1 === 0) b = number[b2];
+    else if (b2 === 0) b = number[b1];
+    else b = number[b2] + '' + number[b1];
+    time = a + ':' + b;
     return time;
 }
 
