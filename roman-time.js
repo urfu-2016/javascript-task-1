6581584952a0d2f4dff2e25f4589e7cd00722c21
Mukhametov Denis <denis.mukhametov@gmail.com>
2016-10-12 22:34:01 +0500

    Initial commit

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,54 @@
 'use strict';
 
+function convertToRoman(parsed) {
+    if (parsed === 0) {
+        return 'N';
+    }
+    var romanDigits = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
+    var arabicDigits = [1, 4, 5, 9, 10, 40, 50];
+    var result = '';
+    var index = arabicDigits.length - 1;
+    while (parsed > 0) {
+        if (parsed >= arabicDigits[index]) {
+            result += romanDigits[index];
+            parsed -= arabicDigits[index];
+        } else {
+            index -= 1;
+        }
+    }
+
+    return result;
+}
+
+function parsePart(part, itemIndex) {
+    var parsed = parseInt(part, 10);
+    if (isNaN(parsed)) {
+        throw new TypeError();
+    }
+    if (parsed < 0 || parsed > (itemIndex === 0 ? 23 : 59)) {
+        throw new TypeError();
+    }
+
+    return parsed;
+}
+
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    if (typeof(time) !== 'string') {
+        throw new TypeError();
+    }
+    var parts = time.split(':');
+    if (parts.length !== 2) {
+        throw new TypeError();
+    }
+    var result = parts.map(parsePart)
+    .map(convertToRoman)
+    .join(':');
+
+    return result;
 }
 
 module.exports = romanTime;
