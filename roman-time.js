a8e92ba354aac8e27951133eda78265785e5ca80
AvinasMaria <marusja510@gmail.com>
2016-10-15 02:27:19 +0600

    Правки кода

--- roman-time.js
+++ roman-time.js
@@ -6,74 +6,65 @@
  */
 function romanTime(time) {
 
-    var splited_time = time.split(":");
-    var result = "";
+    var splitedTime = time.split(":");
+    var result = '';
     
-    for (var i = 0; i < splited_time.length; i++) {
+    for (var i = 0; i < splitedTime.length; i++) {
         try {
-            splited_time[i] = parseInt(splited_time[i]);
-            if isNaN(splited_time[i]) throw "Bad input"; 
-            if (splited_time[i] < 0) throw "Too low";
-            if (splited_time[i] > 59)||(splited_time[0] > 23) throw "Too high";
-            if (splited_time[i] === 0) result += "N"
-           
-            var l = splited_time[i] / 50;
-            
-            result += "L" * l;
-            splited_time[i] = splited_time[i] % 50;
-            
-            var x = splited_time[i] / 10;
-            
-            if (x === 4) {
-                result += "XL";
-            }
-            else {
-                result += "X" * x;
-            }
-            splited_time[i] = splited_time[i] % 10; 
-           
-            var v1 = splited_time[i] / 5;
-            var v2 = splited_time[i] % 5;
-            
-            if (v1 === 0) {
-                switch(v2) {
-                    case 1 :
-                        result += "I";
-                        break;
-                    case 2 :
-                        result += "II";
-                        break;
-                    case 3 :
-                        result += "III";
-                        break;
-                    case 4 :
-                        result += "IV";
-                        break;
-                    default : break;
-                }
-            }
-            else {
-                switch(v2) {
-                    case 1 :
-                        result += "VI";
-                        break;
-                    case 2 :
-                        result += "VII";
-                        break;
-                    case 3 :
-                        result += "VIII";
-                        break;
-                    case 4 :
-                        result += "IX";
-                        break;
-                    default : break;
-                }
-            }
-            if (i === 0) result += ":";
-        }
-        catch(err) {
+            splitedTime[i] = parseInt(splitedTime[i]);
+            if (isNaN(splitedTime[i])) throw "Bad input"; 
+            if (splitedTime[i] < 0) throw "Too low";
+            if ((splitedTime[i] > 59)||(splitedTime[0] > 23)) throw "Too high";
+		} catch (err) {
             return err;
         }
+		if (splitedTime[i] === 0) result += 'N';
+	   
+		var l = splitedTime[i] / 50;
+		
+		result += 'L' * l;
+		splitedTime[i] = splitedTime[i] % 50;
+		
+		var x = splitedTime[i] / 10;
+		
+		if (x === 4) {
+			result += 'XL';
+		} else {
+			result += 'X' * x;
+		}
+		splitedTime[i] = splitedTime[i] % 10; 
+	   
+		var v1 = splitedTime[i] / 5;
+		var v2 = splitedTime[i] % 5;
+		
+		if (v1 === 0) {
+			switch (v2) {
+				case 1 : result += 'I'; 
+				break;
+				case 2 : result += 'II'; 
+				break;
+				case 3 : result += 'III'; 
+				break;
+				case 4 : result += 'IV'; 
+				break;
+				default : break;
+			}
+		} else {
+			switch (v2) {
+				case 1 : result += 'VI';
+				break;
+				case 2 : result += 'VII'; 
+				break;
+				case 3 : result += 'VIII'; 
+				break;
+				case 4 : result += 'IX'; 
+				break;
+				default : break;
+			}
+		}
+		if (i === 0) {
+			result += ':';
+		}
     }
         
     return time;
