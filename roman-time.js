36bef3396b9fe79abb4f8f8fbf02d16377048052
andreyu008 <Andreyu008@gmail.com>
2016-10-14 23:54:28 +0500

    добавлен вариант решения

--- roman-time.js
+++ roman-time.js
@@ -4,9 +4,28 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
-}
+var hours = parseInt(process.argv[2], 10);
+var minutes = parseInt(process.argv[3],10);
 
+function romanTime(time){
+    var tens = Math.floor(time / 10) * 10;
+    var units = time - tens;
+    var hours = romans[tens] + romans[units];
+    return hours;
+}
+if (hours >= 0 && hours <=23 && minutes >=0 && minutes<=59) {
+
+    var romans = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+    romans [20] = 'XX';
+    romans [30] = 'XXX';
+    romans [40] = 'XL';
+    romans [50] = 'L';
+
+    var romanHours = romanTime(hours);
+    var romanMinutes=romanTime(minutes);
+
+    console.log(hours === 0 ? '-' : romanHours, romanMinutes);
+} else{
+    console.log('Время указанно неверно');
+}
 module.exports = romanTime;
