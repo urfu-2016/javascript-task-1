ed84d3f7d001794d9706e8a26ff9851563b1b366
Shmarova Elena <shmarova.ea@gmail.ru>
2016-10-14 23:11:23 +0500

    Исправлена логика и проблемы с complexity

--- roman-time.js
+++ roman-time.js
@@ -8,37 +8,70 @@ var fontAr = [0, 1, 4, 5, 9, 10, 40, 50];
 var fontRom = ['N', 'I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
 
 function getRoman(time) {
-    if (!time) return '';
+    if (Number(time) === 0) {
+
+        return 'N';
+    }
     var rezult = '';
     var n = fontAr.length - 1;
     while (time > 0) {
         if (time >= fontAr[n]) {
             rezult += fontRom[n];
             time -= fontAr[n];
-        } else n--;
+        } else {
+            n--;
+        }
     }
 
     return rezult;
 }
 
+function getIsNan(hours, minutes) {
+    if (isNaN(hours) || isNaN(minutes)) {
+
+        return true;
+    }
+
+    return false;
+}
+
+function getRightMinutes(time) {
+    var part = time.split(':', 2);
+    if (part.length !== 2 || !time ||
+        Number(part[1]) > 59 || Number(part[1]) < 0) {
+
+        return true;
+    }
+
+    return false;
+}
+
+function getRightHours(time) {
+    var part = time.split(':', 2);
+    if (part.length !== 2 || !time ||
+        Number(part[0]) > 23 || Number(part[0]) < 0) {
+
+        return true;
+    }
+
+    return false;
+}
+
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
     var part = time.split(':', 2);
-    if ((part.length === 2) && (isNaN(time))) {
-        var hours = Number(part[0]);
-        var minutes = Number(part[1]);
-    } else {
+    if (getRightMinutes(time) || getRightHours(time)) {
         throw new TypeError('Неверное время');
     }
-    if ((hours >= 1) && (hours <= 23) &&
-        (minutes >= 1) && (minutes <= 59)) {
-        var romHour = getRoman(hours);
-        var romMinute = getRoman(minutes);
+    var hours = Number(part[0]);
+    var minutes = Number(part[1]);
+    var romHour = getRoman(hours);
+    var romMinute = getRoman(minutes);
+    if (getIsNan(hours, minutes)) {
+        throw new TypeError('Неверное время');
+    }
+    time = romHour + ':' + romMinute;
 
-        return romHour + ':' + romMinute;
-    } else {
-        // ((hours === 0 || hours === 24) || (minutes === 0)); {
-        throw new TypeError('Неверное время');
-    }
+    return time;
 }
 module.exports = romanTime;
