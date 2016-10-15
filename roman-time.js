4e1d7d7ce0a524466d8684b2e4ee20c3a24d8424
andreyu008 <Andreyu008@gmail.com>
2016-10-15 13:18:45 +0500

    обновил

--- roman-time.js
+++ roman-time.js
@@ -1,9 +1,4 @@
 'use strict';
-
-/**
- * @param {String} time – время в формате HH:MM (например, 09:05)
- * @returns {String} – время римскими цифрами (IX:V)
- */
 var hours = parseInt(process.argv[2], 10);
 var minutes = parseInt(process.argv[3],10);
 
