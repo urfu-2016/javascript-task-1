11944f6331676f2a234e3f50ca66c34787ce4c7b
AvinasMaria <marusja510@gmail.com>
2016-10-19 04:14:21 +0600

    fix all

--- roman-time.js
+++ roman-time.js
@@ -1,73 +1,56 @@
 'use strict';
 
 /**
+ * @param {String} string
+ * @returns {String}
+ */
+function arabicToRoman(string) {
+    var rTime = '';
+    var number = Number(string);
+    var xList = ['X', 'XX', 'XXX', 'XL', 'L'];
+    var iList = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+
+    if (number === 0) {
+        return 'N';
+    }
+    if (number > 9) {
+        rTime += xList[Math.floor(number / 10) - 1];
+    }
+    number = number % 10;
+    if (number > 0) {
+        rTime += iList[number - 1];
+    }
+
+    return rTime;
+}
+
+/**
+ * @param {String} hours
+ * @param {String} minutes
+ * @returns {Boolean}
+ */
+function isCorrect(hours, minutes) {
+    var corHours = (hours.length === 2) && (typeof hours === 'string') && (Number(hours) >= 0) &&
+    (Number(hours) < 24);
+    var corMinutes = (hours.length === 2) && (typeof hours === 'string') &&
+    (Number(minutes) >= 0) && (Number(minutes) < 60);
+    if (corHours && corMinutes) {
+        return true;
+    }
+
+    throw new TypeError('Bad input');
+}
+
+/**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
+    var splitedTime = time.split(':');
 
-    var splitedTime = time.split(":");
-    var result = '';
-    
-    for (var i = 0; i < splitedTime.length; i++) {
-        try {
-            splitedTime[i] = parseInt(splitedTime[i]);
-            if (isNaN(splitedTime[i])) throw "Bad input"; 
-            if (splitedTime[i] < 0) throw "Too low";
-            if ((splitedTime[i] > 59)||(splitedTime[0] > 23)) throw "Too high";
-		} catch (err) {
-            return err;
-        }
-		if (splitedTime[i] === 0) result += 'N';
-	   
-		var l = splitedTime[i] / 50;
-		
-		result += 'L' * l;
-		splitedTime[i] = splitedTime[i] % 50;
-		
-		var x = splitedTime[i] / 10;
-		
-		if (x === 4) {
-			result += 'XL';
-		} else {
-			result += 'X' * x;
-		}
-		splitedTime[i] = splitedTime[i] % 10; 
-	   
-		var v1 = splitedTime[i] / 5;
-		var v2 = splitedTime[i] % 5;
-		
-		if (v1 === 0) {
-			switch (v2) {
-				case 1 : result += 'I'; 
-				break;
-				case 2 : result += 'II'; 
-				break;
-				case 3 : result += 'III'; 
-				break;
-				case 4 : result += 'IV'; 
-				break;
-				default : break;
-			}
-		} else {
-			switch (v2) {
-				case 1 : result += 'VI';
-				break;
-				case 2 : result += 'VII'; 
-				break;
-				case 3 : result += 'VIII'; 
-				break;
-				case 4 : result += 'IX'; 
-				break;
-				default : break;
-			}
-		}
-		if (i === 0) {
-			result += ':';
-		}
+    if (isCorrect(splitedTime[0], splitedTime[1])) {
+        return arabicToRoman(splitedTime[0]) + ':' + arabicToRoman(splitedTime[1]);
     }
-        
-    return time;
 }
 
 module.exports = romanTime;
