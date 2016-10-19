e42836926ffda293c7ec500be14d617c888ea8c3
MaksimMsnov <maxi.sn.174@gmail.com>
2016-10-19 23:40:29 +0500

    Первая попытка

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,41 @@
 'use strict';
 
+var ou = 'N';
+var first = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+var second = ['X', 'XX', 'XXX', 'XL', 'L', 'LX'];
+
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
+
+
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+
+    var parts = time.split(':');
+
+    var hours = parseInt(parts[0],10);
+    var minutes = parseInt(parts[1],10);
+
+    if (isNaN(hours) || isNaN(minutes) || hours < 0 || hours > 23 || minutes < 0 || minutes > 60)
+    {throw new TypeError();}
+
+    return arToRom(hours) + ':' + arToRom(minutes);
+
+}
+
+function arToRom(pattern) {
+
+    var resPattern = '';
+
+    resPattern += first[pattern % 10];
+    resPattern += second[(pattern / 10) % 10];
+
+    if (result === '')
+    {resPattern = ou;}
+
+    return resPattern;
+
 }
 
 module.exports = romanTime;
