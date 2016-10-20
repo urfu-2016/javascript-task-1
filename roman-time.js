498c730d9939677ddb0401bdb359652bf5511aae
SergeyIstomin96 <sergeyistomin96bello@gmail.com>
2016-10-20 17:21:23 +0500

    Все сначала2

--- roman-time.js
+++ roman-time.js
@@ -7,34 +7,34 @@
 function romanTime(time) {
     try {
         var arab = [1, 4, 5, 9, 10, 40, 50];
-        var roman = ['I','IV','V','IX','X','XL','L'];
+        var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
         var strs = time.split(':');
-        var numTime = [parseInt(srs[0], 10), parseInt(srs[1], 10)];
+        var numTime = [parseInt(strs[0], 10), parseInt(strs[1], 10)];
         if (numTime[0].isNaN() || numTime[1].isNaN()) {
-          throw TypeError("Неверное время");
+            throw new TypeError('Неверное время');
         }
-        var j = arab.length-1;
-        strs[0]="";
-        strs[1]="";
-        for (var i=0; i<2; i++) {
-          if (numTime[i] == 0) {
-            strs[i] = 'N';
-            continue;
-          }
-            while (numTime[i]!=0) {
+        var j = arab.length - 1;
+        strs[0] = '';
+        strs[1] = '';
+        for (var i = 0; i < 2; i++) {
+            if (numTime[i] === 0) {
+              strs[i] = 'N';
+              continue;
+            }
+            while (numTime[i] != 0) {
                 if (numTime[i] <= arab[j]) {
-                    strs[i] += romam[j];
+                    strs[i] += roman[j];
                     numTime[i] -= arab[j];
                 }
                 else {
                     j--;
                 }
             }
-        }
+    }
+    time = numTime[0] + ':' + numTime[1];
     }
     catch (err) {
 			console.log(err.name + ':' + err.message)
     }
-    time = numTime[0]+':'+numTime[1];
     return time;
 }
