bff2883843cb960a86d8033c6a36f60e24bc344f
SergeyIstomin96 <sergeyistomin96bello@gmail.com>
2016-10-20 17:10:04 +0500

    Все сначала

--- roman-time.js
+++ roman-time.js
@@ -6,69 +6,35 @@
  */
 function romanTime(time) {
     try {
-        var time2 = String(time);
-				time = '';
-        var h = 0;
-        var i = 0;
-        while (time2[i] !== ':') {
-            h += time2[i];
-            i++;
+        var arab = [1, 4, 5, 9, 10, 40, 50];
+        var roman = ['I','IV','V','IX','X','XL','L'];
+        var strs = time.split(':');
+        var numTime = [parseInt(srs[0], 10), parseInt(srs[1], 10)];
+        if (numTime[0].isNaN() || numTime[1].isNaN()) {
+          throw TypeError("Неверное время");
         }
-        var m = 0;
-        for (++i; i<time2.length; i++) {
-            m += time2[i];
+        var j = arab.length-1;
+        strs[0]="";
+        strs[1]="";
+        for (var i=0; i<2; i++) {
+          if (numTime[i] == 0) {
+            strs[i] = 'N';
+            continue;
+          }
+            while (numTime[i]!=0) {
+                if (numTime[i] <= arab[j]) {
+                    strs[i] += romam[j];
+                    numTime[i] -= arab[j];
+                }
+                else {
+                    j--;
+                }
+            }
         }
-        h = Number(h);
-		    if (h==NaN || h>23 || h<0) {
-            throw new TypeError('Неверное время');
-        }
-        m = Number(m);
-        if (m==NaN || m>59 || m<0) {
-            throw new TypeError('Неверное время');
-        }
-        h = to_roman(h);
-        m = to_roman(m);
-        time = h + ':' + m;
     }
-	catch (err) {
+    catch (err) {
 			console.log(err.name + ':' + err.message)
-	}
-	return time;
+    }
+    time = numTime[0]+':'+numTime[1];
+    return time;
 }
-
-function to_roman (num) {
-    var res = '';
-    if (num==0) {
-        return 'N';
-    }
-    if (num>=50) { //50-59
-        num -= 50;
-        res = 'L';
-    }
-    else if (num>=10) { //10-49
-        var tens = (num - num%10)/10;
-        for (var j=0; j<tens; j++) {
-            res += 'X';
-        }
-        num -= tens*10;
-    }// only <10
-    if (num>=5) {
-        if (num==9) {
-            res += 'IX';
-            num -= 9;
-        }
-        else {
-            res += 'V';
-            num -= 5;
-        }
-    }
-    else if (num==4) {
-        res += 'IV';
-        num -= 4;
-    } // only 123
-    for (var j=0; j<num; j++) {
-        res +='I';
-    }
-    return res;
-}
-module.exports = romanTime;
