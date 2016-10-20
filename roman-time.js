b0a97a8920d525ca5755ff4df85d5a0b131cca02
SergeyIstomin96 <sergeyistomin96bello@gmail.com>
2016-10-21 00:34:23 +0500

    Похоже получилось

--- roman-time.js
+++ roman-time.js
@@ -1,40 +1,41 @@
 'use strict';
 
 /**
+ * @param {number} num – число, в арабской системе
+ * @returns {String} – строку римских цифр
+ */
+function toRoman(num) {
+    var arab = [1, 4, 5, 9, 10, 40, 50];
+    var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
+    var j = arab.length - 1;
+    var res = '';
+    if (num === 0) {
+      res = 'N';
+    }
+    while (num > 0) {
+        if (num >= arab[j]) {
+            res += roman[j];
+            num -= arab[j];
+        }
+        else {
+            j--;
+        }
+    }
+    return res;
+}
+/**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    try {
-        var arab = [1, 4, 5, 9, 10, 40, 50];
-        var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
-        var strs = time.split(':');
-        var numTime = [parseInt(strs[0], 10), parseInt(strs[1], 10)];
-        if (numTime[0].isNaN() || numTime[1].isNaN()) {
-            throw new TypeError('Неверное время');
-        }
-        var j = arab.length - 1;
-        strs[0] = '';
-        strs[1] = '';
-        for (var i = 0; i < 2; i++) {
-            if (numTime[i] === 0) {
-              strs[i] = 'N';
-              continue;
-            }
-            while (numTime[i] != 0) {
-                if (numTime[i] <= arab[j]) {
-                    strs[i] += roman[j];
-                    numTime[i] -= arab[j];
-                }
-                else {
-                    j--;
-                }
-            }
+    var strs = time.split(':');
+    var numTime = [parseInt(strs[0], 10), parseInt(strs[1], 10)];
+  //  console.info(numTime);
+    if (numTime[0].isNaN || numTime[1].isNaN || numTime[0]*numTime[1] < 0 || numTime[0]>23 || numTime[1]>59) {
+        throw new TypeError('Неверное время');
+        return ;
     }
-    time = numTime[0] + ':' + numTime[1];
-    }
-    catch (err) {
-			console.log(err.name + ':' + err.message)
-    }
+    time = toRoman(numTime[0]) + ':' + toRoman(numTime[1]);
     return time;
 }
+module.exports = romanTime;
