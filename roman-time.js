aab86cf473e036844c17fae8abbdaa916515cade
Barabanov Grigory <grishbar@mail.ru>
2017-10-04 19:37:21 +0600

    XXL 3

--- roman-time.js
+++ roman-time.js
@@ -10,24 +10,24 @@ function toRoman(num) {
     }
     while (num > 0) {
         if (num >= arab[n]) {
-           rezult += roman[n];
-           num -= arab[n];
+            rezult += roman[n];
+            num -= arab[n];
         }
-        else n--;   
+        else { 
+            n--;
+        }   
     }
 
     return rezult;
 }
 function romanTime(time) {
-    if (time === 'null') {
-        return new TypeError();
-    }
     var hours = Number(time.substring(0, 2));
     var minutes = Number(time.substring(3, 5));
     if (hours >= 24 || hours < 0 || minutes >= 60 || minutes < 0 || time.length !== 5) {
-        return new TypeError('Неверное время');
+        return new TypeError();
     }
-return toRoman(hours) + ':' + toRoman(minutes);
+
+    return toRoman(hours) + ':' + toRoman(minutes);
 }
 
 module.exports = romanTime;
